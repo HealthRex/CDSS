5c7fba2986d561005cd5966941d4ad257279fa94
Shivaal Roy <shivaal@stanford.edu>
2017-09-26 08:21:24 -0700

    Modify method of counting consecutive stats to differentiate instances with no prior history (NULL)

---
+++ scripts/LabTestAnalysis/lab_statistics/repeat_component_descriptive.py
@@ -0,0 +1,213 @@
+import time
+from os import path
+
+import customDBUtil
+import numpy as np
+import pandas as pd
+
+from collections import defaultdict, deque
+from cStringIO import StringIO
+from datetime import datetime
+
+# from medinfo.db.test.Const import RUNNER_VERBOSITY
+# from medinfo.db.Util import log
+from medinfo.db.test.Util import DBTestCase
+# from medinfo.common.test.Util import MedInfoTestCase
+from medinfo.db import DBUtil
+from medinfo.db.Model import SQLQuery
+# from medinfo.db.DBUtil import NUMBER, BOOLEAN, STRING, DATETIME
+
+BASE_NAME = 'base_name'
+CLINICAL_ITEM_ID = 'clinical_item_id'
+DATA_FOLDER = 'data_repeat_component_descriptive/'
+NUMBER_SECONDS_IN_A_DAY = 86400
+
+
+class RepeatComponents(object):
+
+  def _getNonNullComponents(self):
+    query = SQLQuery()
+    # SELECT
+    query.addSelect(BASE_NAME)
+    # query.addSelect('max_result_flag')
+    # FROM
+    query.addFrom('order_result_stat')
+    # WHERE
+    query.addWhere('max_result_flag is not null')
+
+    results = DBUtil.execute(query)
+
+    pd.DataFrame(results, columns=query.select).to_csv(
+        DATA_FOLDER + 'base_names.csv', index=False)
+
+  def _getComponentItemIds(self):
+    query = SQLQuery()
+    # SELECT
+    query.addSelect(CLINICAL_ITEM_ID)
+    query.addSelect('name')
+    # FROM
+    query.addFrom('clinical_item')
+    # WHERE
+    query.addWhere('clinical_item_category_id = 58')
+    query.addOrderBy('name')
+
+    results = DBUtil.execute(query)
+
+    df = pd.DataFrame(results, columns=query.select)
+    df['base_name'] = df['name'].str.replace(
+        '\([a-z]*\)', '', case=False).str.strip()
+    df.to_csv(DATA_FOLDER + 'result_ids.csv', index=False)
+
+  def _getPatientsComponentsHistories(self, item_ids):
+    query = SQLQuery()
+    # SELECT
+    query.addSelect('patient_id')
+    query.addSelect('name')
+    query.addSelect('item_date')
+    # FROM
+    query.addFrom('clinical_item as ci')
+    query.addFrom('patient_item as pi')
+    # WHERE
+    query.addWhere('ci.clinical_item_id = pi.clinical_item_id')
+    query.addWhereIn('ci.clinical_item_id', item_ids)
+
+    query.addOrderBy('patient_id')
+    query.addOrderBy('item_date')
+
+    # print query
+    # print query.getParams()
+    return customDBUtil.execute(query)
+
+  @staticmethod
+  def splitByPatient(results):
+    currentSplit = [results.next()]
+    for result in results:
+      if result[0] != currentSplit[-1][0]:
+        yield currentSplit
+        currentSplit = []
+      currentSplit.append(result)
+    yield currentSplit
+
+  @staticmethod
+  def getDayDifference(current_result, first_result):
+    return (current_result[2] - first_result[2]).total_seconds() / NUMBER_SECONDS_IN_A_DAY
+
+  @staticmethod
+  def isNormal(result_name):
+    return 'InRange' in result_name
+
+  @staticmethod
+  def getStats(results, isNormalFn, max_consecutive=5, bins=[1, 2, 4, 7, 30, 90]):
+    """Given a list/generator of lab/component results, returns a dictionary where the keys are (window_size, number of prior consecutive normal results) tuples, and the values are tuples (total number of occurrences, number of occurrences that are normal)
+      - Inputs
+        - Window Sizes / Bins: How far back to look for each lab test ordered for prior results
+        - Maximum number of consecutive normals to assess
+      - Outputs
+        For each window size + consective normal count (0 up to max count)
+        - Number of results in the sequence that occurred where preceded
+          by the EXACT number of consecutive number of normal results within the window period
+          (So if counting 2 prior consecutive normal results, even if see 3+ consecutive normal results,
+          don't count that in this instance, since those can be counted in higher order instances)
+        - Number of the results above that were themselves "normal"
+        - Additional result for consecutive normal count is NULL,
+          to count results where no prior result (normal or abnormal) existed within the window period
+    """
+    counts = [deque() for _ in xrange(len(bins))]
+    # key: (days back, consecutive), value: (total, next_normal)
+    stats = defaultdict(lambda: np.array([0, 0]))
+    prior_histories = [[False] for _ in xrange(len(bins))]
+    for result in results:
+      for window_size, queue, prior_history in zip(bins, counts, prior_histories):
+        # only keep whats relevant to the current result
+        poppedQueue = False
+        if queue:
+          if queue[0][0] is not None:
+            while queue and RepeatComponents.getDayDifference(result, queue[0]) > window_size:
+              poppedQueue = True
+              queue.popleft()
+          else:
+            prior_history[0] = bool(
+                RepeatComponents.getDayDifference(result, queue[0]) <= window_size)
+            queue.clear()
+
+        # if the queue was filtered and is now empty, prior_history is false
+        if poppedQueue and not queue:
+          prior_history[0] = False
+
+        if prior_history[0]:
+          # if there is prior_history, set number_consecutive_normals as usual
+          number_consecutive_normals = len(queue)
+        else:
+          # else set number_consecutive_normals as None
+          number_consecutive_normals = None
+
+        # increment total_count
+        stats[(window_size, number_consecutive_normals)][0] += 1
+
+        if isNormalFn(result[1]):
+          # if normal, increment normal_count
+          queue.append(result)
+          stats[(window_size, number_consecutive_normals)][1] += 1
+        else:
+          # else clear the queue and add a sentinel value with the date of the
+          # most recent result
+          queue.clear()
+          queue.append((None, None, result[2]))
+        prior_history[0] = True
+    # filter out windows that have more consecutive normal results than
+    # max_consecutive
+    return {k: v for k, v in stats.iteritems() if k[1] <= max_consecutive}
+
+  @staticmethod
+  def createGlobalStatsDf(global_stats):
+    df_global_stats = pd.DataFrame()
+    for base_name, d in dict(global_stats).iteritems():
+      to_df = []
+      for k, v in d.iteritems():
+        to_df.append([base_name] + list(k) + list(v))
+      df_global_stats = pd.concat(
+          [df_global_stats, pd.DataFrame(to_df, dtype='string').fillna('NULL')])
+    df_global_stats = df_global_stats.rename(columns={0: 'base_name',
+                                                      1: 'window',
+                                                      2: 'consecutive',
+                                                      3: 'total',
+                                                      4: 'normal'})
+
+    return df_global_stats.sort_values(['base_name', 'window', 'consecutive'])
+
+  def run(self, queryDB=False):
+    if queryDB:
+      self._getNonNullComponents()
+    df_base_names = pd.read_csv(
+        DATA_FOLDER + 'base_names.csv', dtype='string', na_filter=False)
+
+    if queryDB:
+      self._getComponentItemIds()
+    df_result_ids = pd.read_csv(
+        DATA_FOLDER + 'result_ids.csv', dtype='string', na_filter=False)
+
+    df_result_ids = pd.merge(df_result_ids, df_base_names, on='base_name')
+
+    times = []
+
+    global_stats = defaultdict(lambda: defaultdict(lambda: np.array([0, 0])))
+    for result_i, base_name in enumerate(df_result_ids['base_name'].drop_duplicates().tolist()):
+      print result_i, base_name
+      timer = time.time()
+      item_ids = df_result_ids[df_result_ids['base_name'] == base_name][
+          'clinical_item_id'].tolist()
+      results = self._getPatientsComponentsHistories(item_ids)
+      for patient_results in RepeatComponents.splitByPatient(results):
+        for k, v in RepeatComponents.getStats(patient_results, RepeatComponents.isNormal).iteritems():
+          global_stats[base_name][k] += v
+      times.append(time.time() - timer)
+
+    RepeatComponents.createGlobalStatsDf(global_stats).to_csv(
+        DATA_FOLDER + 'global_stats.csv', index=False)
+
+    print map(lambda x: round(x, 2), times)
+
+if __name__ == '__main__':
+  descriptive_stats = RepeatComponents()
+  descriptive_stats.run(queryDB=True)
+  # descriptive_stats.run()
--- scripts/LabTestAnalysis/lab_statistics/repeat_lab_descriptive.py
+++ scripts/LabTestAnalysis/lab_statistics/repeat_lab_descriptive.py
@@ -16,126 +16,85 @@ from medinfo.db.test.Util import DBTestCase
 from medinfo.db import DBUtil
 from medinfo.db.Model import SQLQuery
 # from medinfo.db.DBUtil import NUMBER, BOOLEAN, STRING, DATETIME
+from repeat_component_descriptive import RepeatComponents
 
 CLINICAL_ITEM_ID = 'clinical_item_id'
 DATA_FOLDER = 'data_repeat_lab_descriptive/'
 
 
-class RepeatResults(object):
-	def __init__(self):
-		self._max_consecutive = 5
+class RepeatLabs(object):
 
-	def _getNonNullResults(self):
-		query = SQLQuery()
-		# SELECT
-		query.addSelect('proc_code')
-		# FROM
-		query.addFrom('stride_order_proc')
-		# WHERE
-		query.addWhereLike('proc_code', 'LAB%')
-		query.addWhere('abnormal_yn is not null')
+  def _getNonNullLabs(self):
+    query = SQLQuery()
+    # SELECT
+    query.addSelect('proc_code')
+    # FROM
+    query.addFrom('stride_order_proc')
+    # WHERE
+    query.addWhereLike('proc_code', 'LAB%')
+    query.addWhere('abnormal_yn is not null')
 
-		query.addGroupBy('proc_code')
-		query.addOrderBy('proc_code')
+    query.addGroupBy('proc_code')
+    query.addOrderBy('proc_code')
 
-		results = DBUtil.execute(query)
+    results = DBUtil.execute(query)
 
-		df = pd.DataFrame(results, columns=query.select).to_csv(DATA_FOLDER + 'proc_codes.csv', index=False)
-		# df[df['proc_code'].str.startswith('LAB')].to_csv(DATA_FOLDER + 'proc_codes.csv', index=False)
+    df = pd.DataFrame(results, columns=query.select).to_csv(
+        DATA_FOLDER + 'proc_codes.csv', index=False)
+    # df[df['proc_code'].str.startswith('LAB')].to_csv(DATA_FOLDER + 'proc_codes.csv', index=False)
 
-	def _getPatientsResultsHistories(self, proc_codes):
-		query = SQLQuery()
-		# SELECT
-		query.addSelect('pat_id')
-		query.addSelect('abnormal_yn')
-		query.addSelect('result_time')
-		query.addSelect('proc_code')
-		# FROM
-		query.addFrom('stride_order_proc')
-		# query.addFrom('patient_item as pi')
-		# WHERE
-		query.addWhereEqual('lab_status', 'Final result')
-		# query.addWhereEqual('proc_code', proc_code)
-		query.addWhereIn('proc_code', proc_codes)
+  def _getPatientsLabsHistories(self, proc_codes):
+    query = SQLQuery()
+    # SELECT
+    query.addSelect('pat_id')
+    query.addSelect('abnormal_yn')
+    query.addSelect('result_time')
+    query.addSelect('proc_code')
+    # FROM
+    query.addFrom('stride_order_proc')
+    # query.addFrom('patient_item as pi')
+    # WHERE
+    query.addWhereEqual('lab_status', 'Final result')
+    # query.addWhereEqual('proc_code', proc_code)
+    query.addWhereIn('proc_code', proc_codes)
 
-		query.addOrderBy('proc_code')
-		query.addOrderBy('pat_id')
-		query.addOrderBy('result_time')
+    query.addOrderBy('proc_code')
+    query.addOrderBy('pat_id')
+    query.addOrderBy('result_time')
 
-		return customDBUtil.execute(query)
+    return customDBUtil.execute(query)
 
-	def _splitByPatient(self, results):
-		currentSplit = [results.next()]
-		for result in results:
-			if result[0] != currentSplit[-1][0]:
-				yield currentSplit
-				currentSplit = []
-			currentSplit.append(result)
-		yield currentSplit
+  @staticmethod
+  def isNormal(abnormal_yn):
+    return abnormal_yn is None
 
-	def _isNormal(self, abnormal_yn):
-		return abnormal_yn is None
+  def run(self, queryDB=False, batch_size=10):
+    if queryDB:
+      self._getNonNullLabs()
+    df_proc_codes = pd.read_csv(
+        DATA_FOLDER + 'proc_codes.csv', dtype='string', na_filter=False)
 
-	def _consec(self, q):
-		return min(self._max_consecutive, len(q)-1)
+    times = []
 
-	def _getStats(self, results):
-		bins = [1,2,4,7,30,90]
-		counts = [deque() for _ in bins]
-		# k = (days back, consecutive), val = (total, next_normal)
-		stats = defaultdict(lambda: np.array([0,0]))
-		for result in results:
-			for size, queue in zip(bins, counts):
-				# only keep whats relevant to the current result
-				queue.append(result)
-				while (queue[-1][2] - queue[0][2]).total_seconds()/86400 > size:
-					queue.popleft()
-				# increment total_count
-				stats[(size, self._consec(queue))][0] += 1
-				# if normal, increment normal_count
-				if self._isNormal(result[1]):
-					stats[(size, self._consec(queue))][1] += 1
-				else:
-					queue.clear()
-		return {k:v for k, v in stats.iteritems()}
+    global_stats = defaultdict(lambda: defaultdict(lambda: np.array([0, 0])))
+    proc_codes = df_proc_codes['proc_code'].tolist()
+    for batch_i in xrange(int(np.ceil(1.0*len(proc_codes) / batch_size))):
+      print batch_i, proc_codes[batch_i * batch_size]
+      timer = time.time()
+      batch = proc_codes[batch_i * batch_size:(batch_i + 1) * batch_size]
+      results = self._getPatientsLabsHistories(batch)
+      for patient_results in RepeatComponents.splitByPatient(results):
+        proc_code = patient_results[0][3]
+        for k, v in RepeatComponents.getStats(patient_results, RepeatLabs.isNormal).iteritems():
+          global_stats[proc_code][k] += v
+      times.append(time.time() - timer)
 
-	def run(self, queryDB=False, batch_size=10):
-		if queryDB: self._getNonNullResults()
-		df_proc_codes = pd.read_csv(DATA_FOLDER + 'proc_codes.csv', dtype='string', na_filter=False)
+    RepeatComponents.createGlobalStatsDf(global_stats).to_csv(
+        DATA_FOLDER + 'global_stats.csv', index=False)
 
-		times = []
-
-		global_stats = defaultdict(lambda: defaultdict(lambda: np.array([0,0])))
-		proc_codes = df_proc_codes['proc_code'].tolist()
-		for batch_i in xrange(len(proc_codes) / batch_size + 1):
-			print batch_i, proc_codes[batch_i]
-			timer = time.time()
-			batch = proc_codes[batch_i*batch_size:(batch_i+1)*batch_size]
-			results = self._getPatientsResultsHistories(batch)
-			for patient_results in self._splitByPatient(results):
-				proc_code = patient_results[0][3]
-				for k, v in self._getStats(patient_results).iteritems():
-					global_stats[proc_code][k] += v
-			times.append(time.time() - timer)
-
-		df_global_stats = pd.DataFrame()
-		for base_name, d in dict(global_stats).iteritems():
-			to_df = []
-			for k, v in d.iteritems():
-				to_df.append([base_name] + list(k) + list(v))
-			df_global_stats = pd.concat([df_global_stats, pd.DataFrame(to_df)])
-		df_global_stats = df_global_stats.rename(columns={0:'base_name',
-															1:'window',
-															2:'consecutive',
-															3:'total',
-															4:'normal'})
-
-		df_global_stats = df_global_stats.sort_values(['base_name','window','consecutive'])
-		df_global_stats.to_csv(DATA_FOLDER + 'global_stats.csv', index=False)
-
-		print map(lambda x:round(x,2), times)
+    print map(lambda x: round(x, 2), times)
 
 if __name__ == '__main__':
-	descriptive_stats = RepeatResults()
-	# descriptive_stats.run(queryDB=True)
-	descriptive_stats.run(batch_size=100)
+  descriptive_stats = RepeatLabs()
+  descriptive_stats.run(queryDB=True, batch_size=100)
+  # descriptive_stats.run(batch_size=100)
