c6e9820e8eda24672aabc78c4494379b13166dec
Santhosh Balasubramanian <santhoshbala@gmail.com>
2017-10-12 15:37:23 -0700

    initial commit of repo-wide test suite

---
+++ TestCDSS.py
@@ -0,0 +1,38 @@
+import unittest
+import logging
+
+# Set logging level.
+log = logging.getLogger("CDSS")
+
+# TODO(sbala): Complete implementation by enabling differential logging levels.
+# Move logging level settings to the application logic, away from the TestCases.
+# To do this, call the application name from each of the end test cases as the
+# logger name. Then set the logging level in the actual __main__ function for
+# the relevant test suite.
+#
+# Check on each individual test module...
+# medinfo.cpoe
+# medinfo.dataconversion
+# medinfo.db
+# medinfo.geography
+# medinfo.textanalysis
+# medinfo.web
+# scripts.CDSS
+# scripts.ExpertsVsEveryone
+# scripts.Ferritin
+# scripts.ICUDNR
+# scripts.LabTestAnalysis
+# scripts.MuthuDecayingWindows
+# scripts.OpioidRx
+# scripts.PressGaney
+# scripts.ResidentLogs
+# scripts.SepsisICU
+
+# Load test suite.
+loader = unittest.TestLoader()
+suite = loader.discover('./medinfo/cpoe', pattern="Test*.py")
+log.setLevel(logging.ERROR)
+
+# Run test suite.
+testRunner = unittest.runner.TextTestRunner(verbosity=2)
+testRunner.run(suite)
