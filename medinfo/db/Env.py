c026dbc21a719251124bb63bd58aa02ad04ce692
Santhosh Balasubramanian <santhoshbala@gmail.com>
2017-11-06 17:45:31 +0000

    adding LocalEnv file which is not source controlled along with template file to help understand it

--- medinfo/db/Env.py
+++ medinfo/db/Env.py
@@ -5,6 +5,7 @@ platform / environment where they are installed.
 
 import sys, os;
 import logging
+from LocalEnv import *
 
 """Default level for application logging.  Modify these for different scenarios.
 See Python logging package documentation for more information"""
@@ -53,7 +54,7 @@ DB_PARAM["DSN"]  = "medinfo-5year-time"
 #DB_PARAM["DSN"]  = "medinfo5yr"
 DB_PARAM["UID"]  = "sbala"
 DB_PARAM["PWD"]  = "1234"
-
+DB_PARAM = LOCAL_PROD_DB_PARAM
 
 # Opioid Notes DB
 #DB_PARAM["HOST"] = "cci-db-p03";
@@ -71,6 +72,7 @@ TEST_DB_PARAM["HOST"] = "localhost"
 TEST_DB_PARAM["DSN"]  = "testdb"
 TEST_DB_PARAM["UID"]  = "sbala"
 TEST_DB_PARAM["PWD"]  = "1234"
+TEST_DB_PARAM = LOCAL_TEST_DB_PARAM
 
 #TEST_DB_PARAM["DSN"] = "c:\Box Sync\NoSync\VAAlerts\dave_chan2.sqlite";
 #TEST_DB_PARAM["DSN"] = "/Users/angelicaperez/Documents/JonChen/sqlite_db/dave_chan2.sqlite"
