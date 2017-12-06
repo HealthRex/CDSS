2b1b07199ed0797c04494952c6fb69a0ae8a1db1
Santhosh Balasubramanian <santhoshbala@gmail.com>
2017-12-06 11:21:29 -0800

    set logging level to CRITICAL because some tests intentionally throw errors

--- TestCDSS.py
+++ TestCDSS.py
@@ -1,8 +1,8 @@
-"""Look through directory tree for unit test cases to run.
+"""
+Look through directory tree for unit test cases to run.
 Comparable to command-line functionality:
 
 python -m unittest discover -v -s <directoryName> -p "Test*.py"
-
 """
 
 import unittest
@@ -10,14 +10,11 @@ import logging
 
 # Set logging level.
 log = logging.getLogger("CDSS")
-
-# TODO(sbala): Fix TestDataManager.test_compositeRelated
-# TODO(sbala): Fix TestSTRIDEOrderMedConversion.test_dataConversion_denormalized
+log.setLevel(logging.CRITICAL)
 
 # Load test suite.
 loader = unittest.TestLoader()
 suite = loader.discover('.', pattern="Test*.py")
-log.setLevel(logging.ERROR)
 
 # Run test suite.
 testRunner = unittest.runner.TextTestRunner(verbosity=1)
