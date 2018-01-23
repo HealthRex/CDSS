dfac9822575f4700856c0d7a41b9fa67132401d7
Santhosh Balasubramanian <santhoshbala@gmail.com>
2018-01-23 14:10:55 -0800

    WIP: packing the processing step variables into a params object to minimize repetitive logic in subclasses

--- scripts/LabTestAnalysis/machine_learning/LabNormalityPredictionPipeline.py
+++ scripts/LabTestAnalysis/machine_learning/LabNormalityPredictionPipeline.py
@@ -39,79 +39,52 @@ class LabNormalityPredictionPipeline(SupervisedLearningPipeline):
 
     def _build_processed_matrix_path(self):
         template = '%s-normality-matrix-%d-epi-processed.tab'
-        pipeline_file_name = inspect.getfile(inspect.currentframe())
+        pipeline_file_path = inspect.getfile(inspect.currentframe())
         return SupervisedLearningPipeline._build_matrix_path(self, template, \
-            pipeline_file_name)
+            pipeline_file_path)
 
-    def _process_raw_feature_matrix(self):
+    def _build_processed_feature_matrix(self):
         # Buid paths for matrix files.
         raw_matrix_path = self._build_raw_matrix_path()
         processed_matrix_path = self._build_processed_matrix_path()
 
-        # If processed matrix exists, and the client has not requested to flush
-        # the cache, just use the matrix that already exists and return.
-        if os.path.exists(processed_matrix_path) and not self._flush_cache:
-            pass
-        else:
-            # Read raw matrix.
-            fm_io = FeatureMatrixIO()
-            raw_matrix = fm_io.read_file_to_data_frame(raw_matrix_path)
-
-            # Initialize FMT.
-            fmt = FeatureMatrixTransform()
-            fmt.set_input_matrix(raw_matrix)
-
-            # Add features.
-            FEATURES_TO_ADD = {}
-            SupervisedLearningPipeline._add_features(self, fmt, features_to_add)
-
-            # Impute data and remove features.
-            IMPUTATION_STRATEGIES = {}
-            FEATURES_TO_REMOVE = [
-                'index_time', 'Birth.pre',
-                'Male.preTimeDays', 'Female.preTimeDays',
-                'RaceWhiteHispanicLatino.preTimeDays',
-                'RaceWhiteNonHispanicLatino.preTimeDays',
-                'RaceHispanicLatino.preTimeDays',
-                'RaceAsian.preTimeDays',
-                'RaceBlack.preTimeDays',
-                'RacePacificIslander.preTimeDays',
-                'RaceNativeAmerican.preTimeDays',
-                'RaceOther.preTimeDays',
-                'RaceUnknown.preTimeDays'
-            ]
-            SupervisedLearningPipeline._impute_data(self, fmt, raw_matrix, \
-                IMPUTATION_STRATEGIES, FEATURES_TO_REMOVE)
-            SupervisedLearningPipeline._remove_features(self, fmt, \
-                FEATURES_TO_REMOVE)
-            fmt.drop_duplicate_rows()
-
-            processed_matrix = fmt.fetch_matrix()
-
-            # Divide processed_matrix into training and test data.
-            # This must happen before feature selection so that we don't
-            # accidentally learn information from the test data.
-            self._train_test_split(processed_matrix)
-            self._select_features()
-
-            # Write processed matrix to new file.
-            self._build_processed_feature_matrix()
-
-    def _build_processed_feature_matrix(self):
-        processed_matrix_path = self._build_processed_matrix_path()
-        SupervisedLearningPipeline._build_processed_feature_matrix(self, processed_matrix_path)
-
-    def _train_test_split(self, processed_matrix):
+        # Define parameters for processing steps.
+        params = {}
+        features_to_add = {}
+        imputation_strategies = {}
+        features_to_remove = [
+            'index_time', 'Birth.pre',
+            'Male.preTimeDays', 'Female.preTimeDays',
+            'RaceWhiteHispanicLatino.preTimeDays',
+            'RaceWhiteNonHispanicLatino.preTimeDays',
+            'RaceHispanicLatino.preTimeDays',
+            'RaceAsian.preTimeDays',
+            'RaceBlack.preTimeDays',
+            'RacePacificIslander.preTimeDays',
+            'RaceNativeAmerican.preTimeDays',
+            'RaceOther.preTimeDays',
+            'RaceUnknown.preTimeDays'
+        ]
         outcome_label = 'all_components_normal'
-        SupervisedLearningPipeline._train_test_split(self, processed_matrix, \
-            outcome_label)
+        selection_problem = FeatureSelector.CLASSIFICATION
+        selection_algorithm = FeatureSelector.RECURSIVE_ELIMINATION
+        percent_features_to_select = 0.01
+        matrix_class = LabNormalityMatrix
+        pipeline_file_name = inspect.getfile(inspect.currentframe())
 
-    def _select_features(self):
-        problem = FeatureSelector.CLASSIFICATION
-        algorithm = FeatureSelector.RECURSIVE_ELIMINATION
-        # Use FeatureSelector to prune all but 1% of variables.
-        percent = 0.01
-        SupervisedLearningPipeline._select_features(self, problem, percent, algorithm)
+        # Bundle parameters into single object to be unpacked in SLP.
+        params['features_to_add'] = features_to_add
+        params['imputation_strategies'] = imputation_strategies
+        params['features_to_remove'] = features_to_remove
+        params['outcome_label'] = outcome_label
+        params['selection_problem'] = selection_problem
+        params['selection_algorithm'] = selection_algorithm
+        params['percent_features_to_select'] = percent_features_to_select
+        params['matrix_class'] = matrix_class
+        params['pipeline_file_path'] = pipeline_file_path
+
+        # Defer processing logic to SupervisedLearningPipeline.
+        SupervisedLearningPipeline._build_processed_feature_matrix(self, params)
 
     def _train_predictor():
         pass
@@ -126,4 +99,5 @@ class LabNormalityPredictionPipeline(SupervisedLearningPipeline):
         pass
 
 if __name__ == '__main__':
-    lnpp = LabNormalityPredictionPipeline('LABMETB', 10)
+    print inspect.getfile(inspect.currentframe())
+    # lnpp = LabNormalityPredictionPipeline('LABMETB', 10)
