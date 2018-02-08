63e41d21f81021df22e6b907ac3db9c28e332e34
Santhosh Balasubramanian <santhoshbala@gmail.com>
2018-02-08 15:29:41 -0800

    hyperparameter tuning for DecisionTree

--- medinfo/ml/SupervisedClassifier.py
+++ medinfo/ml/SupervisedClassifier.py
@@ -27,7 +27,7 @@ class SupervisedClassifier:
 
     EXHAUSTIVE_SEARCH = 'exhaustive-search'
     STOCHASTIC_SEARCH = 'stochastic-search'
-    HYPEPARAM_STRATEGIES = [EXHAUSTIVE_SEARCH, STOCHASTIC_SEARCH]
+    HYPERPARAM_STRATEGIES = [EXHAUSTIVE_SEARCH, STOCHASTIC_SEARCH]
 
     def __init__(self, classes, hyperparams=None):
         self._classes = classes
@@ -50,10 +50,10 @@ class SupervisedClassifier:
         if self._hyperparams.get('random_state') is None:
             self._hyperparams['random_state'] = None
         # Set CV strategy.
-        if self._hyperparams.get('cv_strategy') is None:
-            self._hyperparams['cv_strategy'] = SupervisedClassifier.STOCHASTIC_SEARCH
-        elif self._hyperparams['cv_strategy'] not in SupervisedClassifier.HYPERPARAM_STRATEGIES:
-            raise ValueError('CV strategy %s not supported.' % self._hyperparams['hyperparam_strategy'])
+        if self._hyperparams.get('hyperparam_strategy') is None:
+            self._hyperparams['hyperparam_strategy'] = SupervisedClassifier.STOCHASTIC_SEARCH
+        elif self._hyperparams['hyperparam_strategy'] not in SupervisedClassifier.HYPERPARAM_STRATEGIES:
+            raise ValueError('Hyperparameter strategy %s not supported.' % self._hyperparams['hyperparam_strategy'])
 
     def __repr__(self):
         s = "SupervisedClassifier(%s, algorithm='%s', random_state=%s)" % \
@@ -75,6 +75,18 @@ class SupervisedClassifier:
             sig_features = [(coefs[cols.get_loc(f)], f) for f in cols.values if coefs[cols.get_loc(f)] > 0]
             linear_model = ' + '.join('%s*%s' % (weight, feature) for weight, feature in sig_features)
             return 'L1_REGRESS_AND_ROUND(%s)' % linear_model
+        elif self._hyperparams['algorithm'] == SupervisedClassifier.DECISION_TREE:
+            params = self._params_decision_tree()
+            tree = params['tree']
+            node_list = list()
+            for i in range(params['num_nodes']):
+                if params['tree'][i].get('feature') is None:
+                    continue
+                feature = params['tree'][i]['feature']
+                threshold = params['tree'][i]['threshold']
+                node_list.append('(%s<=%s)'% (feature, threshold))
+            decision_path = ', '.join(node_list)
+            return 'DECISION_TREE(%s)' % decision_path
         else:
             return 'SupervisedClassifier(%s, %s)' % (self._classes, self._algorithm)
 
@@ -90,31 +102,63 @@ class SupervisedClassifier:
     def hyperparams(self):
         return self._hyperparams
 
-    def _regression_params(self):
-        parameters = {}
+    def params(self):
+        if self._hyperparams['algorithm'] == SupervisedClassifier.LOGISTIC_REGRESSION:
+            return self._params_regression()
+        elif self._hyperparams['algorithm'] == SupervisedClassifier.REGRESS_AND_ROUND:
+            return self._params_regression()
+        elif self._hyperparams['algorithm'] == SupervisedClassifier.DECISION_TREE:
+            return self._params_decision_tree()
+
+    def _params_regression(self):
+        params = {}
         coefs = self.coefs()
         columns = self._features
         feature_labels = columns.values
         for feature in feature_labels:
             weight = coefs[columns.get_loc(feature)]
-            parameters.update({feature: weight})
+            params.update({feature: weight})
 
-        return parameters
+        return params
 
-    def params(self):
-        if self._algorithm == SupervisedClassifier.LOGISTIC_REGRESSION:
-            return self._regression_params()
-        elif self._algorithm == SupervisedClassifier.REGRESS_AND_ROUND:
-            return self._regression_params()
+    def _params_decision_tree(self):
+        t = self._model.tree_
+        feature_labels = self._features.values
+
+        # Iterate through nodes in tree.
+        params = {'tree': {}}
+        decision_features = list()
+        for i in range(t.node_count):
+            # Build data about each node.
+            node = {}
+            # If not a leaf node, add decision information.
+            if t.children_left[i] != t.children_right[i]:
+                node['feature'] = feature_labels[t.feature[i]]
+                decision_features.append(node['feature'])
+                node['threshold'] = t.threshold[i]
+                node['left_child'] = t.children_left[i]
+                node['right_child'] = t.children_right[i]
+            class_weights = t.value[i].tolist()[0]
+            node['class_weights'] = class_weights
+            node['prediction'] = self._classes[class_weights.index(np.max(class_weights))]
+
+            params['tree'].update({i: node})
+
+        # Add tree-level information.
+        params['decision_features'] = decision_features
+        params['depth'] = t.max_depth
+        params['num_nodes'] = t.node_count
+
+        return params
 
     def _maybe_reshape_y(self, y):
         # If necessary, reshape y from (n_samples, 1) to (n_samples, )
         try:
             num_cols = y.shape[1]
             y = column_or_1d(y)
-            log.info('Reshaped y to 1d.')
+            log.debug('Reshaped y to 1d.')
         except IndexError:
-            log.info('Did not need to reshape y to 1d.')
+            log.debug('Did not need to reshape y to 1d.')
 
         return y
 
@@ -169,9 +213,8 @@ class SupervisedClassifier:
 
     def train(self, X, y):
         self._features = X.columns
-
         y = self._maybe_reshape_y(y)
-
+        log.info('Training %s classifier...' % self._hyperparams['algorithm'])
         if self._algorithm == SupervisedClassifier.DECISION_TREE:
             self._train_decision_tree(X, y)
         elif self._algorithm == SupervisedClassifier.LOGISTIC_REGRESSION:
@@ -187,14 +230,14 @@ class SupervisedClassifier:
         self._hyperparams['criterion'] = 'gini'
         self._hyperparams['splitter'] = 'best'
         # Include 1, 2, 3 to bias towards simpler tree.
-        self._hyperparams['max_depth'] = [1, 2, 3, None]
+        self._hyperparams['max_depth'] = None
         # Include 10 and .01 to bias towards simpler trees.
-        self._hyperparams['min_samples_split'] = [2, 10, 0.01, 0.05]
-        self._hyperparams['min_samples_leaf'] = [1, 10, 0.01, 0.05]
+        self._hyperparams['min_samples_split'] = 2
+        self._hyperparams['min_samples_leaf'] = 1
         self._hyperparams['min_weight_fraction_leaf'] = 0.0
         self._hyperparams['max_features'] = None
         self._hyperparams['max_leaf_nodes'] = None
-        self._hyperparams['min_impurity_decrease'] = None
+        self._hyperparams['min_impurity_decrease'] = 0.0
         self._hyperparams['class_weight'] = 'balanced'
         self._hyperparams['presort'] = None
         # Assume unbalanced classification problems, so use f1_score.
@@ -202,15 +245,16 @@ class SupervisedClassifier:
         # http://scikit-learn.org/stable/modules/grid_search.html#specifying-an-objective-metric
         scorer = make_scorer(f1_score)
         self._hyperparams['scoring'] = scorer
+        self._hyperparams['n_jobs'] = -1
 
         # Initialize model with naive hyperparameter values.
         self._model = DecisionTreeClassifier(\
             criterion=self._hyperparams['criterion'], \
             splitter=self._hyperparams['splitter'], \
-            max_depth=self._hyperparams['max_depth'][0], \
-            min_samples_split=self._hyperparams['min_samples_split'][0], \
-            min_samples_leaf=self._hyperparams['min_samples_leaf'][0], \
-            min_weight_fraction_leaf=self._hyperparams['min_weight_fraction_leaf'][0], \
+            max_depth=self._hyperparams['max_depth'], \
+            min_samples_split=self._hyperparams['min_samples_split'], \
+            min_samples_leaf=self._hyperparams['min_samples_leaf'], \
+            min_weight_fraction_leaf=self._hyperparams['min_weight_fraction_leaf'], \
             max_features=self._hyperparams['max_features'], \
             random_state=self._hyperparams['random_state'], \
             max_leaf_nodes=self._hyperparams['max_leaf_nodes'], \
@@ -221,13 +265,13 @@ class SupervisedClassifier:
 
         # Search hyperparameter space for better values.
         hyperparam_search_space = {
-            'max_depth': [1, 2, 3, None],
-            'min_samples_split': [2, 10, 0.01, 0.05],
-            'min_samples_leaf': [1, 10, 0.01, 0.05]
+            'max_depth': [1, 2, 3, 4, 5, None],
+            'min_samples_split': [2, 20, 0.02, 0.1, 0.2],
+            'min_samples_leaf': [1, 10, 0.01, 0.05, 0.1]
         }
-        self._tune_hyperparams(hyperparam_search_space)
-
-        self._model.fit(X, y)
+        log.info('Tuning hyperparameters...')
+        self._tune_hyperparams(hyperparam_search_space, X, y)
+        log.debug('params: %s' % self.params())
 
     def _train_logistic_regression(self, X, y):
         # Define hyperparameter space.
@@ -274,9 +318,10 @@ class SupervisedClassifier:
         self._model.fit(X, y)
 
     def _train_regress_and_round(self, X, y):
-        log.info('Rounding logistic regression coefficients...')
         self._train_logistic_regression(X, y)
+        log.info('Tuning hyperparameters...')
         self._tune_hyperparams_regress_and_round(X, y)
+        log.debug('params: %s' % self.params())
 
     def _tune_hyperparams_regress_and_round(self, X, y):
         self._hyperparams['hyperparam_strategy'] = SupervisedClassifier.EXHAUSTIVE_SEARCH
@@ -293,17 +338,13 @@ class SupervisedClassifier:
         # coef_max = M = max rounded coefficient value
         # beta_max = max_i|beta_i| = largest unrounded regression coefficient
         beta_max = max([abs(c) for c in self._model.coef_[0]])
-        log.debug('unrounded_coefs: %s' % self.coefs())
         self._model.coef_[0] = [round((coef_max * c) / (beta_max)) for c in self._model.coef_[0]]
-        log.debug('rounded_coefs(M=%s): %s' % (coef_max, self.coefs()))
 
     def _tune_coef_max(self, X, y):
         # The only way to easily compute scores is to modify the model itself.
         # Keep the unrounded coefs so it's always possible to go back.
         unrounded_coefs = self.coefs().copy()
 
-        # The optimal M value from Jung et al. is 3.
-        # https://arxiv.org/abs/1702.04690
         M_grid = [1, 2, 3, 4, 5]
         M_grid_scores = []
 
@@ -322,7 +363,6 @@ class SupervisedClassifier:
             # Compute mean across K folds.
             mean_score = np.mean(scores)
             M_grid_scores.append(mean_score)
-            log.debug('roc_auc(M=%s) = %s' % (M_grid[i], mean_score))
 
             # Conditionally change coef_max and max_roc_auc.
             if mean_score > max_roc_auc:
@@ -334,28 +374,50 @@ class SupervisedClassifier:
             # Reset coefs and return.
             self._model.coef_[0] = unrounded_coefs
 
-        log.info('RANGE(roc_auc | M) = [%s, %s]' % (min_roc_auc, max_roc_auc))
+        # The optimal M value from Jung et al. is 3.
+        # https://arxiv.org/abs/1702.04690
+        log.debug('tune(M): %s --> %s' % (3, coef_max))
+        log.debug('tune(roc_auc): %s --> %s' % (min_roc_auc, max_roc_auc))
 
         return coef_max
 
-    def _tune_hyperparams(self, hyperparam_space):
-        # Don't use sklearn utilities for REGRESS_AND_ROUND.
-        if self._hyperparams['algorithm'] == SupervisedClassifier.REGRESS_AND_ROUND:
-            self._tune_hyperparams_regress_and_round()
-        else:
-            if self._hyperparams['hyperparam_strategy'] == SupervisedClassifier.EXHAUSTIVE_SEARCH:
-                self._tune_hyperparams_exhaustively(hyperparam_space)
-            elif self._hyperparams['hyperparam_strategy'] == SupervisedClassifier.STOCHASTIC_SEARCH:
-                self._tune_hyperparams_stochastically(hyperparam_space)
+    def _tune_hyperparams(self, hyperparam_search_space, X, y):
+        # Log the pre-tuning score.
+        self._hyperparams['cv'] = self._build_cv_generator()
+        pre_tuning_score = np.mean(cross_val_score(self._model, X, y, \
+                                    cv=self._hyperparams['cv'], \
+                                    scoring=self._hyperparams['scoring'], \
+                                    n_jobs=self._hyperparams['n_jobs']))
 
-    def _tune_hyperparams_exhaustively(self, hyperparam_space):
-        # grid search
-        pass
+        # Initialize hyperparam tuner.
+        # Assume the model was initialized before this function.
+        if self._hyperparams['hyperparam_strategy'] == SupervisedClassifier.EXHAUSTIVE_SEARCH:
+            tuner = GridSearchCV(self._model, hyperparam_search_space, \
+                                    scoring=self._hyperparams['scoring'], \
+                                    n_jobs=self._hyperparams['n_jobs'], \
+                                    iid=False, \
+                                    refit=True, \
+                                    cv=self._hyperparams['cv'], \
+                                    return_train_score=False)
+        elif self._hyperparams['hyperparam_strategy'] == SupervisedClassifier.STOCHASTIC_SEARCH:
+            tuner = RandomizedSearchCV(self._model, hyperparam_search_space, \
+                                        scoring=self._hyperparams['scoring'], \
+                                        n_jobs=self._hyperparams['n_jobs'], \
+                                        iid=False, \
+                                        refit=True, \
+                                        cv=self._hyperparams['cv'], \
+                                        random_state=self._hyperparams['random_state'], \
+                                        return_train_score=False)
+        tuner.fit(X, y)
 
-    def _tune_hyperparams_stochastically(self):
-        # random search
-
-        pass
+        # Set model and hyperparams.
+        self._model = tuner.best_estimator_
+        for key in tuner.best_params_.keys():
+            log.debug('tune(%s): %s --> %s' % (key, self._hyperparams[key], \
+                        tuner.best_params_[key]))
+            self._hyperparams[key] = tuner.best_params_[key]
+        log.debug('tune(%s): %s --> %s' % (self._hyperparams['scoring'],\
+                    pre_tuning_score, tuner.best_score_))
 
     def predict(self, X):
         return self._model.predict(X)
