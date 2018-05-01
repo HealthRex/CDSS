b2c829ea16dbea19c6816500721a268014e0b482
Santhosh Balasubramanian <santhoshbala@gmail.com>
2018-04-30 19:35:21 -0700

    processing of demographics, notes, and admit data

---
+++ starr/psql/schemata/starr_admit_meds.sql
@@ -0,0 +1,25 @@
+-- Table: starr_admit_meds
+-- Description: Medications actively taken by pat_anon_id as of contact_date.
+-- Raw Files:
+--  * Chen_Active_Meds_At_Admit.csv.gz
+-- Clean Files:
+--  * starr_admit_meds_2014.csv.gz
+-- CSV Fields:
+--  * "PAT_ANON_ID" (e.g. 11577528280870)
+--  * "CONTACT_DATE" (e.g. 04/21/2009)
+--  * "MEDICATION_ID" (e.g. 1698)
+--  * "DESCRIPTION" (e.g. "PLAVIX 75 MG PO TABS")
+--  * "THERA_CLASS" (e.g. "CARDIOVASCULAR")
+--  * "PHARM_CLASS" (e.g. "PROTON-PUMP INHIBITORS")
+--  * "PHARM_SUBCLASS" (e.g. "Anticoagulants - Coumarin")
+
+CREATE TABLE IF NOT EXISTS starr_admit_meds
+(
+  pat_anon_id BIGINT,
+  contact_date TIMESTAMP,
+  medication_id INTEGER,
+  description TEXT,
+  thera_class TEXT,
+  pharm_class TEXT,
+  pharm_subclass TEXT
+);
---
+++ starr/psql/schemata/starr_admit_vitals.sql
@@ -0,0 +1,25 @@
+-- Table: starr_admit_vitals
+-- Description: Vitals measured for pat_anon_id at time of admission.
+-- Raw Files:
+--  * Chen_Active_Vitals.csv.gz
+-- Clean Files:
+--  * starr_admit_vitals.csv.gz
+-- CSV Fields:
+--  * "PAT_ID" (e.g. 11577528280870)
+--  * "PAT_ENC_CSN_ID" (e.g. 431388)
+--  * "BP_SYSTOLIC" (e.g. 120)
+--  * "BP_DIASTOLIC" (e.g. 80)
+--  * "TEMPERATURE" (e.g. 98.6)
+--  * "PULSE" (e.g. 100)
+--  * "RESPIRATIONS" (e.g. 15)
+
+CREATE TABLE IF NOT EXISTS starr_admit_vitals
+(
+  pat_id BIGINT,
+  pat_enc_csn_id BIGINT,
+  bp_systolic INTEGER,
+  bp_diastolic INTEGER,
+  temperature FLOAT,
+  pulse INTEGER,
+  RESPIRATIONS INTEGER
+);
