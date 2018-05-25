f3d40617c762bd569c4e1d9e4a43d26bbcb28ce7
Santhosh Balasubramanian <santhoshbala@gmail.com>
2018-05-25 00:37:23 -0400

    rename tables

--- stride/psql/schemata/stride_mapped_meds.schema.sql
+++ stride/psql/schemata/stride_mapped_meds.schema.sql
@@ -1,4 +1,4 @@
--- Table: stride_medication
+-- Table: stride_mapped_meds
 -- Description: Medication mapped to RxNorm Concept Unique Identifier (RxCUI).
 -- Original Files:
 --	* Chen_Mapped_Meds_5Yr.csv.gz
@@ -13,7 +13,7 @@
 --  * "GENERIC_NAME" (e.g. "LORazepam 1 mg tablet")
 --  * active_ingredient (e.g. azatadine) (this field imputed via RxNorm API)
 
-CREATE TABLE IF NOT EXISTS stride_medication
+CREATE TABLE IF NOT EXISTS stride_mapped_meds
 (
   medication_id INTEGER,
   name TEXT,
