1120153b7284b728eb79f26970622e345896dbd1
Santhosh Balasubramanian <santhoshbala@gmail.com>
2018-05-25 00:31:55 -0400

    fix plural

--- stride/psql/indices/stride_order_result.indices.sql
+++
@@ -1,6 +0,0 @@
--- Table: stride_order_result
-
-CREATE INDEX IF NOT EXISTS index_stride_order_result_order_proc_id
-                            ON stride_order_result(order_proc_id);
-CREATE INDEX IF NOT EXISTS index_stride_order_result_base_name
-                            ON stride_order_results(base_name);
