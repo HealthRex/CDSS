8550166a1aa3a1f1dd87d746f1430446fc083645
sxu11 <song.xu.ucla@gmail.com>
2018-10-19 14:56:06 -0700

    Clean the UMich data preparation.

--- scripts/LabTestAnalysis/machine_learning/prepareData_UMich.py
+++ scripts/LabTestAnalysis/machine_learning/prepareData_UMich.py
@@ -5,36 +5,34 @@ import sqlite3
 import pandas as pd
 import LocalEnv
 import utils_UMich
-
+from medinfo.common.Util import log
 import os
 
-def prepare_database(raw_data_files, raw_data_folderpath, db_name, fold_enlarge_data=1, USE_CACHED_DB=False): #TODO: UMich.db also put here?
+def prepare_database(raw_data_files, raw_data_folderpath, db_name, fold_enlarge_data=1, USE_CACHED_DB=False):
+
+    if os.path.exists(os.path.join(raw_data_folderpath, db_name)):
+        if USE_CACHED_DB:
+            log.info(db_name + " already exists!")
+            return
+        else:
+            os.remove(os.path.join(raw_data_folderpath, db_name))
 
     if fold_enlarge_data != 1:
-        # large_data_folderpath = LocalEnv.PATH_TO_CDSS + '/' + 'scripts/LabTestAnalysis/machine_learning' + '/' + large_data_foldername
         large_data_folderpath = raw_data_folderpath + '/' + 'enlarged_data_by_%s_fold'%str(fold_enlarge_data)
 
         if not os.path.exists(large_data_folderpath):
             os.mkdir(large_data_folderpath)
-        #TODO: by default, the first one should be labs
+
         large_data_files = [x.replace('sample','large') for x in raw_data_files]
         # Same file names, different folders
         utils_UMich.create_large_files(raw_data_files,raw_data_folderpath,
                                        large_data_files,large_data_folderpath,
-                                                     num_repeats=fold_enlarge_data)
+                                                     num_repeats=fold_enlarge_data, USE_CACHED_DB=USE_CACHED_DB)
         data_files = large_data_files
         data_folderpath = large_data_folderpath
     else:
         data_files = raw_data_files
         data_folderpath = raw_data_folderpath
-
-
-    if os.path.exists(os.path.join(raw_data_folderpath, db_name)):
-        if USE_CACHED_DB:
-            print db_name + " already exists!"
-            return
-        else:
-            os.remove(os.path.join(raw_data_folderpath, db_name))
 
     for data_file in data_files:
         utils_UMich.raw2db(data_file, data_folderpath, db_path=raw_data_folderpath, db_name=db_name, build_index_patid=True)
