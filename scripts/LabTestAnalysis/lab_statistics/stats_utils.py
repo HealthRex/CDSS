0e52cf1d2420a8b570a187c2ff2c18d01b42fdfa
sxu11 <song.xu.ucla@gmail.com>
2018-11-30 22:28:31 -0800

    Fix a typo and move details into utils

--- scripts/LabTestAnalysis/lab_statistics/stats_utils.py
+++ scripts/LabTestAnalysis/lab_statistics/stats_utils.py
@@ -6,6 +6,7 @@ from sklearn.metrics import roc_auc_score, roc_curve, precision_recall_curve, av
 import pandas as pd
 import numpy as np
 from scipy import stats
+import os
 
 def query_lab_usage__df(lab, time_start=None, time_end=None):
     # TODO: deal with lab_type == component?
@@ -192,3 +193,264 @@ def Hosmer_Lemeshow_Test(predict_probas, actual_labels, num_bins=10):
 
 
     return 1 - stats.chi2.cdf(H_stat, dof)
+
+
+def get_thres_by_fixing_PPV(lab, alg, data_folder='', PPV_wanted=0.9, thres_mode="from_test"):
+    if thres_mode == "from_train":
+        df = pd.read_csv(data_folder + '/' + lab + '/' + alg + '/' +
+                         '%s-normality-prediction-%s-direct-compare-results-traindata.csv' % (lab, alg))
+    else:
+        df = pd.read_csv(data_folder + '/' + lab + '/' + alg + '/' +
+                         '%s-normality-prediction-%s-direct-compare-results.csv' % (lab, alg))
+
+    # TODO: calibration?
+    row, col = df.shape
+    thres_last, PPV_last = 1., 1.
+    actual_list = df['actual'].values.tolist()
+
+    for thres in np.linspace(1, 0, num=1001):
+
+        df['predict_class'] = df['predict'].apply(lambda x: 1 if x > thres else 0)
+        predict_class_list = df['predict_class'].values.tolist()
+
+        true_positive = 0
+        false_positive = 0
+        true_negative = 0
+        false_negative = 0
+        for i in range(row):
+            if actual_list[i] == 1 and predict_class_list[i] == 1:
+                true_positive += 1
+            elif actual_list[i] == 0 and predict_class_list[i] == 1:
+                false_positive += 1
+            elif actual_list[i] == 1 and predict_class_list[i] == 0:
+                false_negative += 1
+            elif actual_list[i] == 0 and predict_class_list[i] == 0:
+                true_negative += 1
+            else:
+                print "what?!"
+
+        try:
+            PPV = float(true_positive) / float(true_positive + false_positive)
+        except ZeroDivisionError:
+            # PPV = float('nan')
+            continue
+
+        if PPV < PPV_wanted:
+            thres = thres_last
+            PPV = PPV_last
+            break
+        else:
+            thres_last = thres
+            PPV_last = PPV
+
+    return thres_last
+
+
+def bootstrap_CI(actual_list, predict_list, num_repeats=1000, stat='roc_auc',
+                 confident_lvl=0.95, side='two', random_state=0):
+    assert len(actual_list) == len(predict_list)
+
+    from sklearn.utils import resample
+
+    all_stats = []
+    for i in range(num_repeats):
+        actual_list_resampled, predict_list_resampled = resample(actual_list, predict_list)
+        if stat == 'roc_auc':
+            cur_roc_auc = roc_auc_score(actual_list_resampled, predict_list_resampled)
+            all_stats.append(cur_roc_auc)
+
+    roc_auc_left = np.percentile(all_stats, (1 - confident_lvl) / 2. * 100)
+    roc_auc_right = np.percentile(all_stats, (1 + confident_lvl) / 2. * 100)
+
+    return roc_auc_left, roc_auc_right
+
+
+def fill_df_fix_PPV(lab, alg, data_folder='', PPV_wanted=0.9, lab_type=None, thres_mode="from_test"):
+    df = pd.read_csv(data_folder + '/' + lab + '/' + alg + '/' +
+                     '%s-normality-prediction-%s-direct-compare-results.csv' % (lab, alg))
+    row, col = df.shape
+
+    '''
+    roc_auc, and its confidence interval (CI)
+    Independent of any threshold
+    '''
+    actual_list = df['actual'].values.tolist()
+
+    try:
+        roc_auc = roc_auc_score(actual_list, df['predict'].values)
+        roc_auc_left, roc_auc_right = bootstrap_CI(actual_list, df['predict'], confident_lvl=0.95)
+    except ValueError:
+        roc_auc, roc_auc_left, roc_auc_right = float('nan'), float('nan'), float('nan')
+
+    '''
+    Score threshold is used for "predict_proba --> predict_label"
+    Learning score threshold by fixing training PPV at desired lvl
+    This way, learned test (evaluation) PPV is "unbiased". 
+    '''
+    if thres_mode == "quick_test":
+        thres = 0.5
+    else:
+        thres = get_thres_by_fixing_PPV(lab, alg, data_folder=data_folder, PPV_wanted=PPV_wanted, thres_mode=thres_mode)
+
+    df['predict_class'] = df['predict'].apply(lambda x: 1 if x > thres else 0)
+    predict_class_list = df['predict_class'].values.tolist()
+
+    '''
+    Based on threshold, counting:
+    'true_positive', 'false_positive', 'true_negative', 'false_negative'
+
+    Calculating:
+    'sensitivity', 'specificity', 'LR_p', 'LR_n', 'PPV', 'NPV'
+    '''
+    true_positive = 0
+    false_positive = 0
+    true_negative = 0
+    false_negative = 0
+    total_cnt = 0
+    for i in range(row):
+        if actual_list[i] == 1 and predict_class_list[i] == 1:
+            true_positive += 1
+        elif actual_list[i] == 0 and predict_class_list[i] == 1:
+            false_positive += 1
+        elif actual_list[i] == 1 and predict_class_list[i] == 0:
+            false_negative += 1
+        elif actual_list[i] == 0 and predict_class_list[i] == 0:
+            true_negative += 1
+        else:
+            print "what?!"
+        total_cnt += 1
+
+    res_dict = {'lab': lab, 'alg': alg, 'threshold': thres,
+                'true_positive': true_positive / float(total_cnt), 'false_positive': false_positive / float(total_cnt),
+                'true_negative': true_negative / float(total_cnt), 'false_negative': false_negative / float(total_cnt),
+                'total_cnt': total_cnt, 'roc_auc': roc_auc,
+                '95%_CI': '[%f, %f]' % (roc_auc_left, roc_auc_right)}
+    res_dict['sensitivity'] = float(true_positive) / float(true_positive + false_negative)
+    res_dict['specificity'] = float(true_negative) / float(true_negative + false_positive)
+    try:
+        res_dict['LR_p'] = res_dict['sensitivity'] / (1 - res_dict['specificity'])
+    except ZeroDivisionError:
+        if res_dict['sensitivity'] == 0:
+            res_dict['LR_p'] = float('nan')
+        else:
+            res_dict['LR_p'] = float('inf')
+
+    try:
+        res_dict['LR_n'] = (1 - res_dict['sensitivity']) / res_dict['specificity']
+    except ZeroDivisionError:
+        if res_dict['sensitivity'] == 1:
+            res_dict['LR_n'] = float('nan')
+        else:
+            res_dict['LR_n'] = float('inf')
+
+    try:
+        res_dict['PPV'] = float(true_positive) / float(true_positive + false_positive)
+    except ZeroDivisionError:
+        res_dict['PPV'] = float('nan')
+
+    try:
+        res_dict['NPV'] = float(true_negative) / float(true_negative + false_negative)
+    except ZeroDivisionError:
+        res_dict['NPV'] = float('nan')
+
+    return res_dict
+
+
+def add_panel_cnts_fees(one_lab_alg_dict):
+    panel = one_lab_alg_dict['lab']
+    df_cnts_fees = pd.read_csv('data_summary_stats/labs_charges_volumes.csv')
+    cnts_fees_dict = df_cnts_fees.ix[df_cnts_fees['name'] == panel, ['count',
+                                                                     'min_price',
+                                                                     'max_price',
+                                                                     'mean_price',
+                                                                     'median_price',
+                                                                     'min_volume_charge',
+                                                                     'max_volume_charge',
+                                                                     'mean_volume_charge',
+                                                                     'median_volume_charge']].to_dict(orient='list')
+    # print cnts_fees_dict
+    for key in cnts_fees_dict.keys():
+        try:
+            cnts_fees_dict[key] = cnts_fees_dict[key][0]
+        except IndexError:
+            cnts_fees_dict[key] = float('nan')
+
+    one_lab_alg_dict.update(cnts_fees_dict)
+    return one_lab_alg_dict
+
+
+'''
+For individual component, there is no well-defined "fee".
+'''
+
+
+def add_component_cnts(one_lab_dict, years):
+    component = one_lab_dict['lab']
+    df_cnts = pd.read_csv('data_summary_stats/component_cnts.txt', sep='\t', keep_default_na=False)
+    # df = df.rename(columns={'Base':'lab'})
+
+    for year in years:
+        one_lab_dict[str(year) + '_Vol'] = df_cnts.ix[df_cnts['Base'] == component, str(year)].values[0]
+        # TODO: rename total_cnt to avoid confusion between total STRIDE cnt & testing cnt!
+
+    return one_lab_dict
+
+
+def get_baseline(file_path):
+    # try:
+    df = pd.read_csv(file_path + 'baseline_comparisons.csv')  # TODO: baseline file should not have index!
+    # except IOError:
+    #     from medinfo.dataconversion.FeatureMatrixFactory import FeatureMatrixFactory
+    #     f = FeatureMatrixFactory()
+    #     f.obtain_baseline_results(raw_matrix_path=file_path, random_state=123456789, isLabPanel=True)
+    #     df = pd.read_csv(file_path + 'baseline_comparisons.csv')
+
+    try:
+        res = roc_auc_score(df['actual'], df['predict'])
+    except ValueError:
+        res = float('nan')
+
+    return res
+
+
+'''
+For each lab at each (train_PPV, vital_day), 
+write all stats (e.g. roc_auc, PPV, total cnts) into csv file. 
+'''
+
+
+def lab2stats_csv(lab_type, lab, years, all_algs, PPV_wanted, vital_day,
+                  folder_path, data_folder, result_folder, columns,
+                  thres_mode="from_test"):
+    if thres_mode == "from_train":
+        curr_res_file = '%s-alg-summary-trainPPV-%s-vitalDays-%d.csv' % (lab, str(PPV_wanted), vital_day)
+    elif thres_mode == "from_test":
+        curr_res_file = '%s-alg-summary-testPPV-%s-vitalDays-%d.csv' % (lab, str(PPV_wanted), vital_day)
+
+    if os.path.exists(result_folder + curr_res_file):
+        return
+
+    df = pd.DataFrame(columns=columns)
+
+    baseline_roc_auc = get_baseline(file_path=folder_path + '/' + data_folder + '/' + lab + '/')
+
+    for alg in all_algs:
+        print 'Processing lab %s with alg %s' % (lab, alg)
+        # try:
+        one_lab_alg_dict = fill_df_fix_PPV(lab, alg, data_folder=folder_path + '/' + data_folder,
+                                           PPV_wanted=PPV_wanted, lab_type=lab_type, thres_mode=thres_mode)
+        if lab_type == 'component':
+            one_lab_alg_dict = add_component_cnts(one_lab_alg_dict, years=years)
+        elif lab_type == 'panel':
+            one_lab_alg_dict = add_panel_cnts_fees(one_lab_alg_dict)
+
+        one_lab_alg_dict['baseline_roc'] = baseline_roc_auc
+
+        df = df.append(one_lab_alg_dict, ignore_index=True)
+        # except Exception as e:
+        #     print e
+        #     pass
+
+    # print 'PPV_wanted=%.2f finished!' % PPV_wanted #TODO
+
+    df[columns].to_csv(result_folder + curr_res_file, index=False)
