2595e08b03c5c988ac40e0e307177f2ce8b492f3
Song Xu <song.xu.ucla@gmail.com>
2019-01-14 08:50:28 -0800

    New get baseline and test

---
+++ scripts/LabTestAnalysis/machine_learning/ml_utils.py
@@ -0,0 +1,33 @@
+
+
+'''
+
+Only operational functions
+Not aware of file system organization
+
+'''
+import pandas as pd
+
+def test_get_baseline():
+    df_1 = pd.DataFrame([[123, '2014-01-01', 1, 2]], columns=['pat_id', 'order_time', 'actual', 'predict'])
+    df_2 = pd.DataFrame([[456, '2015-01-01', 3]], columns=['pat_id', 'order_time', 'actual'])
+    print get_baseline(df_1, df_2)
+    # assert df's not changed
+
+
+def get_baseline(df_train, df_test, y_true_label='actual', y_pred_label='predict'):
+    df_res = df_test[['pat_id', 'order_time', y_true_label]].copy() # Not change the input
+
+    prevalence = float(df_train[y_true_label].values.sum()) / float(df_train.shape[0])
+
+    df_res[y_pred_label] = df_res[y_true_label].apply(lambda x: prevalence) # use any column to create an extra column
+
+    df_res = df_res.sort_values(['pat_id', 'order_time'])
+    for i in range(1, df_res.shape[0]):
+        if df_res.ix[i-1, 'pat_id'] == df_res.ix[i, 'pat_id']:
+            df_res.ix[i, y_pred_label] = df_res.ix[i-1, y_true_label]
+
+    return df_res[[y_true_label, y_pred_label]]
+
+if __name__ == '__main__':
+    test_get_baseline()
