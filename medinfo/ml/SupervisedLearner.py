61572880d83423411af7a621ab1eca65bfc34772
Song Xu <song.xu.ucla@gmail.com>
2019-01-16 14:23:12 -0800

    process_matrix run through

--- medinfo/ml/SupervisedLearner.py
+++ medinfo/ml/SupervisedLearner.py
@@ -55,6 +55,7 @@ TODO: Specific pipelines do not "override" this singleton class, but calls its f
 import LocalEnv
 import os
 
+from medinfo.ml.FeatureSelector import FeatureSelector
 import pandas as pd
 pd.set_option('display.width', 3000)
 pd.set_option('display.max_columns', 3000)
@@ -210,16 +211,24 @@ def get_train_and_evalu_processed_matrices(lab, data_lab_folderpath, features, r
     processed_matrix_evalu_filepath = os.path.join(data_lab_folderpath, processed_matrix_evalu_template % lab)
 
     if os.path.exists(processed_matrix_train_filepath) and os.path.exists(processed_matrix_evalu_filepath):
+        processed_matrix_train = pd.read_csv(processed_matrix_train_filepath, keep_default_na=False)
+        processed_matrix_evalu = pd.read_csv(processed_matrix_evalu_filepath, keep_default_na=False)
         pass
 
-    raw_matrix_train, raw_matrix_evalu = get_train_and_evalu_raw_matrices(lab, data_lab_folderpath, random_state)
+    else:
+        raw_matrix_train, raw_matrix_evalu = get_train_and_evalu_raw_matrices(lab, data_lab_folderpath, random_state)
+        assert raw_matrix_train.shape[0] > raw_matrix_evalu.shape[0]
+        assert raw_matrix_train.shape[1] == raw_matrix_evalu.shape[1]
+        '''
+        
+        '''
 
-    '''
-    
-    '''
+        processed_matrix_train, impute_template  = process_matrix(raw_matrix_train, features=features)
+        processed_matrix_evalu, _ = process_matrix(raw_matrix_evalu, features=features, impute_template=impute_template)
 
-    processed_matrix_train, process_template  = process_matrix(raw_matrix_train, features=features)
-    processed_matrix_evalu = process_matrix(raw_matrix_evalu, features=features, process_template=process_template)
+        processed_matrix_train.to_csv(processed_matrix_train_filepath, sep='\t', index=False)
+        processed_matrix_evalu.to_csv(processed_matrix_evalu_filepath, sep='\t', index=False)
+
     return processed_matrix_train, processed_matrix_evalu
 
 
@@ -229,22 +238,20 @@ def impute_features(matrix, strategy):
     impute_template = {}
 
     features_to_remove = {}
-    for feature in matrix.columns.values:
-        # if feature in self._removed_features:
-        #     continue
-        # TODO _removed_features?
 
-        # If all values are null, just remove the feature.
-        # Otherwise, imputation will fail (there's no mean value),
-        # and sklearn will ragequit.
+    for feature in matrix.columns.values:
+        '''
+        If all values are null, just remove the feature.
+        Otherwise, imputation will fail (there's no mean value),
+        and sklearn will ragequit.
+        '''
         if matrix[feature].isnull().all():
-            # remove_feature(feature)
-            # self._removed_features.append(feature)
             features_to_remove.append(feature)
 
-        # Only try to impute if some of the values are null.
-        # elif matrix[feature].isnull().any():
         else:
+            '''
+            Only try to impute if some of the values are null.
+            '''
             #TODO: other strategies
             if matrix[feature].dtype not in (int, long, float):
                 print feature, matrix[feature].dtype
@@ -253,7 +260,7 @@ def impute_features(matrix, strategy):
             imputed_value = matrix[feature].mean()
             impute_template[feature] = imputed_value
 
-            matrix[feature].fillna(imputed_value)
+            matrix[feature].fillna(imputed_value, inplace=True)
 
     matrix = matrix.drop(features_to_remove, axis=1)
     # TODO: assert all existing features have impute values
@@ -261,9 +268,30 @@ def impute_features(matrix, strategy):
     return matrix, impute_template
 
 
-def select_features(matrix, strategy):\
-    # TODO: fill in
-    return matrix
+
+def select_features(matrix, features, random_state=0):
+    select_params = features['select_params']
+    fs = FeatureSelector(problem = select_params['selection_problem'],
+                         algorithm = select_params['selection_algorithm'],
+                         random_state = random_state
+                         )
+
+    X, y = split_Xy(matrix, features['ylabel'])
+    fs.set_input_matrix(X, y)
+    num_features_to_select = int(select_params['percent_features_to_select'] * len(matrix.columns.values))
+
+    fs.select(k=num_features_to_select)
+
+    feature_ranks = fs.compute_ranks()
+
+    features_to_keep = []
+    for i in range(len(feature_ranks)):
+        if feature_ranks[i] <= num_features_to_select:
+            # If in features_to_keep, pretend it wasn't eliminated.
+            # self._eliminated_features.append(self._X_train.columns[i])
+            features_to_keep.append(X.columns[i])
+
+    return matrix[features_to_keep].copy()
 
 def process_matrix(raw_matrix, features, data_path='', impute_template=None):
     '''
@@ -301,7 +329,7 @@ def process_matrix(raw_matrix, features, data_path='', impute_template=None):
     Set aside ylabel and info features 
     '''
 
-    features_setaside = [features['ylabel']] + features['info']
+    features_setaside = features['info']
     processed_matrix = processing_matrix[features_setaside].copy()
     processing_matrix = processing_matrix.drop(features_setaside, axis=1)
 
@@ -320,7 +348,9 @@ def process_matrix(raw_matrix, features, data_path='', impute_template=None):
         # if 'abnormal_panel' not in processing_matrix.columns.values.tolist(): #TODO: delete in the future
         #     processing_matrix['abnormal_panel'] = processing_matrix['all_components_normal'].apply(lambda x:1.-x)
 
+        print 'processing_matrix.shape:', processing_matrix.shape
         processing_matrix = processing_matrix[columns_impute_ordered]
+        print 'processing_matrix.shape:', processing_matrix.shape
         # TODO: header info like done by fm_io?
 
     else:
@@ -337,7 +367,6 @@ def process_matrix(raw_matrix, features, data_path='', impute_template=None):
         Impute
         '''
 
-        
         processing_matrix, impute_template = impute_features(processing_matrix, strategy="mean")
 
         # TODO: keep order?
@@ -345,184 +374,33 @@ def process_matrix(raw_matrix, features, data_path='', impute_template=None):
         '''
         Keep features
         '''
-        processed_matrix = pd.concat([processed_matrix, processing_matrix[features['keep']].copy()], axis=1) # or concat?
-        processing_matrix = processing_matrix.drop(features['keep'], axis=1)
+        keep_features = features['keep']
+        processed_matrix = pd.concat([processed_matrix, processing_matrix[keep_features].copy()], axis=1) # or concat?
+        processing_matrix = processing_matrix.drop(keep_features, axis=1)
 
 
         '''
         Select features
         '''
-        processing_matrix = select_features(processing_matrix)
+        processing_matrix = select_features(processing_matrix,
+                                            features = features)
+        '''
+        Record the order of the feature in the numeric matrix, 
+        as the dictionary impute_template does not keep order
+        '''
+
+        for key, val in impute_template.items():
+            if key not in processing_matrix.columns:
+                impute_template.pop(key)
+            else:
+                feature_ind = processing_matrix.columns.tolist().index(key)
+                impute_template[key] = (feature_ind, impute_template[key])
         # processed_matrix = pd.merge(processed_matrix, processing_matrix)
 
-    processed_matrix = pd.concat([processed_matrix, processing_matrix[columns_impute_ordered]], axis=1)
+    processed_matrix = pd.concat([processing_matrix, processed_matrix], axis=1)
 
     return processed_matrix, impute_template
 
-
-
-
-def train_ml_model(X_train, y_train, lab, alg):
-    # TODO: How to easily include SVM, Xgboost, and Keras?
-    ml_models = {}  # key: (lab,alg), value: model
-
-    model = train(X_train, y_train, alg)
-
-    ml_models[(lab,alg)] = model
-
-    return ml_model
-
-
-def load_ml_model(lab, ml_alg, data_folder):
-    ml_model_filename = '%s-normality-%s-model.pkl'%(lab, ml_alg)
-    predictor_path = os.path.join(data_folder, lab, ml_model_filename) # TODO
-    return joblib.load(predictor_path)
-
-
-def calibrate_ml_model(ml_model, X_cali, y_cali, cali_method='isotonic'):
-    model_isotonic = CalibratedClassifierCV(ml_model, cv='prefit', method=cali_method)
-    model_isotonic.fit(X=X_cali, y=y_cali)
-    return model_isotonic
-
-def predict(lab, ml_classifier, X, y, data_folder):
-    pass
-
-
-############# Main functions #############
-
-
-
-
-
-############# Other functions #############
-
-
-
-
-def load_raw_matrix(lab, lab_folder, file_name=None):
-    fm_io = FeatureMatrixIO()
-
-    if not file_name:
-        file_name = '%s-normality-matrix-raw.tab'%lab
-
-
-    raw_matrix_path = os.path.join(lab_folder, lab, file_name)
-
-    raw_matrix = fm_io.read_file_to_data_frame(raw_matrix_path)
-    return raw_matrix
-
-    # processed_matrix_filename = "%s-normality-matrix-10000-episodes-raw.tab"%lab
-
-    # processed_matrix = fm_io.read_file_to_data_frame(os.path.join(lab_folder, lab, processed_matrix_filename))
-
-def get_processed_matrix(lab,
-                         lab_folder=
-                         os.path.join(LocalEnv.PATH_TO_CDSS,
-                                      'scripts/LabTestAnalysis/machine_learning/data-panels')):
-    '''
-    Create processed matrix anyway (do not try to load). Because this step is fast. TODO
-
-    :param lab:
-    :param lab_folder:
-    :return:
-    '''
-
-    processed_matrix_filename = "%s-normality-matrix-processed.tab"%lab
-
-
-
-############# Other functions #############
-
-
-
-
-
-
-############# Compatible functions for previous results #############
-
-def load_process_template(lab, non_impute_features, data_folder):
-    raw_matrix_filename = '%s-normality-matrix-raw.tab' % lab
-    raw_matrix_path = os.path.join(data_folder, lab, raw_matrix_filename)
-
-    processed_matrix_filename = '%s-normality-matrix-processed.tab' % lab
-    processed_matrix_path = os.path.join(data_folder, lab, processed_matrix_filename)
-
-    fm_io = FeatureMatrixIO() # TODO: use def get_raw_matrix instead
-    raw_matrix = fm_io.read_file_to_data_frame(raw_matrix_path)
-
-    raw_features_all = raw_matrix.columns.values.tolist()
-    numeric_features = [x for x in raw_features_all if x not in non_impute_features]
-
-
-    processed_matrix = fm_io.read_file_to_data_frame(processed_matrix_path)
-
-    final_features = processed_matrix.columns.values.tolist()
-
-    process_template = {}
-    ind_feature = 0
-    for feature in final_features:
-        if feature in numeric_features:
-            imputed_value = raw_matrix[feature].mean()
-            process_template[feature] = (ind_feature, imputed_value)
-            ind_feature += 1
-
-    return process_template
-
-
-def get_process_template(feat2imputed_path):
-    import pickle
-    feat2imputed_dict = pickle.load(open(feat2imputed_path, 'r'))
-    return feat2imputed_dict
-
-############# Compatible functions for previous results #############
-
-
-
-
-
-
-
-
-
-def pipelining(source_set_folder, labs, source_type, source_ids):
-    '''
-    This function aims at gluing several steps.
-
-    The full steps are:
-        Empty (data in SQL) -> raw matrix -> processed matrix
-        -> processed train, processed test -> trained models
-        -> results and reports
-
-    Args:
-        source_set_folder:
-        labs:
-        source_type:
-
-    Returns:
-
-    '''
-    status = 'ready'
-    assert os.path.exists(source_set_folder)
-
-    if source_type == 'raw-matrix':
-        # TODO:
-        # get one new lab's raw matrix
-        # get one old lab's model
-        # run it through the model
-
-
-        for lab in labs:
-            cur_folder = os.path.join(source_set_folder,
-                                      lab)
-            cur_files = os.listdir(cur_folder)
-            for cur_file in cur_files:
-                if 'raw' in cur_file and all(x in cur_file for x in source_ids):
-                    print cur_file
-
-            quit()
-
-
-    return status
 
 
 def standard_pipeline(lab, algs, learner_params, data_lab_folderpath, random_state):
@@ -630,154 +508,9 @@ def standard_pipeline(lab, algs, learner_params, data_lab_folderpath, random_sta
     #                              random_state=random_state)
 
 
-def output_result(X_test, y_test, model_src, res_folderpath):
-    y_pred_prob = model_src.predict_probability(X_test)[:,1]
-
-    direct_comparisons = pd.DataFrame({'actual': y_test.values.flatten(), 'predict': y_pred_prob})
-    direct_comparisons.to_csv(res_folderpath)
-
-def obtain_baseline_results(train_matrix, test_matrix, dst_filepath='', isLabPanel=True):
-    # for episode_dict in self.getPatientEpisodeIterator():
-    #     episode_dict
-
-    # Step1: group by pat_id
-    # Step2: For each group, obtain predicts
-    #   Step 2.1: order by order_time
-    #   Step 2.2: Obtain
-
-    actual_cnt_1 = 0
-    actual_cnt_0 = 0
-
-    train_labels = train_matrix['all_components_normal'].values
-
-    # Calc the prevalence from training data
-    prevalence_1 = float(sum(train_labels))/float(len(train_labels))
-
-    raw_matrix = test_matrix
-    episode_cnt = raw_matrix.shape[0]
-
-    if isLabPanel:
-        ylabel = 'all_components_normal'
-    else:
-        ylabel = 'component_normal'
-
-    # raw_matrix = raw_matrix.rename(columns={'component_normal':'all_components_normal'})
-
-    raw_matrix_dict = raw_matrix[['pat_id', 'order_time', ylabel]].to_dict('records')
-
-    # for _ in self.getPatientEpisodeIterator(): # less stupid way to do this
-    #     episode_cnt += 1
-
-
-
-    episode_groups_dict = {}  # pat_id: [episode_dicts]
-    for episode_dict in raw_matrix_dict:
-        if episode_dict['pat_id'] in episode_groups_dict:
-            episode_groups_dict[episode_dict['pat_id']].append(episode_dict)
-        else:
-            episode_groups_dict[episode_dict['pat_id']] = [episode_dict]
-
-
-
-
-    baseline_comparisons = pd.DataFrame(columns=['actual', 'predict'])
-
-    for pat_id in episode_groups_dict:
-        #   Step 2.1: order by order_time
-        newlist = sorted(episode_groups_dict[pat_id], key=lambda k: k['order_time'])
-
-        newlist[0]['predict'] = prevalence_1
-        baseline_comparisons = baseline_comparisons.append({'actual':newlist[0][ylabel],
-                                     'predict':newlist[0]['predict']}, ignore_index=True)
-
-        for i in range(1,len(newlist)):
-            newlist[i]['predict'] = newlist[i-1][ylabel]
-            baseline_comparisons = baseline_comparisons.append({'actual': newlist[i][ylabel],
-                                         'predict': newlist[i]['predict']}, ignore_index=True)
-
-    baseline_comparisons.to_csv(dst_filepath)
-
-def main_different_testsets():
-    '''
-    Apply trained model on another test set.
-
-    Returns:
-
-    '''
-    main_folder = os.path.join(LocalEnv.PATH_TO_CDSS, 'scripts/LabTestAnalysis/machine_learning/')
-
-    src_foldername = "data-panels-10000-episodes"
-    dst_foldername = "data-panels-5000-holdout"
-
-    src_folderpath = os.path.join(main_folder, src_foldername)
-    dst_folderpath = os.path.join(main_folder, dst_foldername)
-
-    from scripts.LabTestAnalysis.machine_learning.LabNormalityPredictionPipeline import NON_PANEL_TESTS_WITH_GT_500_ORDERS
-
-    for lab in NON_PANEL_TESTS_WITH_GT_500_ORDERS:
-
-        feat2imputed_path = os.path.join(src_folderpath, lab, 'feat2imputed_dict.csv')
-        template_src = pd.read_csv(feat2imputed_path, keep_default_na=False)#get_process_template(feat2imputed_path)
-
-        # TODO: such info should be hidden in the function?
-
-        raw_matrix_src = load_raw_matrix(lab, src_folderpath)
-
-        #
-        raw_matrix_dst = load_raw_matrix(lab, dst_folderpath)
-
-        raw_matrix_1, raw_matrix_2 = split_rows(raw_matrix_dst, fraction=0.5)
-        raw_matrices = [raw_matrix_1, raw_matrix_2]
-
-        for ml_alg in SupervisedClassifier.SUPPORTED_ALGORITHMS:  # TODO
-            model_src = load_ml_model(lab, ml_alg, src_folderpath)
-
-
-
-            res_foldername_template = "results-from-panels-10000-to-panels-5000-part-%i/"
-            for ind, raw_matrix_dst in enumerate(raw_matrices):
-                res_foldername = res_foldername_template%(ind+1)
-                res_folderpath = os.path.join(main_folder, res_foldername)
-
-                baseline_filename = 'baseline_comparisons.csv'
-                baseline_filepath = os.path.join(res_folderpath, lab, baseline_filename)
-                # obtain_baseline_results(raw_matrix_src, raw_matrix_dst, baseline_filepath)
-
-
-                if not os.path.exists(res_folderpath):
-                    os.mkdir(res_folderpath)
-
-                if not os.path.exists(os.path.join(res_folderpath, lab)):
-                    os.mkdir(os.path.join(res_folderpath, lab))
-
-                feat2imputed = template_src.to_dict()
-                for key in feat2imputed:
-                    feat2imputed[key] = feat2imputed[key][0]
-
-                raw_matrix_dst_filepath = os.path.join(res_folderpath, lab, '%s-normality-test-matrix-raw.tab' % lab)
-                # raw_matrix_dst.to_csv(raw_matrix_dst_filepath, index=False)
-
-
-
-                processed_matrix_dst = raw_matrix_dst[template_src.columns.tolist()+['pat_id']].fillna(feat2imputed)
-
-                processed_matrix_dst_filepath = os.path.join(res_folderpath, lab, '%s-normality-test-matrix-processed.tab'%lab)
-                processed_matrix_dst.to_csv(processed_matrix_dst_filepath, index=False) # TODO: use fm_io
-
-                processed_matrix_dst.pop('pat_id')
-                continue
-
-                X_test, y_test = split_Xy(processed_matrix_dst, outcome_label='all_components_normal')
-
-                output_filename = 'direct_comparisons.csv'
-                output_filepath = os.path.join(res_folderpath, lab, ml_alg, output_filename)
-                if not os.path.exists(os.path.join(res_folderpath, lab, ml_alg)):
-                    os.mkdir(os.path.join(res_folderpath, lab, ml_alg))
-                output_result(X_test, y_test, model_src, output_filepath)
-
 
 
 
 if __name__ == '__main__':
-    main_different_testsets()
+    pass
 
