e0400a07349bf85fad5c1cff2c35d9becd20e1c0
Song Xu <song.xu.ucla@gmail.com>
2019-01-17 09:45:20 -0800

    Revert this safety (potentially useful for the future?) check for now to avoid causing trouble in the setting up

--- medinfo/db/test/Util.py
+++ medinfo/db/test/Util.py
@@ -36,8 +36,8 @@ class DBTestCase(MedInfoTestCase):
         """
         MedInfoTestCase.setUp(self)
 
-        if medinfo.db.Env.DB_PARAM["DSN"] == medinfo.db.Env.TEST_DB_PARAM["DSN"]:
-            quit("Please use different names for production and test databases!")
+        # if medinfo.db.Env.DB_PARAM["DSN"] == medinfo.db.Env.TEST_DB_PARAM["DSN"]:
+        #     quit("Please use different names for production and test databases!")
 
         # Override default DB connection params to test DB, but retain links to original
         self.orig_DB_PARAM = dict(medinfo.db.Env.DB_PARAM);
