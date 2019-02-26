345e0ee10857402f96bfc81902a20de5a5795087
Jonathan Chiang <chiang810@gmail.com>
2019-02-26 07:42:12 -0800

    Update auth_quiver.py

--- scripts/DevWorkshop/GoogleCloudPlatform/auth_quiver.py
+++ scripts/DevWorkshop/GoogleCloudPlatform/auth_quiver.py
@@ -1,18 +1,7 @@
-# quiver_auth
 import pandas as pd
-import pyarrow.parquet as pq
-import pyarrow as pa
-sync_environemnt  = "/Users/jonc101/Box Sync/Jonathan Chiang's Files/mining-clinical-decisions/"
+#import pyarrow.parquet as pq
+#import pyarrow as pa
+sync_environment  = "/Users/jonc101/Box Sync/Jonathan Chiang's Files/mining-clinical-decisions/devworkshop"
+gcp = pd.read_csv("/Users/jonc101/Box Sync/Jonathan Chiang's Files/mining-clinical-decisions/devworkshop/dev_chiang.csv")
+print(gcp.head)
 
-#gcp = pd.read_csv("/Users/jonc101/Box Sync/Jonathan Chiang's Files/mining-clinical-decisions/gcp.csv")
-#print(gcp.head)
-
-#gcp_parquet = gcp.to_parquet
-#print(gcp_parquet)
-#table = pa.Table.from_pandas(gcp)
-#pq.write_table(table, sync_environemnt + 'gcp.parquet')
-
-table2 = pq.read_table(sync_environemnt + 'gcp.parquet')
-
-
-print(table2.to_pandas())
