abe9c5abd3f17fef3eb4e10544606ee846a5865d
Song Xu <song.xu.ucla@gmail.com>
2019-03-07 14:48:20 -0800

    Feature Remover

---
+++ scripts/LabTestAnalysis/machine_learning/LabNormalityLearner_Config.py
@@ -0,0 +1,19 @@
+
+
+features_to_remove = [
+                'pat_id', 'order_time', 'order_proc_id',
+                'Birth.pre',
+                'Male.preTimeDays', 'Female.preTimeDays',
+                'RaceWhiteHispanicLatino.preTimeDays',
+                'RaceWhiteNonHispanicLatino.preTimeDays',
+                'RaceHispanicLatino.preTimeDays',
+                'RaceAsian.preTimeDays',
+                'RaceBlack.preTimeDays',
+                'RacePacificIslander.preTimeDays',
+                'RaceNativeAmerican.preTimeDays',
+                'RaceOther.preTimeDays',
+                'RaceUnknown.preTimeDays',
+                'Death.post',
+                'Death.postTimeDays'
+            ]
+
