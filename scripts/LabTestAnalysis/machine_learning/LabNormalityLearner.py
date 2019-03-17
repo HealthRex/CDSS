4b112d90947eef4b69315485593771668b9ca5cb
Song Xu <song.xu.ucla@gmail.com>
2019-03-17 12:39:55 -0700

    Use lab (with lab_type, data_source, version) as unit of File Organizer

--- scripts/LabTestAnalysis/machine_learning/LabNormalityLearner.py
+++ scripts/LabTestAnalysis/machine_learning/LabNormalityLearner.py
@@ -9,8 +9,10 @@ Keep this file as simple as possible.
 '''
 
 import pandas as pd
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 500)
+pd.set_option('display.max_columns', 1000)
+pd.set_option('display.width', 1000)
+
+import logging
 
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
@@ -29,38 +31,34 @@ def run_one_lab_local(lab, lab_type, data_source, version):
 
     # X_train_raw, y_train = [[1], [2]], [1, 2]
     # X_test_raw, y_test = [[3], [4]], [3, 4]
-
-    file_organizer = LNL_sys.FileOrganizerLocal(lab_type=lab_type,
-                                                 data_source=data_source,
+    file_organizer = LNL_sys.FileOrganizerLocal(lab=lab,
+                                                lab_type=lab_type,
+                                                data_source=data_source,
                                                 version=version)
 
-    Xy_raw = file_organizer.get_raw_matrix(lab)
+    raw_matrix = file_organizer.get_raw_matrix()
 
-    X_train_raw, y_train = Utils.Split_Xy(Xy_raw)
+    X_train_raw, y_train = Utils.Split_Xy(raw_matrix)
 
-    pipeline = Pipeline(memory=LNL_sys.get_cached_pipeline_filepath(lab=lab,
-                                                                    lab_type=lab_type,
-                                                                    data_source=data_source,
-                                                                    version=version),
-                        steps=
-                            [
-                                ('feature_engineering', Pipeline
-                                    (
-                                        [
-                                            ('remove_features', Cls.FeatureRemover()),
-                                            ('impute_features', Cls.FeatureImputer()),
-                                            ('select_features', Cls.Select_Features())
-                                        ]
-                                    )
-                                 ),
-                                ('machine_learning', Pipeline
-                                    (
-                                         [
-                                             ('svc', LinearSVC())
-                                         ]
-                                    )
-                                 )
-                            ]
+    pipeline = Pipeline(memory = file_organizer.cached_pipeline_filepath,
+                        steps = [
+                                    ('feature_engineering', Pipeline
+                                        (
+                                            [
+                                                ('remove_features', Cls.FeatureRemover()),
+                                                ('impute_features', Cls.FeatureImputer()),
+                                                ('select_features', Cls.Select_Features())
+                                            ]
+                                        )
+                                     ),
+                                    ('machine_learning', Pipeline
+                                        (
+                                             [
+                                                 ('svc', LinearSVC())
+                                             ]
+                                        )
+                                     )
+                                ]
     )
 
     pipeline.set_params()
@@ -83,7 +81,9 @@ if __name__ == '__main__':
     data_source = 'Stanford'
     version = '10000-episodes-lastnormal'
 
+    logging.getLogger().setLevel(logging.INFO)
+
     run_one_lab_local(lab=lab,
-            lab_type=lab_type,
-            data_source=data_source,
-            version=version)
+                      lab_type=lab_type,
+                      data_source=data_source,
+                      version=version)
--- scripts/LabTestAnalysis/machine_learning/LabNormalityLearner_System.py
+++ scripts/LabTestAnalysis/machine_learning/LabNormalityLearner_System.py
@@ -8,9 +8,12 @@ Takes care where raw, process, intermediate, and final output store.
 
 import LocalEnv
 import os
+import logging
 from medinfo.dataconversion.FeatureMatrixIO import FeatureMatrixIO
 project_folderpath = os.path.join(LocalEnv.PATH_TO_CDSS,
                                   'scripts/LabTestAnalysis')
+machine_learning_folderpath = os.path.join(project_folderpath,
+                                           'machine_learning',)
 
 class FileOrganizerRemote():
     def __init__(self, lab_type, data_source_train, data_source_test, version):
@@ -34,28 +37,48 @@ class FileOrganizerRemote():
 
 
 class FileOrganizerLocal():
-    def __init__(self, lab_type, data_source, version):
+    '''
+    Structure:
+    machine_learning / ml_dataset_folder / ml_lab_folder / cached pipeline
+
+    '''
+
+    def __init__(self, lab, lab_type, data_source, version):
+        self.lab = lab
         self.lab_type = lab_type
         self.data_source = data_source
         self.version = version
 
-        self.machine_learning_folderpath = os.path.join(project_folderpath,
-                                                        'machine_learning',
-                                                        )
-        self.dataset_foldername = 'data-%s-%s-%s' % (data_source,
-                                                     lab_type,
-                                                     version
-                                                     )
+        self.cur_tag = '%s-%s-%s'% (data_source, lab_type, version)
+
+
+        self.dataset_foldername = 'data-%s' % self.cur_tag
+        self.ml_dataset_folderpath = os.path.join(machine_learning_folderpath,
+                                               self.dataset_foldername)
+        if not os.path.exists(self.ml_dataset_folderpath):
+            logging.info('Path %s not existing, creating...' % self.ml_dataset_folderpath)
+            os.mkdir(self.ml_dataset_folderpath)
+        else:
+            logging.info('Path %s exists...' % self.ml_dataset_folderpath)
 
         self.raw_matrix_filename_template = '%s-normality-matrix-raw.tab'
 
-    def get_raw_matrix(self, lab, data_label):
-        raw_matrix_filepath = os.path.join(self.machine_learning_folderpath,
-                                           lab,
-                                           self.raw_matrix_filename_template % lab
-                                          )
-        return FeatureMatrixIO().read_file_to_data_frame(raw_matrix_filepath)
+        self.cached_pipeline_filename = 'pipeline_memory_%s' % self.cur_tag
 
 
-    def get_cached_pipeline_filepath(self, lab):
-        return
+        self.ml_lab_folderpath = os.path.join(self.ml_dataset_folderpath, lab)
+        if not os.path.exists(self.ml_lab_folderpath):
+            logging.info('Path %s not existing, creating...' % self.ml_lab_folderpath)
+            os.mkdir(self.ml_lab_folderpath)
+        else:
+            logging.info('Path %s exists...' % self.ml_lab_folderpath)
+
+        self.raw_matrix_filepath = os.path.join(self.ml_lab_folderpath,
+                                           self.raw_matrix_filename_template % self.lab)
+
+        self.cached_pipeline_filepath = os.path.join(self.ml_lab_folderpath,
+                                                     self.cached_pipeline_filename)
+
+    def get_raw_matrix(self):
+        return FeatureMatrixIO().read_file_to_data_frame(self.raw_matrix_filepath)
+
