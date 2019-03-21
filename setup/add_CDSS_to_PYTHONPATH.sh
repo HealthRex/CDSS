6deb972bf6796ccbfbbe78bfa02b87e176f7246e
Jonathan H Chen <jonc101@gmail.com>
2019-03-20 22:09:17 -0700

    Remove unused EventDigraph module that is causing unit test case crashes due to uninstalled dependencies that aren't being used elsewhere anyway (graph_tool) TestFeatureMatrixFactory_UMich.py still causing crashes in batch tests because out of order DB connector type assignment. Should make DBUtil more robust to handle multiple connection types, even within same runtime.

--- setup/add_CDSS_to_PYTHONPATH.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Adding ~/healthrex/CDSS/ to PYTHONPATH..."
-export PYTHONPATH=${PYTHONPATH}:~/healthrex/CDSS
-printf 'export PYTHONPATH=${PYTHONPATH}:~/healthrex/CDSS\n' >> ~/.bashrc
-printf 'export PYTHONPATH=${PYTHONPATH}:~/healthrex/CDSS\n' >> ~/.bash_profile
