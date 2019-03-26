1c9a3f296392ae95f656822d4f2e8449c37210b1
Song Xu <song.xu.ucla@gmail.com>
2019-03-25 19:39:11 -0700

    Initial specialty-based prediction with spec@10=13.3%

--- scripts/OutpatientReferral/first_implementation.py
+++ scripts/OutpatientReferral/first_implementation.py
@@ -89,6 +89,9 @@ def train_model(k=10, start_col='referral'): #
     return my_dict
 
 def prec_at_k(actuals, predicts, k=10): # TODO: how to use k
+    if len(predicts)==0 and len(actuals)==0:
+        return float('nan')
+
     num_relevant = 0
     for predict in predicts:
         if predict in actuals:
@@ -96,124 +99,127 @@ def prec_at_k(actuals, predicts, k=10): # TODO: how to use k
     prec = float(num_relevant) / len(predicts)
     return prec
 
-start_col="specialty"
 
-refer2order_model = train_model(start_col=start_col)
+if __name__ == '__main__':
 
-'''
-An extra layer: referral -> specialty
-'''
-if start_col == 'referral':
-    refer2spec_df = pd.read_csv('data/JCquestion_20190318/referral_specialty_next3mo_newvisits_cnt_2016.csv')
-    refer2spec_dict = dict(zip(refer2spec_df['description'].values, refer2spec_df['specialty'].values))
+    start_col="specialty"
 
-'''
-Get test data by query:
-Granularity: Each row is a specific encounter
-Want referral code (p1.description), department name & specialty, list of orders
+    refer2order_model = train_model(start_col=start_col)
 
-Raw queried data: referral, refer_enc_id, department_name, specialty, item
-Key: referral + refer_enc_id (a refer_enc_id could correspond to several referrals)
-Val: associated orders
+    '''
+    An extra layer: referral -> specialty
+    '''
+    if start_col == 'referral':
+        refer2spec_df = pd.read_csv('data/JCquestion_20190318/referral_specialty_next3mo_newvisits_cnt_2016.csv')
+        refer2spec_dict = dict(zip(refer2spec_df['description'].values, refer2spec_df['specialty'].values))
 
-Ideal processed data: {(referral, enc_id): relevant orders}
+    '''
+    Get test data by query:
+    Granularity: Each row is a specific encounter
+    Want referral code (p1.description), department name & specialty, list of orders
+    
+    Raw queried data: referral, refer_enc_id, department_name, specialty, item
+    Key: referral + refer_enc_id (a refer_enc_id could correspond to several referrals)
+    Val: associated orders
+    
+    Ideal processed data: {(referral, enc_id): relevant orders}
+    
+    for each referral: (TODO: maintain a list of enc_ids for each referral)
+        get all (referral, enc_id) keys: (TODO: get a dictionary of key:val)
+            get pred by using model
+            compare against true orders (val)
+    
+    select 
+        p1.description as referral, 
+        p1.pat_enc_csn_id_coded as refer_enc_id,
+        d.department_name, 
+        d.specialty, 
+        p2.description as item
+    from 
+        datalake_47618.order_proc p1,
+        datalake_47618.order_proc p2,
+        datalake_47618.encounter e1,
+        datalake_47618.encounter e2,
+        datalake_47618.dep_map d
+    where
+        lower(p1.description) like '%referral%' 
+        and p1.pat_enc_csn_id_coded = e1.pat_enc_csn_id_coded
+        and e1.jc_uid = e2.jc_uid
+        and e1.pat_enc_csn_id_coded != e2.pat_enc_csn_id_coded
+        and e2.visit_type like '%NEW PATIENT%'
+        and e1.appt_when_jittered <= e2.appt_when_jittered
+        and DATE_ADD(date(timestamp(e1.appt_when_jittered)), INTERVAL 3 month) > date(timestamp(e2.appt_when_jittered))
+        and e2.department_id = d.department_id
+        and e1.appt_when_jittered >= '2017-01-01'
+        and p2.pat_enc_csn_id_coded = e2.pat_enc_csn_id_coded
+    '''
 
-for each referral: (TODO: maintain a list of enc_ids for each referral)
-    get all (referral, enc_id) keys: (TODO: get a dictionary of key:val)
-        get pred by using model
-        compare against true orders (val)
+    df_test = pd.read_csv('data/JCquestion_20190318/testset_2017.csv')
 
-select 
-    p1.description as referral, 
-    p1.pat_enc_csn_id_coded as refer_enc_id,
-    d.department_name, 
-    d.specialty, 
-    p2.description as item
-from 
-    datalake_47618.order_proc p1,
-    datalake_47618.order_proc p2,
-    datalake_47618.encounter e1,
-    datalake_47618.encounter e2,
-    datalake_47618.dep_map d
-where
-    lower(p1.description) like '%referral%' 
-    and p1.pat_enc_csn_id_coded = e1.pat_enc_csn_id_coded
-    and e1.jc_uid = e2.jc_uid
-    and e1.pat_enc_csn_id_coded != e2.pat_enc_csn_id_coded
-    and e2.visit_type like '%NEW PATIENT%'
-    and e1.appt_when_jittered <= e2.appt_when_jittered
-    and DATE_ADD(date(timestamp(e1.appt_when_jittered)), INTERVAL 3 month) > date(timestamp(e2.appt_when_jittered))
-    and e2.department_id = d.department_id
-    and e1.appt_when_jittered >= '2017-01-01'
-    and p2.pat_enc_csn_id_coded = e2.pat_enc_csn_id_coded
-'''
-
-df_test = pd.read_csv('data/JCquestion_20190318/testset_2017.csv')
-
-'''
-get dict: {referral: a list of enc_ids}
-'''
-referral_to_encs_dict = df_test[['referral', 'refer_enc_id']]\
-                            .groupby('referral')['refer_enc_id'].apply(list).to_dict()
-
-
-'''
-get dict: {(referral, enc_id): relevant orders}
-'''
-
-keys = zip(df_test['refer_enc_id'].values, df_test['referral'].values)
-
-vals = df_test['item'].values
-actual_orders_dict = {}
-for i in range(len(keys)):
-    if keys[i] in actual_orders_dict:
-        actual_orders_dict[keys[i]].append(vals[i])
-    else:
-        actual_orders_dict[keys[i]] = [vals[i]]
-
-f = open('data/actual_predict_samples.txt', 'w')
+    '''
+    get dict: {referral: a list of enc_ids}
+    '''
+    referral_to_encs_dict = df_test[['referral', 'refer_enc_id']]\
+                                .groupby('referral')['refer_enc_id'].apply(list).to_dict()
 
 
-case_to_look_each_type = 10
+    '''
+    get dict: {(referral, enc_id): relevant orders}
+    '''
 
-'''
-Check the prec@k for each type
-'''
+    keys = zip(df_test['refer_enc_id'].values, df_test['referral'].values)
 
-avgprecs_by_referral = {}
+    vals = df_test['item'].values
+    actual_orders_dict = {}
+    for i in range(len(keys)):
+        if keys[i] in actual_orders_dict:
+            actual_orders_dict[keys[i]].append(vals[i])
+        else:
+            actual_orders_dict[keys[i]] = [vals[i]]
 
-all_referrals = referral_to_encs_dict.keys()
-for referral in all_referrals:
-    predict_orders = refer2order_prediction.get(referral, ['nonitem_predict']) # TODO
-
-    enc_ids = referral_to_encs_dict[referral]
-
-    precs_by_referral = []
-    case_ind = 0
-    for enc_id in enc_ids:
-        actual_orders = actual_orders_dict.get((enc_id, referral), ['nonitem_actual']) # TODO
-
-        cur_prec = prec_at_k(actual_orders, predict_orders)
-
-        '''
-        For each type of referral, check 10 examples
-        '''
-        if case_ind < case_to_look_each_type and referral=='REFERRAL TO NEUROGENETICS ONCOLOGY':
-            f.write('referral:' + referral + '\n')
-            f.write('specialty:' + str(refer2spec_dict.get(referral, 'no_corresponding_specialty')) + '\n')
-            f.write('actual_orders:' + str(actual_orders) + '\n')
-            f.write('predict_orders:' + str(predict_orders) + '\n')
-            if cur_prec > 0:
-                f.write('Something matched!\n')
-            f.write('\n')
-
-            case_ind += 1
-        precs_by_referral.append(cur_prec)
-    avgprecs_by_referral[referral] = sum(precs_by_referral)/len(precs_by_referral)
+    f = open('data/actual_predict_samples.txt', 'w')
 
 
-f.close()
-for key, val in sorted(avgprecs_by_referral.items(), key=lambda (k,v):v)[::-1]:
-    print 'referral, avgprec:', key, val
-# print 'mean precision at 10:', sum(all_precs)/len(all_precs)
+    case_to_look_each_type = 10
+
+    '''
+    Check the prec@k for each type
+    '''
+
+    avgprecs_by_referral = {}
+
+    all_referrals = referral_to_encs_dict.keys()
+    for referral in all_referrals:
+        predict_orders = refer2order_model.get(referral, ['nonitem_predict']) # TODO
+
+        enc_ids = referral_to_encs_dict[referral]
+
+        precs_by_referral = []
+        case_ind = 0
+        for enc_id in enc_ids:
+            actual_orders = actual_orders_dict.get((enc_id, referral), ['nonitem_actual']) # TODO
+
+            cur_prec = prec_at_k(actual_orders, predict_orders)
+
+            '''
+            For each type of referral, check 10 examples
+            '''
+            if case_ind < case_to_look_each_type and referral=='REFERRAL TO NEUROGENETICS ONCOLOGY':
+                f.write('referral:' + referral + '\n')
+                f.write('specialty:' + str(refer2spec_dict.get(referral, 'no_corresponding_specialty')) + '\n')
+                f.write('actual_orders:' + str(actual_orders) + '\n')
+                f.write('predict_orders:' + str(predict_orders) + '\n')
+                if cur_prec > 0:
+                    f.write('Something matched!\n')
+                f.write('\n')
+
+                case_ind += 1
+            precs_by_referral.append(cur_prec)
+        avgprecs_by_referral[referral] = sum(precs_by_referral)/len(precs_by_referral)
+
+
+    f.close()
+    for key, val in sorted(avgprecs_by_referral.items(), key=lambda (k,v):v)[::-1]:
+        print 'referral, avgprec:', key, val
+    # print 'mean precision at 10:', sum(all_precs)/len(all_precs)
 
---
+++ scripts/OutpatientReferral/second_implementation.py
@@ -0,0 +1,79 @@
+
+'''
+
+
+'''
+import pandas as pd
+pd.set_option('display.width', 1000)
+
+'''
+Preparing Training data:
+
+select
+    d.department_name,
+    p2.pat_enc_csn_id_coded as specialty_enc_id,
+    d.specialty,
+    p2.description as item
+from
+    datalake_47618.order_proc p2,
+    datalake_47618.encounter e2,
+    datalake_47618.dep_map d
+where
+    e2.visit_type like '%NEW PATIENT%'
+    and e2.department_id = d.department_id
+    and e2.appt_when_jittered >= '2016-01-01'
+    and e2.appt_when_jittered < '2017-01-01'
+    and p2.pat_enc_csn_id_coded = e2.pat_enc_csn_id_coded
+    
+Then, create dictionary specialty_to_orders as training
+'''
+df_train = pd.read_csv('data/second_implementation/training_data.csv')
+print df_train.head()
+all_specialties = df_train['specialty'].drop_duplicates().values.tolist()
+
+df_tmp = df_train[['specialty', 'item', 'specialty_enc_id']].groupby(['specialty', 'item']).count()\
+        .rename(columns={'specialty_enc_id':'cnt'}).reset_index().sort_values('cnt', ascending=False)
+print df_tmp.head()
+specialty_to_orders = {}
+for specialty in all_specialties:
+    df_tmptmp = df_tmp[df_tmp['specialty']==specialty].copy().reset_index().iloc[:10]
+    specialty_to_orders[specialty] = df_tmptmp['item'].values.tolist()
+
+'''
+Preparing Test data:
+
+select
+    d.department_name,
+    p2.pat_enc_csn_id_coded as specialty_enc_id,
+    d.specialty,
+    p2.description as item
+from
+    datalake_47618.order_proc p2,
+    datalake_47618.encounter e2,
+    datalake_47618.dep_map d
+where
+    e2.visit_type like '%NEW PATIENT%'
+    and e2.department_id = d.department_id
+    and e2.appt_when_jittered >= '2017-01-01'
+    and p2.pat_enc_csn_id_coded = e2.pat_enc_csn_id_coded
+    
+For testing, group by encounters!
+'''
+df_test = pd.read_csv('data/second_implementation/test_data.csv')
+all_encounters = df_test['specialty_enc_id'].drop_duplicates().values.tolist()
+
+from first_implementation import prec_at_k
+all_precs = []
+for encounter in all_encounters:
+    df_tmp = df_test[df_test['specialty_enc_id']==encounter]
+    cur_items = df_tmp['item'].values.tolist()
+    specialty = df_tmp['specialty'].values.tolist()[0]
+
+    actuals = cur_items
+    predicts = specialty_to_orders.get(specialty, 'no_mapped_order')
+    if len(predicts)==0:
+        continue
+    cur_prec = prec_at_k(actuals=actuals, predicts=predicts)
+
+    all_precs.append(cur_prec)
+print 'average prec:', sum(all_precs)/len(all_precs)
