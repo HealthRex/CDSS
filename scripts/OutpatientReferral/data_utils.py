9b66dae4b6afd6be84162e33c8fdf6a9ca332cbb
Song Xu <song.xu.ucla@gmail.com>
2019-04-05 09:21:12 -0700

    success query for recent 6 month data

--- scripts/OutpatientReferral/data_utils.py
+++ scripts/OutpatientReferral/data_utils.py
@@ -1,19 +1,11 @@
 
 ''''''
 
-from medinfo.db.Model import SQLQuery
-query = SQLQuery()
-query.addSelect('p1.pat_enc_csn_id_coded as referral_enc_id')
-
 import os
 import pandas as pd
+import queries
 
 from google.cloud import bigquery
-
-ENCOUNTER_TABLE = 'datalake_47618.encounter'
-ORDERPROC_TABLE = 'datalake_47618.order_proc'
-DIAGNOSIS_TABLE = 'datalake_47618.diagnosis_code'
-DEPMAP_TABLE = 'datalake_47618.dep_map'
 
 result_folderpath = 'queried_results'
 if not os.path.exists(result_folderpath):
@@ -24,14 +16,14 @@ def setup_client(jsonkey_filepath):
     return client
 
 def make_bigquery(query, client, project_id):
-    df = client.query(query, project=project_id).to_dataframe()
+    df = client.query(str(query), project=project_id).to_dataframe()
 
     return df
 
 def get_queried_data(query):
     query_id = hash(query)
 
-    cached_filepath = os.path.join(result_folderpath, 'queried_%d.csv'%query_id)
+    cached_filepath = os.path.join(result_folderpath, 'queried_data_%d.csv'%query_id)
 
     if os.path.exists(cached_filepath):
         return pd.read_csv(cached_filepath,
@@ -43,10 +35,11 @@ def get_queried_data(query):
         df = make_bigquery(query, client=client, project_id=project_id)
 
         f = open(cached_filepath, 'a')
-        f.write('#' + query.__str__() + '\n')
+        f.write('#' + str(query) + '\n')
         df.to_csv(f, index=False)
         f.close()
 
         return df
 
+query = queries.query_sample()
 print get_queried_data(query)
