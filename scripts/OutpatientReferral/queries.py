e97ea2a0bb70d64696bdd4280ee79bee42390bf1
sxu11 <song.xu.ucla@gmail.com>
2019-05-01 07:55:41 -0700

    include medication, init

--- scripts/OutpatientReferral/queries.py
+++ scripts/OutpatientReferral/queries.py
@@ -6,6 +6,7 @@ ENCOUNTER_TABLE = 'datalake_47618.encounter'
 ORDERPROC_TABLE = 'datalake_47618.order_proc'
 DIAGNOSIS_TABLE = 'datalake_47618.diagnosis_code'
 DEPMAP_TABLE = 'datalake_47618.dep_map'
+ORDERMED_TABLE = 'datalake_47618.order_med'
 
 def query_sample():
     return """
@@ -15,7 +16,7 @@ def query_sample():
             where item_date >= timestamp('2014-01-01 00:00:00')
             """
 
-def query_for_recent6months(newPatientOnly=True, referral_name=None):
+def query_for_recent6months(newPatientOnly=True, referral_name=None, include_med=False):
     query = SQLQuery()
     query.addSelect('p1.pat_enc_csn_id_coded AS referral_enc_id')
     query.addSelect('p1.description AS referral_name')
@@ -29,6 +30,8 @@ def query_for_recent6months(newPatientOnly=True, referral_name=None):
     query.addSelect('d2.department_name AS specialty_dep')
     query.addSelect('d2.specialty AS specialty_name')
     query.addSelect('p2.description AS specialty_order')
+    if include_med:
+        query.addSelect('o2.amb_med_disp_name as medication')
 
     query.addFrom('%s AS e1' % ENCOUNTER_TABLE)
     query.addFrom('%s AS p1' % ORDERPROC_TABLE)
@@ -37,6 +40,8 @@ def query_for_recent6months(newPatientOnly=True, referral_name=None):
     query.addFrom('%s AS e2' % ENCOUNTER_TABLE)
     query.addFrom('%s AS p2' % ORDERPROC_TABLE)
     query.addFrom('%s AS d2' % DEPMAP_TABLE)
+
+    query.addFrom('%s AS o2' % ORDERMED_TABLE)
 
     query.addWhere("lower(p1.description) like '%referral%'")
     query.addWhere("p1.pat_enc_csn_id_coded = e1.pat_enc_csn_id_coded")
@@ -55,4 +60,7 @@ def query_for_recent6months(newPatientOnly=True, referral_name=None):
         query.addWhere("e2.visit_type like '%NEW PATIENT%'")
     query.addWhere("e2.department_id = d2.department_id")
     query.addWhere("p2.pat_enc_csn_id_coded = e2.pat_enc_csn_id_coded")
+
+    if include_med:
+        query.addWhere("o2.pat_enc_csn_id_coded = e2.pat_enc_csn_id_coded")
     return str(query)
