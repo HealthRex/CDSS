25e441398168f41f7ac73f3b8fb30b11ebfe9bfa
sxu11 <song.xu.ucla@gmail.com>
2019-05-30 23:04:55 -0700

    refactor fig2

--- scripts/LabTestAnalysis/lab_statistics/analysis.py
+++ scripts/LabTestAnalysis/lab_statistics/analysis.py
@@ -13,7 +13,7 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 import LocalEnv
-
+import stats_database
 from scripts.LabTestAnalysis.machine_learning.LabNormalityPredictionPipeline \
         import NON_PANEL_TESTS_WITH_GT_500_ORDERS, \
             UCSF_TOP_PANELS, UMICH_TOP_PANELS, \
@@ -41,6 +41,8 @@ class Stats_Plotter():
 
         self.dataset_foldername = 'data-%s-%s-%s'%(data_source, lab_type, curr_version)
 
+        self.labStats_folderpath = os.path.join(LocalEnv.PATH_TO_CDSS, 'scripts/LabTestAnalysis/lab_statistics')
+
         self.lab_descriptions = stats_utils.get_lab_descriptions(data_source=self.data_source, lab_type=self.lab_type)
 
 
@@ -49,7 +51,7 @@ class Stats_Plotter():
 
         inverse_maker = '_inversed01' if inverse01 else ''
 
-        statsByLab_folderpath = '/Users/songxu/healthrex/CDSS/scripts/LabTestAnalysis/lab_statistics/data-%s-%s-%s/'%(self.data_source,self.lab_type,self.curr_version)
+        statsByLab_folderpath = os.path.join(self.labStats_folderpath, self.dataset_foldername)
         ml_folderpath = statsByLab_folderpath.replace("lab_statistics", "machine_learning")
 
         result_folderpath = os.path.join(statsByLab_folderpath, '../', 'Fig1_cartoon')
@@ -196,7 +198,183 @@ class Stats_Plotter():
         else:
             plt.savefig(os.path.join(result_folderpath, 'cartoon_%s%s.png' % (lab, inverse_maker)))
 
-def main_full_analysis(curr_version, inverse01=False):
+    def plot_order_intensities_barh(self, lab, time_since_last_order_binned, columns, scale,
+                                    labeling=True, lab_ind=None):
+
+        alphas = [1, 0.5, 0.2, 0.1]
+        colors = ['black', 'blue', 'blue', 'blue']
+
+        local_lab_descriptions = {
+            'LABCBCD': 'CBC WITH\nDIFF',
+            'LABPHOS': 'PHOSPHORUS\nSERUM/PLASMA',
+            'LABA1C': 'HEMOGLOBIN\nA1C',
+            'LABALB': 'ALBUMIN\nSERUM/PLASMA',
+            'LABTSH': 'THYROID\nSIMULATING\nHORMONE',
+            'LABESRP': 'SEDIMENTATION\nRATE (ESR)'
+        }
+
+        lab_descriptions = self.lab_descriptions
+
+        pre_sum = 0
+        pre_sums = []
+        for key in columns:
+            pre_sum += time_since_last_order_binned[key]
+            pre_sums.append(pre_sum)
+
+        for i in range(len(columns)-1,-1,-1):
+            key = columns[i]
+            pre_sum = pre_sums[i]
+
+            lab_descriptions['LABTSH'] = 'TSH'
+            lab_descriptions['LABESRP'] = 'Sedimentation Rate'
+            lab_descriptions['LABCBCD'] = 'CBC w/ Diff'
+            lab_descriptions['LABPTT'] = 'PTT'
+            lab_descriptions['LABHEPAR'] = 'Heparin Activity Level'
+            lab_descriptions['LABLIDOL'] = 'Lidocaine Level'
+            lab_desciption = lab_descriptions.get(lab, lab)
+
+            if labeling:
+                plt.barh([lab_desciption], pre_sum, color=colors[i], alpha=alphas[i], label=key)
+            else:
+                plt.barh([lab_desciption], pre_sum, color=colors[i], alpha=alphas[i])
+
+    def draw__Order_Intensities(self, labs,
+                                scale=1., result_label=None, scale_method = 'normalize',
+                                use_cached_fig_data=True,
+                                include_legend=True):
+
+
+        '''
+        Drawing Figure 2 in the main text.
+
+        :param self.lab_type:
+        :return:
+        '''
+
+        '''
+        Get labs
+        '''
+        print "Labs to be plot:", labs
+
+        cached_result_foldername = os.path.join(self.labStats_folderpath, 'Fig2_Order_Intensities/')
+        if not os.path.exists(cached_result_foldername):
+            os.mkdir(cached_result_foldername)
+        cached_result_filename = 'Order_Intensities_%s_%s.csv'%(self.lab_type, result_label)
+        cached_result_path = os.path.join(cached_result_foldername, cached_result_filename)
+
+        '''
+        Each lab 
+            -> all its orders in 2014/07-2017/06 (implicit) 
+            -> {time since last order:cnts} (cached)
+            -> {0-1 days: cnt, 1-3 days: ...} 
+            -> barh
+        '''
+        lab2stats = {}
+        columns = ['< 24 hrs', '[24 hrs, 3 days)', '[3 days, 1 week)', '>= 1 week']
+
+        if os.path.exists(cached_result_path) and use_cached_fig_data:
+            lab2stats_pd = pd.read_csv(cached_result_path)
+            lab2stats_pd = lab2stats_pd.rename(columns={'< 1 day':'< 24 hrs',
+                                                        '1-3 days':'[24 hrs, 3 days)',
+                                                        '3-7 days':'[3 days, 1 week)',
+                                                        '> 7 days':'>= 1 week'})
+            lab2stats = lab2stats_pd.set_index('lab').to_dict(orient='index')
+
+        else:
+            for lab in labs:
+                print 'Getting Order Intensities of lab %s..'%lab
+
+                df_lab = stats_utils.get_queried_lab(lab, self.lab_type, time_limit=stats_utils.DEFAULT_TIMELIMIT)
+
+                dict_lab = stats_utils.get_floored_day_to_number_orders_cnts(lab, df_lab)
+
+                day_counts = [0] * 4
+                tot_cnt = 0
+                for time, cnt in dict_lab.items():
+                    if 0 <= time < 1:
+                        day_counts[0] += cnt
+                    elif 1 <= time < 3:
+                        day_counts[1] += cnt
+                    elif 3 <= time < 7:
+                        day_counts[2] += cnt
+                    elif time >= 7:
+                        day_counts[3] += cnt
+                    else:
+                        print "time is out of range:", time
+                    tot_cnt += cnt
+
+                lab2stats[lab] = dict(zip(columns, day_counts))
+
+            df_res = pd.DataFrame.from_dict(lab2stats, orient='index').reset_index().rename(columns={'index':'lab'})
+
+            df_res.to_csv(cached_result_path, index=False)
+
+        for lab, stats in lab2stats.items():
+            stats['total'] = float(sum(stats.values()))
+
+        labs_toplots = [labs]
+
+        fig, ax = plt.subplots(figsize=(8, .3 + 5.7/20.*len(labs))) #5+ 3./20.*len(labs)
+        # 8, 3.435 for guideline
+        # 8, 6 for freq labs
+        # 6 - 3.435 = 2.565
+        # Size for saturation is 7, 2.565 ?
+        print .3 + 5.7/20.*len(labs)
+        for i, lab in enumerate(labs[::-1]):
+
+            time_since_last_order_binned = lab2stats[lab]
+
+
+            for time, cnt in time_since_last_order_binned.items():
+                if time == 'total':
+                    continue
+                if scale_method == 'normalize':
+                    time_since_last_order_binned[time] = cnt/time_since_last_order_binned['total']
+
+                elif scale_method == 'by_scale':
+                    time_since_last_order_binned[time] = cnt*scale
+
+            if i == 0:
+                self.plot_order_intensities_barh(lab, time_since_last_order_binned,
+                                                 columns=columns,
+                                                 labeling=True,
+                                                 lab_ind=i,
+                                                 scale=scale)
+            else:
+                self.plot_order_intensities_barh(lab, time_since_last_order_binned,
+                                                 columns=columns,
+                                                 labeling=False,
+                                                 lab_ind=i,
+                                                 scale=scale)
+
+        if scale_method == 'normalize':
+            ax.set_xticklabels(['{:,.0%}'.format(x) for x in np.linspace(0,1,num=6)])
+            plt.tick_params('x', labelsize=13)
+            plt.tick_params('y', labelsize=11)
+            plt.xlim([0,1])
+        else:
+            plt.tick_params('x', labelsize=15) #12
+            plt.tick_params('y', labelsize=13) #10
+
+        if result_label != 'labs_guideline':
+            plt.xlabel('Number of orders per 1000 patient encounters', fontsize=14) #'Order number between 2014/07-2017/06'
+
+        if include_legend:
+            plt.legend(prop={'size': 12})
+        else:
+            ax.yaxis.tick_right()
+            ax.yaxis.set_label_position("right")
+            pass
+        # plt.xscale('log')
+
+        plt.tight_layout()
+        cached_result_folderpath = cached_result_foldername + 'Order_Intensities_%s_%s.png'%(self.lab_type,result_label)
+
+
+        plt.savefig(cached_result_folderpath)
+        plt.clf()
+
+def main_full_analysis(figs_to_plot, curr_version, inverse01=False):
 
     '''
     :param curr_version:
@@ -205,10 +383,11 @@ def main_full_analysis(curr_version, inverse01=False):
     '''
 
     '''Figure 1'''
-    plotter = Stats_Plotter(data_source='Stanford', lab_type='panel', curr_version=curr_version)
-    # plotter.main_generate_lab_statistics(verbose=False)
-    plotter.plot_full_cartoon(lab='LABLDH', include_threshold_colors=False, inverse01=inverse01)
-    plotter.plot_full_cartoon(lab='LABLDH', include_threshold_colors=True, inverse01=inverse01)
+    if 1 in figs_to_plot:
+        plotter = Stats_Plotter(data_source='Stanford', lab_type='panel', curr_version=curr_version)
+        # plotter.main_generate_lab_statistics(verbose=False)
+        plotter.plot_full_cartoon(lab='LABLDH', include_threshold_colors=False, inverse01=inverse01)
+        plotter.plot_full_cartoon(lab='LABLDH', include_threshold_colors=True, inverse01=inverse01)
     #
 
     '''Figure 2: 
@@ -216,6 +395,14 @@ def main_full_analysis(curr_version, inverse01=False):
     Stanford Component Order_Intensities, 
     Stanford Component Normality_Saturations.
     '''
+    if 2 in figs_to_plot:
+        labs_cnts_order_1day = stats_database.TOP_PANELS_AND_COUNTS_IN_1DAY[:20]
+        labs_order_1day = [x[0] for x in labs_cnts_order_1day]
+        plotter = Stats_Plotter(data_source='Stanford', lab_type='panel', curr_version=curr_version)
+        plotter.draw__Order_Intensities(labs=labs_order_1day, result_label='labs_order_1day',
+                                     scale=1. / stats_utils.NUM_DISTINCT_ENCS * 1000, use_cached_fig_data=True, scale_method='by_scale',
+                                     include_legend=True)
+        quit()
 
     for data_source in ['Stanford', 'UCSF', 'UMich']: #'Stanford',
         for lab_type in ['panel', 'component']:
@@ -233,5 +420,5 @@ def main_full_analysis(curr_version, inverse01=False):
 
 if __name__ == '__main__':
     curr_version = '10000-episodes-lastnormal'
-
-    main_full_analysis(curr_version=curr_version, inverse01=True)
+    figs_to_plot = [2]
+    main_full_analysis(figs_to_plot, curr_version=curr_version, inverse01=True)
