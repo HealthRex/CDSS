81c73848fbf06549e9efa2b4b113f83405fa6ee5
Jonathan Chiang <13003102+JonathanChiang@users.noreply.github.com>
2019-06-19 19:15:10 -0700

    make it look a little cleaner

--- scripts/SimulationRecommender/RecommenderPipeline/syncPipeline/UnitTests/configuration.py
+++ scripts/SimulationRecommender/RecommenderPipeline/syncPipeline/UnitTests/configuration.py
@@ -5,15 +5,17 @@ import pandas as pd
 import numpy as np
 import os
 
-
+# SET PATH TO CLINICAL RECOMMENDER FOLDER
 recommender_path = "/Users/jonc101/Box Sync/jichiang_folders/clinical_recommender_pipeline/"
+
 physician_grading = recommender_path + "physician_grading/"
 physician_response = recommender_path + "physician_response/"
 tracker_data = recommender_path + "tracker_data/"
 unit_test = recommender_path + "unit_test/"
 
-        # SET DB CONNECTION
+# SET DB CONNECTION
 connection = pg.connect("host='localhost' dbname=stride_inpatient_2014 user=postgres password='MANUAL PASSWORD'")
+
 clinical_item = pd.read_sql_query('select * from clinical_item', con=connection)
 sim_patient_order = pd.read_sql_query('select * from sim_patient_order',con=connection)
 sim_state = pd.read_sql_query('select * from sim_state',con=connection)
