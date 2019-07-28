30d5e30cca159bdba914a351902bf52f8eadcbcb
sxu11 <song.xu.ucla@gmail.com>
2019-07-28 15:15:46 -0700

    put import module also into config

--- scripts/LabTestAnalysis/lab_statistics/JAMANetworkOpen/JAMA_config.py
+++ scripts/LabTestAnalysis/lab_statistics/JAMANetworkOpen/JAMA_config.py
@@ -1,6 +1,21 @@
 
+import warnings
+warnings.filterwarnings("ignore")
+
 import os
 import LocalEnv
+
+import matplotlib.pyplot as plt
+
+from sklearn import metrics
+
+import pandas as pd
+import numpy as np
+from scripts.LabTestAnalysis.lab_statistics import stats_utils
+from scripts.LabTestAnalysis.machine_learning.ml_utils import map_lab
+
+
+
 
 curr_version = '10000-episodes-lastnormal'
 inverse01 = True # Setting 'True' to interpret 'Normal' as 'Negative'
