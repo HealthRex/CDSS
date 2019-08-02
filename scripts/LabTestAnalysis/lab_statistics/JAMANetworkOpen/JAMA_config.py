9c84e6b62146da630440371742567d809285a8c2
sxu11 <song.xu.ucla@gmail.com>
2019-08-01 23:30:44 -0700

    reformat

--- scripts/LabTestAnalysis/lab_statistics/JAMANetworkOpen/JAMA_config.py
+++ scripts/LabTestAnalysis/lab_statistics/JAMANetworkOpen/JAMA_config.py
@@ -14,7 +14,10 @@ import numpy as np
 from scripts.LabTestAnalysis.lab_statistics import stats_utils
 from scripts.LabTestAnalysis.machine_learning.ml_utils import map_lab
 
+from scripts.LabTestAnalysis.lab_statistics import stats_database
 
+from scripts.LabTestAnalysis.machine_learning.LabNormalityPredictionPipeline \
+        import STRIDE_COMPONENT_TESTS_common, UMICH_TOP_COMPONENTS_common, UCSF_TOP_COMPONENTS_common
 
 
 curr_version = '10000-episodes-lastnormal'
@@ -25,3 +28,5 @@ inverse_maker = '_inversed01' if inverse01 else ''
 project_folderpath = os.path.join(LocalEnv.PATH_TO_CDSS, 'scripts/LabTestAnalysis')
 stats_folderpath = os.path.join(project_folderpath, 'lab_statistics')
 ML_folderpath = os.path.join(project_folderpath, 'machine_learning')
+
+
