807e274f354533f32c6802e14431e83148b05c70
Ivana <52838521+anacapa17@users.noreply.github.com>
2019-08-14 15:08:47 -0700

    Partial query to pull blood glucose labs

---
+++ scripts/DiabeticsAtStanford/blood_glucose_partialquery.sql
@@ -0,0 +1,3 @@
+WHERE UPPER(lab_name) LIKE '%GLUCOSE%' 
+	AND (lab_name) NOT IN ( 'Glucose, Urine', 'Glucose Urine', 'Glucose, Fluid', "Est. Mean Glucose", "Glucose, CSF", "Collection Period Glucose", "Glucose Excretion", "Glucose Body Fluid", "Volume Glucose") 
+	AND UPPER(ordering_mode) = 'INPATIENT' AND ord_num_value BETWEEN 0 AND 9999998
