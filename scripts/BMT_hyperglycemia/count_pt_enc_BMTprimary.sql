eca89f9371d73a95c583d2f0d59aaf40ac36875f
Ivana <52838521+anacapa17@users.noreply.github.com>
2019-08-19 15:03:29 -0700

    Distinct count of pt encounters w/ BMT primary

--- scripts/BMT_hyperglycemia/count_pt_enc_BMTprimary.sql
+++ scripts/BMT_hyperglycemia/count_pt_enc_BMTprimary.sql
@@ -1,5 +1,5 @@
-SELECT count(pat_enc_csn_id_coded) FROM `mining-clinical-decisions.starr_datalake2018.treatment_team` 
+SELECT count(distinct(pat_enc_csn_id_coded)) FROM `mining-clinical-decisions.starr_datalake2018.treatment_team` 
 
 WHERE name = "Primary Team" AND prov_name LIKE "%BMT%"
 
--- Count = 7786 patient encounters with BMT as a primary team
+-- Count = 7484 distinct patinet encounters out of 7786 total patient encounters with BMT as a primary team
