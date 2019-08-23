64ce9d922ab91e8bbcb18b3b00cb5999a10aa8a3
Ivana <52838521+anacapa17@users.noreply.github.com>
2019-08-23 09:57:02 -0700

    Switched from service to treatment team

--- scripts/BMT_hyperglycemia/BGs_BMT_pts.sql
+++ scripts/BMT_hyperglycemia/BGs_BMT_pts.sql
@@ -1,5 +1,7 @@
--- BGs of pts who have been on the BMT service 
+-- BGs of pts who have had BMT as primary treatment team
 
 SELECT rit_uid, lab_name, base_name, ord_value, ord_num_value FROM `starr_datalake2018.lab_result`  
   WHERE UPPER(lab_name) LIKE '%GLUCOSE%' AND (lab_name) NOT IN ( 'Glucose, Urine', 'Glucose Urine', 'Glucose, Fluid', "Est. Mean Glucose", "Glucose, CSF", "Collection Period Glucose", "Glucose Excretion", "Glucose Body Fluid", "Volume Glucose") AND UPPER(ordering_mode) = 'INPATIENT' AND ord_num_value BETWEEN 0 AND 9999998 
-  AND rit_uid IN (SELECT DISTINCT(jc_uid) FROM starr_datalake2018.adt WHERE pat_service LIKE 'Bone%')
+  AND rit_uid IN (SELECT DISTINCT(rit_uid) FROM starr_datalake2018.treatment_team AS TT WHERE (TT.name = "Primary Team" AND TT.prov_name LIKE "%BMT%"))
+
+
