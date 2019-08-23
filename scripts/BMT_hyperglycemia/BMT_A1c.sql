4e061cb789b4de8d9ee17fcb302f7b2b17752c9b
Ivana <52838521+anacapa17@users.noreply.github.com>
2019-08-23 14:55:18 -0700

    Pt encounters with BMT as primary treatment team &  A1c > 6.5%

--- scripts/BMT_hyperglycemia/BMT_A1c.sql
+++ scripts/BMT_hyperglycemia/BMT_A1c.sql
@@ -1,5 +1,7 @@
--- Pts who have been on BMT service with A1c > 6.5% —> 379
+-- Pt encounters with BMT as primary treatment team &  A1c > 6.5% —> 281
  	
- SELECT count (DISTINCT(rit_uid))  FROM `starr_datalake2018.lab_result` 
-  WHERE UPPER(lab_name) LIKE '%A1C%’ AND ord_num_value >= 6.5 
-  AND rit_uid IN (SELECT DISTINCT(jc_uid) FROM starr_datalake2018.adt WHERE pat_service LIKE 'Bone%’)
+SELECT count(distinct(Lab.pat_enc_csn_id_coded))  FROM starr_datalake2018.lab_result AS Lab
+	  WHERE  Lab.pat_enc_csn_id_coded IN (SELECT DISTINCT(TT.pat_enc_csn_id_coded) FROM starr_datalake2018.treatment_team AS TT 
+      WHERE (TT.name = "Primary Team" AND TT.prov_name LIKE "%BMT%"))
+  AND UPPER(Lab.lab_name) LIKE '%A1C%' AND ord_num_value >= 6.5 AND ord_num_value != 9999999
+  
