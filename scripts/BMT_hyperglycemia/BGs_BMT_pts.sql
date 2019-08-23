724178079168faf1700840773bb1a840bdca48a3
Ivana <52838521+anacapa17@users.noreply.github.com>
2019-08-23 13:53:50 -0700

    Individual BGs of patients who had BMT as their primary team during this patient encounter

--- scripts/BMT_hyperglycemia/BGs_BMT_pts.sql
+++ scripts/BMT_hyperglycemia/BGs_BMT_pts.sql
@@ -7,7 +7,8 @@
 
 
 -- Individual BGs of patients who had BMT as their primary team during this patient encounter
-SELECT Lab.rit_uid, Lab.pat_enc_csn_id_coded, Lab.lab_name, Lab.base_name, Lab.ord_value, Lab.ord_num_value, Lab.order_id_coded FROM starr_datalake2018.lab_result AS Lab
+
+SELECT Lab.rit_uid, Lab.pat_enc_csn_id_coded, Lab.lab_name, Lab.base_name, Lab.ord_num_value, Lab.taken_time_jittered, Lab.order_id_coded FROM starr_datalake2018.lab_result AS Lab
 
 WHERE Lab.order_id_coded IN (SELECT distinct (Lab.order_id_coded)FROM starr_datalake2018.treatment_team AS TT JOIN starr_datalake2018.lab_result AS Lab ON Lab.pat_enc_csn_id_coded = TT.pat_enc_csn_id_coded  
   WHERE Lab.pat_enc_csn_id_coded IN (SELECT DISTINCT(pat_enc_csn_id_coded) FROM starr_datalake2018.treatment_team AS TT WHERE (TT.name = "Primary Team" AND TT.prov_name LIKE "%BMT%"))
@@ -18,7 +19,9 @@ WHERE Lab.order_id_coded IN (SELECT distinct (Lab.order_id_coded)FROM starr_data
 AND   
   (UPPER(Lab.lab_name) LIKE '%GLUCOSE%' AND (Lab.lab_name) NOT IN ( 'Glucose, Urine', 'Glucose Urine', 'Glucose, Fluid', "Est. Mean Glucose", "Glucose, CSF", "Collection Period Glucose", "Glucose Excretion", "Glucose Body Fluid", "Volume Glucose") AND UPPER(Lab.ordering_mode) = 'INPATIENT' AND Lab.ord_num_value BETWEEN 0 AND 9999998)
 
-Order by Lab.order_id_coded, pat_enc_csn_id_coded
+Order by Lab.pat_enc_csn_id_coded, Lab.order_id_coded, Lab.taken_time_jittered
+
+
 
 
 
