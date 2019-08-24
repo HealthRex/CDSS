604ccce9630c6e91f6b33eddc7cd6d8b9c49a410
Ivana <52838521+anacapa17@users.noreply.github.com>
2019-08-24 13:39:20 -0700

    BMT pts on ISS

--- scripts/BMT_hyperglycemia/pts_on_ISS.sql
+++ scripts/BMT_hyperglycemia/pts_on_ISS.sql
@@ -1,5 +1,6 @@
-	SELECT  rit_uid, ss_section_name  FROM `mining-clinical-decisions.starr_datalake2018.med_orderset` 
-	WHERE UPPER(ss_section_name) LIKE '%SCALE%'
-	ORDER BY ss_section_name ASC
+	SELECT  count(distinct(TT.pat_enc_csn_id_coded)) FROM starr_datalake2018.med_orderset as OS JOIN starr_datalake2018.treatment_team AS TT ON OS.rit_uid = TT.rit_uid 
 
-	-- Patients on ISS (distinct pts = 50924; distinct pt encounters = 84859)
+	WHERE (TT.name = "Primary Team" AND TT.prov_name LIKE "%BMT%")
+	AND  UPPER(ss_section_name) LIKE '%SCALE%'
+
+	-- Patient encounters with BMT pirmary and ISS (3571)
