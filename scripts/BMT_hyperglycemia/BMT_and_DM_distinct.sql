f1e4dac62dfca419cce99b029748329ea6397f6c
Ivana <52838521+anacapa17@users.noreply.github.com>
2019-08-24 13:59:26 -0700

    Distinct pt encounters on the BMT service with DM coded

--- scripts/BMT_hyperglycemia/BMT_and_DM_distinct.sql
+++ scripts/BMT_hyperglycemia/BMT_and_DM_distinct.sql
@@ -1,6 +1,6 @@
--- Distinct pts with BMT and DM coded —> 1018
+-- Distinct pt encounters on the BMT service with DM coded —> 1242
 
-SELECT count (DISTINCT(jc_uid)) from `starr_datalake2018.diagnosis_code` 
-  WHERE jc_uid IN (SELECT (jc_uid) from `starr_datalake2018.diagnosis_code` 
-    WHERE UPPER(dx_name) LIKE '%BONE MARROW%' AND UPPER(dx_name) LIKE '%TRANSPLANT%') 
-  AND UPPER(dx_name) LIKE '%DIABETES%’
+SELECT count(distinct(pat_enc_csn_id_coded)) from `starr_datalake2018.diagnosis_code` 
+  WHERE pat_enc_csn_id_coded IN (SELECT (pat_enc_csn_id_coded) from starr_datalake2018.treatment_team AS TT  
+  WHERE (TT.name = "Primary Team" AND TT.prov_name LIKE "%BMT%") )
+  AND UPPER(dx_name) LIKE '%DIABETES%'
