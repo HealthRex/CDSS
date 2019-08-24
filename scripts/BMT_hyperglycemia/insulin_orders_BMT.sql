10b9708f5cb3ef5c3ed5a63c902247884aa2510a
Ivana <52838521+anacapa17@users.noreply.github.com>
2019-08-24 15:30:21 -0700

    Insulin orders for patient encounters on the BMT service

--- scripts/BMT_hyperglycemia/insulin_orders_BMT.sql
+++ scripts/BMT_hyperglycemia/insulin_orders_BMT.sql
@@ -1,7 +1,7 @@
--- Insulin orders for patients who have ever been on the BMT service 
+-- Insulin orders (excluding ISS) for patients during encounters on the BMT service 
 
 SELECT jc_uid, pat_enc_csn_id_coded, order_time_jittered, medication_id,  med_description, med_route_c, med_route, freq_display_name, hv_discrete_dose, hv_dose_unit, pharm_class_name, thera_class_name FROM `starr_datalake2018.order_med`
 	WHERE UPPER(med_description) LIKE '%INSULIN%'
 	AND (ordering_mode_c) = 2
-	AND jc_uid IN (SELECT DISTINCT(jc_uid) FROM starr_datalake2018.adt WHERE pat_service LIKE 'Bone%') -- patients with "patient service" = Bone Marrow Transplant
+	AND pat_enc_csn_id_coded IN (SELECT (pat_enc_csn_id_coded) from starr_datalake2018.treatment_team AS TT  WHERE (TT.name = "Primary Team" AND TT.prov_name LIKE "%BMT%") )
 	ORDER BY jc_uid, order_time_jittered asc 
