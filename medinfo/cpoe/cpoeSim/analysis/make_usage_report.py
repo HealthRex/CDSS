1f12ff91485cbb0ac0b259c1bf25c64a31de2791
Nodir Yuldashev <y_nodir@yahoo.com>
2019-11-13 19:29:36 +0100

    Issue #263: Added grading functionality (multiple graders possible) to `make_usage_report`. Corrected misspelled column `unqique_recommended_options`.

--- medinfo/cpoe/cpoeSim/analysis/make_usage_report.py
+++ medinfo/cpoe/cpoeSim/analysis/make_usage_report.py
@@ -6,20 +6,39 @@ from optparse import OptionParser
 import pandas as pd
 
 from CPOETrackerAnalysis import aggregate_simulation_data
-from medinfo.common.Const import COMMENT_TAG
+from medinfo.common.Const import COMMENT_TAG, VALUE_DELIM
 from medinfo.common.Util import log
 from medinfo.db import DBUtil
 from medinfo.db.Model import SQLQuery
+from medinfo.cpoe.cpoeSim.SimManager import SimManager
 
 
-def make_usage_report(data_home, out_file):
+def make_usage_report(data_home, graders, out_file):
     aggregate_simulation_data(data_home, output_path=out_file)
     csv = pd.read_csv(out_file)
 
-    # add calculated fields
+    add_calculated_fields_to(csv)
+    csv = add_sim_case_column_to(csv)
+    csv, grade_columns_names = add_grades_to(csv, graders)
+
+    # sort by user, sim_case, sim_patient_id combo
+    csv = csv.sort_values(['user', 'sim_case', 'sim_patient_id'])
+
+    # output (without row numbers)
+    columns_order = ['user', 'sim_case', 'sim_patient_id', 'patient', 'recommenderOn', 'elapsed_time', 'total_num_clicks',
+                     'num_note_clicks', 'num_results_review_clicks', 'recommended_options',
+                     'unique_recommended_options', 'manual_search_options', 'total_orders', 'orders_from_recommender',
+                     'orders_from_manual_search', 'orders_from_recommender_missed',
+                     'ratioOrdersFromRecommenderVsUniqueRecommenderOptions',
+                     'ratioOrdersFromManualSearchVsManualSearchOptions', 'ratioOrdersFromRecommenderVsTotalOrders',
+                     'ratioOrdersFromManualSearchVsTotalOrders'] + grade_columns_names
+    csv.to_csv(out_file, index=False, columns=columns_order)
+
+
+def add_calculated_fields_to(csv):
     csv['recommenderOn'] = [1 if recommended_option > 0 else 0 for recommended_option in csv['recommended_options']]
     csv['ratioOrdersFromRecommenderVsUniqueRecommenderOptions'] = \
-        1.0 * csv['orders_from_recommender'] / csv['unqique_recommended_options']
+        1.0 * csv['orders_from_recommender'] / csv['unique_recommended_options']
     csv['ratioOrdersFromManualSearchVsManualSearchOptions'] = \
         1.0 * csv['orders_from_manual_search'] / csv['manual_search_options']
     csv['ratioOrdersFromRecommenderVsTotalOrders'] = \
@@ -27,7 +46,8 @@ def make_usage_report(data_home, out_file):
     csv['ratioOrdersFromManualSearchVsTotalOrders'] = \
         1.0 * csv['orders_from_manual_search'] / csv['total_orders']
 
-    # add sim_case
+
+def add_sim_case_column_to(csv):
     query = SQLQuery()
     query.addSelect("sim_patient_id")
     query.addSelect("sim_case_name as sim_case")
@@ -36,51 +56,79 @@ def make_usage_report(data_home, out_file):
                   "sgk.clinical_item_id = spo.clinical_item_id and spo.sim_state_id = sgk.sim_state_id")
     query.addGroupBy("sim_patient_id")
     query.addGroupBy("sim_case_name")
-
     case_names = DBUtil.execute(str(query))
 
     # merge sim_case column
-    complete_csv = pd.merge(csv, pd.DataFrame(case_names, columns=['sim_patient_id', 'sim_case']),
-                            left_on='patient', right_on='sim_patient_id')
+    csv = pd.merge(csv, pd.DataFrame(case_names, columns=['sim_patient_id', 'sim_case']),
+                   left_on='patient', right_on='sim_patient_id')
+    return csv
 
-    # sort by user, patient combo
-    complete_csv = complete_csv.sort_values(['user', 'sim_case', 'sim_patient_id'])
 
-    # output (without row numbers)
-    columns_order = ['user', 'sim_case', 'sim_patient_id', 'recommenderOn', 'elapsed_time', 'total_num_clicks',
-                     'num_note_clicks', 'num_results_review_clicks', 'recommended_options',
-                     'unqique_recommended_options', 'manual_search_options', 'total_orders', 'orders_from_recommender',
-                     'orders_from_manual_search', 'orders_from_recommender_missed',
-                     'ratioOrdersFromRecommenderVsUniqueRecommenderOptions',
-                     'ratioOrdersFromManualSearchVsManualSearchOptions',
-                     'ratioOrdersFromRecommenderVsTotalOrders', 'ratioOrdersFromManualSearchVsTotalOrders']
-    complete_csv.to_csv(out_file, index=False, columns=columns_order)
+def add_grades_to(csv, graders):
+    sim_manager = SimManager()
+    grade_columns_ordered = []
+    for i, grader in enumerate(sorted(graders)):
+        grade_column = 'grade ({})'.format(grader)
+        grades = sim_manager.grade_cases(csv['patient'].values.astype(int), grader)
+
+        # validate most_active_user_id == most_graded_user_id
+        inconsistent_users = [grade for grade in grades if grade['most_active_user_id'] != grade['most_graded_user_id']]
+        if inconsistent_users:
+            for inconsistent_user in inconsistent_users:
+                log.warn("Case {} grading issue: most_active_user_id <> most_graded_user_id ({} <> {})!"
+                         .format(inconsistent_user['sim_patient_id'],
+                                 inconsistent_user['most_active_user_id'],
+                                 inconsistent_user['most_graded_user_id']))
+
+        grade_results = pd.DataFrame(grades, columns=['sim_patient_id', 'most_active_user_id', 'total_score'])
+        patient_id_column = 'grade{}_patient_id'.format(i)
+        grade_results.rename(columns={'sim_patient_id': patient_id_column}, inplace=True)
+        csv = pd.merge(csv, grade_results,
+                       left_on=['patient', 'user'], right_on=[patient_id_column, 'most_active_user_id'])
+        csv.rename(columns={'total_score': grade_column}, inplace=True)
+        grade_columns_ordered.append(grade_column)
+
+    # don't need grader name if there's only 1 grade
+    # if len(grade_columns_ordered) == 1:
+    #     csv.rename(columns={grade_columns_ordered[0]: 'grade'}, inplace=True)
+    #     grade_columns_ordered = ['grade']
+
+    return csv, grade_columns_ordered
 
 
 if __name__ == "__main__":
     """Main method, callable from command line"""
-    usage_str = "usage: %prog <inputJsonDataFolder> <outputFile>\n" \
+    usage_str = "usage: %prog [options] <inputJsonDataFolder> <outputFile>\n" \
                 "   <outputFile> CSV file with the usage report."
 
     parser = OptionParser(usage=usage_str)
+    parser.add_option("-g", "--graders", dest="graders", help="Comma-separated list of graders to use for grading")
+
     (options, args) = parser.parse_args(sys.argv[1:])
 
     log.info("Starting: " + str.join(" ", sys.argv))
     timer = time.time()
     summary_data = {"argv": sys.argv}
 
-    if len(args) < 2:
+    grader_ids = set()
+    if not options.graders:     # graders is a mandatory parameter
+        print("No graders given. Cannot grade patient cases. Exiting.\n")
+        parser.print_help()
+        sys.exit()
+    else:
+        grader_ids.update(options.graders.split(VALUE_DELIM))
+
+    if len(args) < 2:   # we need input and output files given
         parser.print_help()
         sys.exit()
 
     input_folder = args[0]
-
     output_filename = args[1]
 
     # Print comment line with arguments to allow for deconstruction later as well as extra results
     print(COMMENT_TAG, json.dumps(summary_data))
 
-    make_usage_report(input_folder, output_filename)
+    make_usage_report(input_folder, grader_ids, output_filename)
 
     timer = time.time() - timer
     log.info("%.3f seconds to complete", timer)
