e76d11fe5cae394f70e1bf12cffdb01ad58aa668
Ivana Jankovic <Ivana@DNab4215d3.stanford.edu>
2019-12-24 19:04:32 -0500

    update >0 units and Cr <2

--- scripts/InpatientDiabetes/inpt_subQ_insulin.sql
+++ scripts/InpatientDiabetes/inpt_subQ_insulin.sql
@@ -142,7 +142,7 @@ ORDER BY CAST(mar.sig AS float64) --converts string to number
 
 -- 
 -- Units subQ insulin given where creatinine <2 during the patient encounter
-SELECT mar.jc_uid, mar.pat_enc_csn_id_coded, mar.sig as units, mar.mar_action, medord.med_description, mar.route, medord.medication_id, medord.sig as instructions FROM `som-nero-phi-jonc101.starr_datalake2018.mar` as mar 
+SELECT mar.jc_uid, mar.pat_enc_csn_id_coded, mar.sig as units, mar.mar_action, medord.med_description, mar.route, medord.medication_id, medord.sig, mar.taken_time_jittered as instructions FROM `som-nero-phi-jonc101.starr_datalake2018.mar` as mar 
   LEFT JOIN `som-nero-phi-jonc101.starr_datalake2018.order_med` as medord on mar.order_med_id_coded=medord.order_med_id_coded 
   WHERE mar.order_med_id_coded in 
   (SELECT medord2.order_med_id_coded FROM `som-nero-phi-jonc101.starr_datalake2018.order_med` as medord2
@@ -165,6 +165,31 @@ SELECT mar.jc_uid, mar.pat_enc_csn_id_coded, mar.sig as units, mar.mar_action, m
     AND taken_time_jittered IS NOT null
     AND ord_num_value > 2)
 
+--
+-- Units subQ insulin given (>0units) where creatinine <2 during the patient encounter
+SELECT mar.jc_uid, mar.pat_enc_csn_id_coded, mar.sig as units, mar.mar_action, medord.med_description, mar.route, medord.medication_id, medord.sig, mar.taken_time_jittered as instructions FROM `som-nero-phi-jonc101.starr_datalake2018.mar` as mar 
+  LEFT JOIN `som-nero-phi-jonc101.starr_datalake2018.order_med` as medord on mar.order_med_id_coded=medord.order_med_id_coded 
+  WHERE mar.order_med_id_coded in 
+  (SELECT medord2.order_med_id_coded FROM `som-nero-phi-jonc101.starr_datalake2018.order_med` as medord2
+       WHERE UPPER(medord2.med_description) LIKE '%INSULIN%'-- insulin ordered)
+       AND (medord2.ordering_mode_c) = 2 -- inpatient
+       AND UPPER(medord2.med_description) NOT LIKE '%PUMP%' --excludes pumps
+       AND (medord2.med_description) NOT IN ("INSULIN NPH HUMAN RECOMB 100 UNIT/ML SC CRTG", "INSULIN LISPRO PROTAM-LISPRO 100 UNIT/ML (50-50) SC SUSP", "INSULIN GLARGINE 300 UNIT/ML (3 ML) SC INPN", "INSULIN NPH & REGULAR HUMAN 100 UNIT/ML (70-30) SC CRTG", "INSULIN NPH-REGULAR HUM S-SYN 100 UNIT/ML (70-30) SC CRTG", "INSULIN ASP PRT-INSULIN ASPART 100 UNIT/ML (70-30) SC SOLN", "INSULIN LISPRO 100 UNIT/ML SC INPH", "INSULIN NPH HUMAN SEMI-SYN 100 UNIT/ML SC CRTG", "INSULIN ASPART PROTAMINE-ASPART (70/30) 100 UNIT/ML SUBCUTANEOUS PEN", "INSULIN LISPRO PROTAM-LISPRO 100 UNIT/ML (75-25) SC SUSP", "INSULIN GLULISINE 100 UNIT/ML SC CRTG", "INSULIN DEGLUDEC-LIRAGLUTIDE 100 UNIT-3.6 MG /ML (3 ML) SC INPN", "INSULIN LISPRO PROTAM & LISPRO 100 UNIT/ML (75-25) SC SUSP", "INSULIN REGULAR HUM U-500 CONC 500 UNIT/ML SC SOLN", "INSULIN LISPRO 100 UNIT/ML SC CRTG", "INSULIN ASPART 100 UNIT/ML SC CRTG") -- removes anything ordered <10 times
+       AND medord2.med_description NOT LIKE "%CRTG%" -- excludes anything ordered as a cartridge to remove pumps
+       AND medord2.med_route = 'Subcutaneous') --and subQ
+     AND (mar.mar_action) IN ('Given') --medication actually given
+   AND (mar.mar_action) NOT IN ('Bag Removal', 'Canceled Entry', 'Due', 'Existing Bag', 'Infusion Restarted', 'Infusion Started', 'Infusion Stopped', 'New Bag', 'Patch Removal', 'Patient\'s Own Med', 'Patient/Family Admin', 'Paused', 'Pending', 'Rate Changed', 'Rate Verify', 'Pump%', 'See Anesthesia Record', 'Self Administered Med', 'See Override Pull','Refused', 'Held', 'Stopped', 'Missed', 'Bolus', 'Complete', 'Completed', 'Push')
+   AND mar.dose_unit_c = 5 -- "units", not an infusion (not units/hr)
+   AND mar.sig <> "0-10"
+   AND CAST(mar.sig AS float64) > 0
+   AND mar.sig IS NOT NULL 
+   AND mar.sig NOT LIKE "%.%" -- removes any partial unit injections (assumed to be pump)
+   AND mar.infusion_rate IS NULL -- infusion_rate assumed to signify pump pt
+   AND mar.pat_enc_csn_id_coded NOT IN (SELECT lab.pat_enc_csn_id_coded FROM `som-nero-phi-jonc101.starr_datalake2018.lab_result` as lab -- excludes patient encounters with creatinine >2 
+    WHERE (lab_name) LIKE "Creatinine, Ser/Plas" --most common creatinine order
+    AND ord_num_value != 9999999
+    AND taken_time_jittered IS NOT null
+    AND ord_num_value > 2)
 
 
 
