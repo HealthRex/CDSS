bf3b796244b7f3f7e168eac1d107e274619efb78
Ivana Jankovic <Ivana@DNab42167d.stanford.edu>
2020-02-11 15:40:12 -0800

    verified subqueries for cohort building

--- scripts/InpatientDiabetes/TDD_subqueries.sql
+++ scripts/InpatientDiabetes/TDD_subqueries.sql
@@ -1,5 +1,7 @@
 
--- Patients receiving 1-100u per dose inpatient subQ insulin that is not a pump, U500, or a rare order
+-- 
+-- Patients receiving 1-100u per dose inpatient subQ insulin that is not a pump, U500, or a rare order (<25 orders in dataset)
+-- Verified that dose was given, dose 1-100
 
 WITH inpt_insulin_given AS (
 
@@ -10,7 +12,10 @@ SELECT mar.jc_uid, mar.pat_enc_csn_id_coded, mar.sig as units, mar.mar_action, m
        WHERE UPPER(medord2.med_description) LIKE '%INSULIN%'-- insulin ordered)
        AND (medord2.ordering_mode_c) = 2 -- inpatient
        AND UPPER(medord2.med_description) NOT LIKE '%PUMP%' --excludes pumps
+       AND medord2.med_description NOT LIKE "%CRTG%" -- excludes anything ordered as a cartridge to remove pumps
+       AND medord2.med_description NOT LIKE "%U-500%" -- exlcudes U-500
        AND (medord2.med_description) NOT IN ("INSULIN NPH HUMAN RECOMB 100 UNIT/ML SC CRTG", "INSULIN LISPRO PROTAM-LISPRO 100 UNIT/ML (50-50) SC SUSP", "INSULIN GLARGINE 300 UNIT/ML (3 ML) SC INPN", "INSULIN NPH & REGULAR HUMAN 100 UNIT/ML (70-30) SC CRTG", "INSULIN NPH-REGULAR HUM S-SYN 100 UNIT/ML (70-30) SC CRTG", "INSULIN ASP PRT-INSULIN ASPART 100 UNIT/ML (70-30) SC SOLN", "INSULIN LISPRO 100 UNIT/ML SC INPH", "INSULIN NPH HUMAN SEMI-SYN 100 UNIT/ML SC CRTG", "INSULIN ASPART PROTAMINE-ASPART (70/30) 100 UNIT/ML SUBCUTANEOUS PEN", "INSULIN LISPRO PROTAM-LISPRO 100 UNIT/ML (75-25) SC SUSP", "INSULIN GLULISINE 100 UNIT/ML SC CRTG", "INSULIN DEGLUDEC-LIRAGLUTIDE 100 UNIT-3.6 MG /ML (3 ML) SC INPN", "INSULIN LISPRO PROTAM & LISPRO 100 UNIT/ML (75-25) SC SUSP", "INSULIN REGULAR HUM U-500 CONC 500 UNIT/ML SC SOLN", "INSULIN LISPRO 100 UNIT/ML SC CRTG", "INSULIN ASPART 100 UNIT/ML SC CRTG") -- removes anything ordered <10 times
+      AND (medord2.med_description) NOT IN ("INSULIN REGULAR 100 UNIT/ML SC VERY AGGRESSIVE SCALE FEEDING (BEDTIME)", "INSULIN GLULISINE 100 UNIT/ML SC SOLN", "INSULIN DEGLUDEC 200 UNIT/ML (3 ML) SC INPN", "INSULIN GLARGINE 300 UNIT/3 ML SC INPN", "INSULIN LISPRO PROTAM & LISPRO 100 UNIT/ML (75-25) SC INPN", "INSULIN GLARGINE 100 UNIT/ML (3 ML) SC INPN", " INV - INSULIN LISPRO SC (ORAL DIETS/BOLUS FEEDS) INTERVENTION", "INSULIN LISPRO PROTAMIN-LISPRO 100 UNIT/ML (75-25) SC INPN", "INSULIN ASPART 100 UNIT/ML AGGRESSIVE SCALE FEEDING", "INSULIN GLARGINE 300 UNIT/ML (1.5 ML) SC INPN") -- removes any insulin ordered < 25 times, conveniently excluding any U200 or U300
       AND medord2.med_description NOT LIKE "%CRTG%" -- excludes anything ordered as a cartridge to remove pumps
       AND medord2.med_route = 'Subcutaneous'
       ) --and subQ
@@ -23,5 +28,4 @@ SELECT mar.jc_uid, mar.pat_enc_csn_id_coded, mar.sig as units, mar.mar_action, m
     AND mar.sig IS NOT NULL 
     AND mar.sig NOT LIKE "%.%" -- removes any partial unit injections (assumed to be pump)
     AND mar.infusion_rate IS NULL -- infusion_rate assumed to signify pump pt
-   )
-
+  )
