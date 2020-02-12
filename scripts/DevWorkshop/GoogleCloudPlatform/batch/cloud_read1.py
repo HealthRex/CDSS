f2fc304b5241b72a01e19bc2bf6a714f986bf925
Jonathan Chiang <13003102+JonathanChiang@users.noreply.github.com>
2020-02-11 21:02:02 -0800

    batch read upload

---
+++ scripts/DevWorkshop/GoogleCloudPlatform/batch/cloud_read1.py
@@ -0,0 +1,12 @@
+from google.cloud import bigquery
+from medinfo.db.bigquery import bigQueryUtil
+import datetime
+import pandas
+
+bq_client = bigQueryUtil.BigQueryClient()
+sql = "select * from  `datalake_47618_sample.order_med` where med_description =  'ZOLOFT PO' "
+query1 = bq_client.queryBQ(sql)
+df = query1.to_dataframe()
+file_name = str(datetime.datetime.now()) + '.csv'
+df.to_csv(file_name, encoding='utf-8', index=False)
+print query1.to_dataframe()
