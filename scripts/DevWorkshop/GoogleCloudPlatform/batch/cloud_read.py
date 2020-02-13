576b51e2bb18814fd89b6e9cae7f941df7d7f0da
Jonathan Chiang <13003102+JonathanChiang@users.noreply.github.com>
2020-02-13 14:58:25 -0800

    including sleep argument

--- scripts/DevWorkshop/GoogleCloudPlatform/batch/cloud_read.py
+++ scripts/DevWorkshop/GoogleCloudPlatform/batch/cloud_read.py
@@ -7,18 +7,22 @@ import time
 
 bq_client = bigQueryUtil.BigQueryClient()
 
+# sleep  time 
+a1 = sys.argv[1] 
+
 # letter that you searching for  first 
-a1 = sys.argv[1]
+a2 = sys.argv[1]
 
 # number of rows
-a2 = sys.argv[2]
+a3 = sys.argv[2]
 
-sql = ["select count(med_description) as med_count, med_description from datalake_47618.order_med where lower(med_description) like \'"  , a1 , "%' group by med_description order by med_count  desc limit ", a2 ]
+
+sql = ["select count(med_description) as med_count, med_description from datalake_47618.order_med where lower(med_description) like \'"  , a2 , "%' group by med_description order by med_count  desc limit ", a3 ]
 sql1 = ''.join(sql)
 query1 = bq_client.queryBQ(sql1)
 df = query1.to_dataframe()
 
 for row_index,row in df.iterrows():
    print '\nrow number:',row_index, '\n-------------' 
-   time.sleep(1)
+   time.sleep(a1)
    print row
