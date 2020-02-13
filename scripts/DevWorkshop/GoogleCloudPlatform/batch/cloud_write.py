db452622a36c8c2fac6c6cc90f3f80ae4c062734
Jonathan Chiang <13003102+JonathanChiang@users.noreply.github.com>
2020-02-12 17:13:54 -0800

    update cat list

--- scripts/DevWorkshop/GoogleCloudPlatform/batch/cloud_write.py
+++ scripts/DevWorkshop/GoogleCloudPlatform/batch/cloud_write.py
@@ -1,10 +1,13 @@
 t = []
+f = []
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' , 'k' , 'l', 'i', 'm', 'n', 'o', 'p', 'q', 'r',  's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 for i  in letters:
-  t.append((str('nohup python2 -u cloud_read10.py ' +  i + str(' 100') + ' &> results.log  &')))
-
+  t.append((str('nohup python2 -u cloud_read10.py ' +  i)))
+for j in range(1,27):
+   f.append(str(' 100') + ' &> results' + str(j) + '.log  &')
+z = t + f 
 outF = open("cloud_log.sh", "w")
-for line in t:
+for line in z:
   # write line to output file
   outF.write(line)
   outF.write("\n")
