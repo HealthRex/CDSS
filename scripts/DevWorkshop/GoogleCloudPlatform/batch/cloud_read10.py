9ef79fa91c9f0cc5036a25e15fdb266e6a2f3ba7
Jonathan Chiang <13003102+JonathanChiang@users.noreply.github.com>
2020-02-12 16:26:16 -0800

    print statement debug

--- scripts/DevWorkshop/GoogleCloudPlatform/batch/cloud_read10.py
+++ scripts/DevWorkshop/GoogleCloudPlatform/batch/cloud_read10.py
@@ -9,6 +9,7 @@ bq_client = bigQueryUtil.BigQueryClient()
 a1 = sys.argv[0]
 sql = ["select count(med_description) as med_count, med_description from datalake_47618.order_med where lower(med_description) like \'"  , a1 , "%' group by med_description order by med_count  desc limit 100 "]
 sql1 = ''.join(sql)
+print  sql1 
 query1 = bq_client.queryBQ(sql1)
 df = query1.to_dataframe()
 
