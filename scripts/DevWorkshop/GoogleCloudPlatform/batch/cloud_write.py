d743ef90b7eaaaa67832aad493df1d0e6b1c9249
Jonathan Chiang <13003102+JonathanChiang@users.noreply.github.com>
2020-02-12 17:19:43 -0800

    zipping lists

--- scripts/DevWorkshop/GoogleCloudPlatform/batch/cloud_write.py
+++ scripts/DevWorkshop/GoogleCloudPlatform/batch/cloud_write.py
@@ -5,7 +5,7 @@ for i  in letters:
   t.append((str('nohup python2 -u cloud_read10.py ' +  i)))
 for j in range(1,27):
    f.append(str(' 100') + ' &> results' + str(j) + '.log  &')
-z = t + f 
+z = [x + y for x,y in zip(t,f)]
 outF = open("cloud_log.sh", "w")
 for line in z:
   # write line to output file
