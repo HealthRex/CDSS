d35373136708831b9aefd14dc3249354f2411dcd
Jonathan Chiang <13003102+JonathanChiang@users.noreply.github.com>
2020-02-12 16:37:33 -0800

    spacing on argument

--- scripts/DevWorkshop/GoogleCloudPlatform/batch/cloud_read10.py
+++ scripts/DevWorkshop/GoogleCloudPlatform/batch/cloud_read10.py
@@ -13,7 +13,7 @@ a1 = sys.argv[1]
 # number of rows
 a2 = sys.argv[2]
 
-sql = ["select count(med_description) as med_count, med_description from datalake_47618.order_med where lower(med_description) like \'"  , a1 , "%' group by med_description order by med_count  desc limit", a2 ]
+sql = ["select count(med_description) as med_count, med_description from datalake_47618.order_med where lower(med_description) like \'"  , a1 , "%' group by med_description order by med_count  desc limit ", a2 ]
 sql1 = ''.join(sql)
 query1 = bq_client.queryBQ(sql1)
 df = query1.to_dataframe()
