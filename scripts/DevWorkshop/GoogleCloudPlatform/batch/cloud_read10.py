1173325e76f1b9eef9eca66baa1b2050394b6ff3
Jonathan Chiang <13003102+JonathanChiang@users.noreply.github.com>
2020-02-12 16:35:14 -0800

    adding two arguments

--- scripts/DevWorkshop/GoogleCloudPlatform/batch/cloud_read10.py
+++ scripts/DevWorkshop/GoogleCloudPlatform/batch/cloud_read10.py
@@ -6,10 +6,15 @@ import sys
 import time
 
 bq_client = bigQueryUtil.BigQueryClient()
+
+# letter that you searching for  first 
 a1 = sys.argv[1]
-sql = ["select count(med_description) as med_count, med_description from datalake_47618.order_med where lower(med_description) like \'"  , a1 , "%' group by med_description order by med_count  desc limit 100 "]
+
+# number of rows
+a2 = sys.arv[2]
+
+sql = ["select count(med_description) as med_count, med_description from datalake_47618.order_med where lower(med_description) like \'"  , a1 , "%' group by med_description order by med_count  desc limit", a2 ]
 sql1 = ''.join(sql)
-print  sql1 
 query1 = bq_client.queryBQ(sql1)
 df = query1.to_dataframe()
 
