827ea76ccb37be47f30769df5add1d86ed065a77
Jonathan Chiang <chiang810@gmail.com>
2020-02-19 07:43:40 -0800

    fixing type to int

--- scripts/DevWorkshop/GoogleCloudPlatform/sleep_loop.py
+++ scripts/DevWorkshop/GoogleCloudPlatform/sleep_loop.py
@@ -1,9 +1,8 @@
 import time
 import sys
-t = sys.argv[0] 
+t = int(sys.argv[0]) 
 for i in range(0,t):
 	while i < t:
 		time.sleep(1)
-		print('step', i + 1) 
-		break 
-    
+		print('step', i + 1)
+		break
