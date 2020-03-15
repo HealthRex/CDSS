ca5e220c693259904c5c9b366887dcec1e5094e2
Yenaled <delaneyk.sullivan@gmail.com>
2020-03-15 14:40:55 -0700

    Update stratify_data_by_time.py

--- scripts/ClinicNet/data_processing/stratify_data_by_time.py
+++ scripts/ClinicNet/data_processing/stratify_data_by_time.py
@@ -7,10 +7,7 @@ import gc
 import sys, getopt
 import multiprocessing
 
-def remove_items(f):
-        date_threshold = 1325376000000000000
-        remove_greater = True
-        
+def remove_items(f):        
         # Read in data
         data_s = pd.read_hdf(data_dir + "/" + f, 'data_s')
         data_x = pd.read_hdf(data_dir + "/" + f, 'data_x')
@@ -20,14 +17,14 @@ def remove_items(f):
         data_y = data_y.reset_index(drop=True)
         
         # Remove data rows based on date threshold
-        rows_to_remove = None
+        rows_to_keep = None
         if remove_greater:
-                rows_to_remove = list(data_s.loc[data_s.loc[:,"item_date"] >= date_threshold,:].index)
+                rows_to_keep = list(data_s.loc[data_s.loc[:,"item_date"] < date_threshold,:].index)
         else:
-                rows_to_remove = list(data_s.loc[data_s.loc[:,"item_date"] < date_threshold,:].index)
-        data_s = data_s.loc[rows_to_remove,:]
-        data_x = data_x.loc[rows_to_remove,:]
-        data_y = data_y.loc[rows_to_remove,:]
+                rows_to_keep = list(data_s.loc[data_s.loc[:,"item_date"] >= date_threshold,:].index)
+        data_s = data_s.loc[rows_to_keep,:]
+        data_x = data_x.loc[rows_to_keep,:]
+        data_y = data_y.loc[rows_to_keep,:]
         
         # Write output to file
 	data_x.to_hdf(output_dir + "/" + f, key='data_x', mode='w', complevel=1)
@@ -54,18 +51,21 @@ def main(argv):
                 if opt == '-h':
                         print('stratify_data_by_time.py -i <data_directory> -o <output_dir> -t <timestamp> [-p num_processes] [-g] [-h]')
                         print('')
-                        print('This script performs processes data files (in HDF5 format), from <data_directory>, to add item_date timestamps based on an patient item to item date mapping file.')
-                        print('The new data will be appended to existing data files in <data_directory>.')
-                        print('One hdf5 file nees to be specified:')
-                        print('1. <patient_itemdate_mapping_file>: data frame of patient items with columns being item_date (as nanoseconds since epoch) and patient_item_id')
+                        print('This script performs processes data files (in HDF5 format), from <data_directory>, to filter rows based on date nanoseconds-since-epoch timestamp <timestamp>.')
+                        print('The new data will be outputed to data files in <output_dir>.')
+                        print('Use -g to specify removal of rows with timestamp >= threshold; otherwise rows with timestamp < threshold will be removed.')
                         print('Use num_processes to specify the number of processes to use for multiprocessing.')
                         sys.exit()
                 elif opt == '-i':
                         data_dir = arg
+                elif opt == '-o':
+                        output_dir = arg
+                elif opt == '-t':
+                        date_threshold = int(arg)
                 elif opt == '-p':
                         num_processes = int(arg)
-                elif opt == '-d':
-                        dates_dir = arg
+                elif opt == '-g':
+                        remove_greater = True
         if len(argv) < 3 or data_dir == '' or output_dir == '' or date_threshold <= 0:
                 print('stratify_data_by_time.py -i <data_directory> -o <output_dir> -t <timestamp> [-p num_processes] [-g] [-h]')
                 sys.exit(2)
@@ -75,13 +75,10 @@ def main(argv):
                 num_processes = multiprocessing.cpu_count()-1
         print("Number of processes: " + str(num_processes))
 
-        # Setup date time stamps
-        all_item_dates = pd.read_hdf(dates_dir)
-
         # Iterate through data_dir to process the files
         files_list = os.listdir(data_dir)
         pool = multiprocessing.Pool(num_processes)
-        pool.map(add_dates, files_list)
+        pool.map(remove_items, files_list)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
