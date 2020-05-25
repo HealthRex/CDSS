f1e36e075f802da83ebca76da9d0c2fa2aa1ac75
Jonathan H Chen <jonc101@hotmail.com>
2020-05-25 12:09:50 -0700

    Consolidate unit test workshop sample code into a subdirectory and update to reflect Python 3.6+ with additional notes and example to illustrate command-line argument parsing and setUp, tearDown of a temporary file.

--- scripts/DevWorkshop/UnitTesting/TestApplicationModule.py
+++ scripts/DevWorkshop/UnitTesting/TestApplicationModule.py
@@ -1,45 +1,62 @@
 #!/usr/bin/env python
-"""Test case for respective module in application package"""
+"""Test case for respective module in application package
+
+!!!! This is test demonstration code that goes with the TestFirstDev workshop.
+!!!! There are supposed to be bugs in the code. 
+!!!! Do NOT commit the fixes back into the code repository.
+!!!! The point is to allow new people to encounter, identify, and debug them.
+"""
 
 import sys, os
 from io import StringIO
 import unittest
 
-from .ApplicationModule import ApplicationClass;
+from ApplicationModule import ApplicationClass;
 
 RUNNER_VERBOSITY=2; # How much log text you want the test runner to express
+
+TEMP_FILENAME = "tempData.TestApplicationModule.txt";
+SAMPLE_INPUT_FILE_STR = \
+"""To be, or not to be, Ay there's the point,
+To Die, to sleep, is that all? Aye all:
+No to sleep, to dream, aye marry there it goes,
+For in that dream of death, when we awake,
+"""
 
 class TestApplicationModule(unittest.TestCase):
     def setUp(self):
         """Prepare state for test cases.
         For example, preparing test input files, database connections and sample data.
         """
-        unittest.TestCase.setUp(self);
+        unittest.TestCase.setUp(self);  # Superclass setUp
         
         # Instance to test on
         self.app = ApplicationClass();
 
-        # Sample input file string. Convert into a "file" format by wrapping a StringIO object around it
-        self.SAMPLE_INPUT_FILE_STR = \
-        """To be, or not to be, Ay there's the point,
-To Die, to sleep, is that all? Aye all:
-No to sleep, to dream, aye marry there it goes,
-For in that dream of death, when we awake,
-"""
+        # Sample input lines.
+        # Can convert into a "file" by wrapping a StringIO object around it, 
+        #   but will illustrate creating temp file here
+        self.tempFile = open(TEMP_FILENAME,"w");
+        self.tempFile.write(SAMPLE_INPUT_FILE_STR);
+        self.tempFile.close();
+
     def tearDown(self):
         """Restore state from any setUp or test steps.
         For example, deleting any temp files or temp data records in the database.
         """
-        unittest.TestCase.tearDown(self);
+        if os.path.exists(TEMP_FILENAME):
+            os.remove(TEMP_FILENAME);
+        unittest.TestCase.tearDown(self);   # Superclass tearDown
 
     def test_extractWordsByIndex(self):
         # Prepare sample input into application code
-        wordIndex = 0;
-        inputFile = StringIO(self.SAMPLE_INPUT_FILE_STR);
+        wordIndex = 1;
+        inputFile = open(TEMP_FILENAME);
         outputFile = StringIO();    # Empty IO object can write to like a file, but will just save as an in memory string
         
         # Run application code against sample input, and collect output
         self.app.extractWordsByIndex(wordIndex, inputFile, outputFile);
+        inputFile.close();
         actualOutputFileStr = outputFile.getvalue();
         actualOutputList = actualOutputFileStr[:-1].split("\n"); # Get one value per line, ignoring the ending newline
         
@@ -51,12 +68,13 @@ For in that dream of death, when we awake,
 
         ######## More test iterations ########
         # Prepare sample input into application code
-        wordIndex = 2;
-        inputFile = StringIO(self.SAMPLE_INPUT_FILE_STR);
+        wordIndex = 3;
+        inputFile = open(TEMP_FILENAME);
         outputFile = StringIO();    # Empty IO object can write to like a file, but will just save as an in memory string
         
         # Run application code against sample input, and collect output
         self.app.extractWordsByIndex(wordIndex, inputFile, outputFile);
+        inputFile.close();
         actualOutputFileStr = outputFile.getvalue();
         actualOutputList = actualOutputFileStr[:-1].split("\n"); # Get one value per line, ignoring the ending newline
         
@@ -68,12 +86,13 @@ For in that dream of death, when we awake,
 
         ######## More test iterations ########
         # Prepare sample input into application code
-        wordIndex = 9;
-        inputFile = StringIO(self.SAMPLE_INPUT_FILE_STR);
+        wordIndex = 10;
+        inputFile = open(TEMP_FILENAME);
         outputFile = StringIO();    # Empty IO object can write to like a file, but will just save as an in memory string
         
         # Run application code against sample input, and collect output
         self.app.extractWordsByIndex(wordIndex, inputFile, outputFile);
+        inputFile.close();
         actualOutputFileStr = outputFile.getvalue();
         actualOutputList = actualOutputFileStr[:-1].split("\n"); # Get one value per line, ignoring the ending newline
         
@@ -86,7 +105,7 @@ For in that dream of death, when we awake,
         ######## More test iterations ########
         # Prepare sample input into application code
         wordIndex = 1.2;    # Invalid index. Must be an integer
-        inputFile = StringIO(self.SAMPLE_INPUT_FILE_STR);
+        inputFile = open(TEMP_FILENAME);
         outputFile = StringIO();    # Empty IO object can write to like a file, but will just save as an in memory string
         
         # Run application code against sample input, and collect output
@@ -98,6 +117,7 @@ For in that dream of death, when we awake,
             actualOutputList = actualOutputFileStr[:-1].split("\n"); # Get one value per line, ignoring the ending newline
         except TypeError:
             actualError = True;
+        inputFile.close();
         # In this case, expect an error to have occurred (invalid wordIndex)
         self.assertEqual(expectError, actualError);
 
