33e3a0df0e3d21fdc6f68a0fbbbcbccfb0479500
Ivana Jankovic <Ivana@DN2lk5car.stanford.edu>
2020-10-15 17:20:18 -0700

    indented

--- scripts/InpatientDiabetes/TDD prediction/Patients_with_TxTeam_as_ICU.sql
+++ scripts/InpatientDiabetes/TDD prediction/Patients_with_TxTeam_as_ICU.sql
@@ -2,25 +2,26 @@ SELECT extract(year from trtmnt_tm_begin_dt_jittered) as asgnment_yr, count(dist
 
 FROM `som-nero-phi-jonc101.shc_core.treatment_team`
 
-WHERE prov_map_id IN ('S176',
-'S358',
-'SUNK00008',
-'S400',
-'SG1000003',
-'SG1000007',
-'SG1000005',
-'SG1000009',
-'S1398',
-'S3395',
-'S3399',
-'S3396',
-'SG1000041',
-'SG1000044',
-'S224',
-'SG1000046',
-'SG1000031',
-'SG1000027',
-'S19626') -- Removed NSurg, ICU, CCU, removed anything with <100  
+WHERE prov_map_id IN ( -- Removed NSurg, removed anything with <100  
+	'S176',
+	'S358',
+	'SUNK00008',
+	'S400',
+	'SG1000003',
+	'SG1000007',
+	'SG1000005',
+	'SG1000009',
+	'S1398',
+	'S3395',
+	'S3399',
+	'S3396',
+	'SG1000041',
+	'SG1000044',
+	'S224',
+	'SG1000046',
+	'SG1000031',
+	'SG1000027',
+	'S19626') 
 
 AND name = 'Primary Team'
 
