0bc3c3ed81a6185f1e7b07dee108f5677e1d8c7c
Jonathan H Chen <jonc101@gmail.com>
2021-02-18 00:31:17 -0800

    Remove debug print statement

--- medinfo/web/cgibin/cpoe/ItemRecommenderWeb.py
+++ medinfo/web/cgibin/cpoe/ItemRecommenderWeb.py
@@ -200,7 +200,6 @@ class ItemRecommenderWeb(BaseCPOEWeb):
         self.requestData["resultsInfo"] += "(%1.3f seconds)" % timer
 
 if __name__ == "__main__":
-    print("hello", file=sys.stderr)
     webController =  ItemRecommenderWeb()
     webController.handleRequest(cgi.FieldStorage())
 
