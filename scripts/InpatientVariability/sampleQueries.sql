cfc2ebc0c64b0caa9cae6eff9eb8738ad2327ab8
Jonathan Chen <jonc101@gmail.com>
2021-10-12 14:54:36 -0700

    Generalize labeling to be any admission cohort, not necessarily surgery

--- scripts/InpatientVariability/sampleQueries.sql
+++ scripts/InpatientVariability/sampleQueries.sql
@@ -1,26 +1,27 @@
--- (Elective) Orthopedic Surgery as Example
+-- (Elective Ortho Surgery) Admission as Example
 WITH
 
 -- Identify Cases by Diagnosis Related Group
-surgeryDiagnosisRelatedGroup AS
+cohortDiagnosisRelatedGroup AS
 (
     select *
     from `mining-clinical-decisions.shc_core.drg_code`
-    where drg_mpi_code in 
+    /*where drg_mpi_code in 
     (   '301' -- Hip Joint Replacement
         --'470',    -- Hip and Knee Joint Replacement or Reattachment w/o MCC    14K encounters
         --'301',    -- Hip Joint Replacement    9.7K encounters
         --'302'     -- Knee Joint Replacement   8.6K encounters
     )
+    */
 ),
 
--- Find all Specialty Primary Team Assignments
-surgeryPrimaryEncounter AS
+-- Find all Specialty Primary cohort Assignments
+cohortPrimaryEncounter AS
 (
     select distinct anon_id, pat_enc_csn_id_coded
     from `mining-clinical-decisions.shc_core.treatment_team`
     where name = 'Primary Team'
-    and prov_name like 'ORTHO ARTHRITIS%'
+    --and prov_name like 'ORTHO ARTHRITIS%'
     -- Up to 1,000 cases per year
 ),
 
@@ -32,29 +33,29 @@ emergencyAdmissionEncounter AS
     where pat_service_c in ('100','187') -- Emergency and Emergency Services
     and in_event_type_c = 1 -- Admission
 ),
-surgeryNonEmergencyEncounter AS
+cohortNonEmergencyEncounter AS
 (
     select anon_id, pat_enc_csn_id_coded
-    from surgeryPrimaryEncounter 
+    from cohortPrimaryEncounter 
     EXCEPT DISTINCT
     select anon_id, pat_enc_csn_id_coded
     from emergencyAdmissionEncounter 
 ),
-surgeryNonEmergencyEncounterByYear AS
+cohortNonEmergencyEncounterByYear AS
 (
     select EXTRACT(YEAR FROM trtmnt_tm_begin_dt_jittered) as assignYear, count(distinct anon_id) nPatients, count(distinct pat_enc_csn_id_coded) as nEncounters
-    from surgeryNonEmergencyEncounter join shc_core.treatment_team using (anon_id, pat_enc_csn_id_coded)
+    from cohortNonEmergencyEncounter join shc_core.treatment_team using (anon_id, pat_enc_csn_id_coded)
     group by assignYear
     order by assignYear desc
     -- Close to 1,000 patients per year, confirming most of these admissions are elective / non-emergency
 ),
 
 -- Find top Admission Diagnoses for those Encounters
-surgeryDiagnosisCounts AS
+cohortDiagnosisCounts AS
 (  
     select source, icd9, icd10, dx_name, count(distinct snee.anon_id) as nPatients, count(distinct snee.pat_enc_csn_id_coded) as nEncounters, count(*) as nDiagnoses
     from `mining-clinical-decisions.shc_core.diagnosis_code` as dc 
-        join surgeryNonEmergencyEncounter as snee on (snee.anon_id = dc.anon_id and snee.pat_enc_csn_id_coded = dc.pat_enc_csn_id_jittered)
+        join cohortNonEmergencyEncounter as snee on (snee.anon_id = dc.anon_id and snee.pat_enc_csn_id_coded = dc.pat_enc_csn_id_jittered)
     group by source, icd9, icd10, dx_name
     order by nPatients desc
 ),
@@ -67,25 +68,25 @@ diagnosisRelatedGroupCounts AS
 ),
 
 -- Calculate Length of Stay Distribution for Top Admission Diagnosis
-surgeryDiagnosisRelatedGroupLengthOfStay AS
+cohortDiagnosisRelatedGroupLengthOfStay AS
 (
     select drg_mpi_code, drg_name, anon_id, pat_enc_csn_id_coded, 
         min(event_time_jittered) as firstEventTime,
         DATETIME_DIFF(max(event_time_jittered), min(event_time_jittered), HOUR) as lengthOfStayHour, 
         ROUND(DATETIME_DIFF(max(event_time_jittered), min(event_time_jittered), HOUR) / 24, 2) as lengthOfStayDay,
-    from surgeryDiagnosisRelatedGroup
-        join surgeryNonEmergencyEncounter using (anon_id, pat_enc_csn_id_coded)
+    from cohortDiagnosisRelatedGroup
+        join cohortNonEmergencyEncounter using (anon_id, pat_enc_csn_id_coded)
         join shc_core.adt using (anon_id, pat_enc_csn_id_coded) -- Join ADT data for first and last event time to estimate length of stay
     group by drg_mpi_code, drg_name, anon_id, pat_enc_csn_id_coded
 ),
-surgeryDRGLengthOfStayHistogram AS
+cohortDRGLengthOfStayHistogram AS
 (
     select 
         --EXTRACT(YEAR FROM firstEventTime) as admitYear,
         drg_mpi_code, drg_name,
         FLOOR(lengthOfStayHour/24) as lengthOfStayDayMin, FLOOR(lengthOfStayHour/24)+1 as lengthOfStayDayMax,
         count(distinct anon_id) as nPatients, count(distinct pat_enc_csn_id_coded) as nEncounters
-    from surgeryDiagnosisRelatedGroupLengthOfStay
+    from cohortDiagnosisRelatedGroupLengthOfStay
     group by 
         --admitYear, 
         drg_mpi_code, drg_name, lengthOfStayDayMin, lengthOfStayDayMax
@@ -105,5 +106,5 @@ spacer AS ( select * from `mining-clinical-decisions.shc_core.demographic`)
 
 
 select *
-from surgeryDRGLengthOfStayHistogram
-limit 100
+from cohortDRGLengthOfStayHistogram
+limit 1009
