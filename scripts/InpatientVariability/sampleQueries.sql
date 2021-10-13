a9e66262cfb59959a508f2d475af6547241f526d
Jonathan Chen <jonc101@gmail.com>
2021-10-12 20:24:36 -0700

    Break out length of stay summary stats by provider and allow for histogram view

--- scripts/InpatientVariability/sampleQueries.sql
+++ scripts/InpatientVariability/sampleQueries.sql
@@ -70,29 +70,90 @@ diagnosisRelatedGroupCounts AS
 -- Calculate Length of Stay Distribution for Top Admission Diagnosis
 cohortDiagnosisRelatedGroupLengthOfStay AS
 (
-    select drg_mpi_code, drg_name, anon_id, pat_enc_csn_id_coded, 
+    select 
+        drg_mpi_code, drg_name, anon_id, pat_enc_csn_id_coded, 
+        admn_prov_map_id, visit_prov_map_id,
         min(event_time_jittered) as firstEventTime,
         DATETIME_DIFF(max(event_time_jittered), min(event_time_jittered), HOUR) as lengthOfStayHour, 
         ROUND(DATETIME_DIFF(max(event_time_jittered), min(event_time_jittered), HOUR) / 24, 2) as lengthOfStayDay,
+        ROUND(DATETIME_DIFF(max(event_time_jittered), min(event_time_jittered), HOUR) / 24) as lengthOfStayDayFloor
     from cohortDiagnosisRelatedGroup
         join cohortNonEmergencyEncounter using (anon_id, pat_enc_csn_id_coded)
         join shc_core.adt using (anon_id, pat_enc_csn_id_coded) -- Join ADT data for first and last event time to estimate length of stay
-    group by drg_mpi_code, drg_name, anon_id, pat_enc_csn_id_coded
+        join shc_core.encounter using (anon_id, pat_enc_csn_id_coded)
+    group by 
+        drg_mpi_code, drg_name, anon_id, pat_enc_csn_id_coded,
+        admn_prov_map_id, visit_prov_map_id
 ),
+-- Calculate summary stats per diagnosis 
+cohortDRGLengthOfStaySummary AS
+(
+    select 
+        drg_mpi_code, drg_name, 
+        AVG(lengthOfStayDay) AS avgLengthOfStayDay, STDDEV(lengthOfStayDay) as stddevLengthOfStayDay,
+        COUNT(distinct anon_id) as nPatients, COUNT(distinct pat_enc_csn_id_coded) as nEncounters,
+        STDDEV(lengthOfStayDay) * COUNT(distinct pat_enc_csn_id_coded) AS variabilityImpact,
+        SUM(lengthOfStayDay) as totalDays
+    from cohortDiagnosisRelatedGroupLengthOfStay
+    group by 
+        drg_mpi_code, drg_name
+    order by 
+        stddevLengthOfStayDay * nEncounters desc -- Potential impact? Volume * variability
+),
+-- Break out diagnosis summary stats by admitting provider for top diagnoses...
+cohortDRGLengthOfStaySummaryByProvider AS
+(
+    select 
+        drg_mpi_code, drg_name, 
+        admn_prov_map_id,
+        AVG(lengthOfStayDay) AS avgLengthOfStayDay, STDDEV(lengthOfStayDay) as stddevLengthOfStayDay,
+        COUNT(distinct anon_id) as nPatients, COUNT(distinct pat_enc_csn_id_coded) as nEncounters,
+        STDDEV(lengthOfStayDay) * COUNT(distinct pat_enc_csn_id_coded) AS variabilityImpact,
+        SUM(lengthOfStayDay) as totalDays
+    from cohortDiagnosisRelatedGroupLengthOfStay
+    -- where drg_mpi_code = '021' -- Restrict to one diagnosis at a time (e.g., Craniotomy) to make it easier to parse
+    group by 
+        drg_mpi_code, drg_name,
+        admn_prov_map_id
+    having nPatients > 10 -- Only inspect providers who've had a non-trival number of admissions
+    order by 
+        --avgLengthOfStayDay DESC
+        drg_mpi_code, drg_name,
+        stddevLengthOfStayDay * nEncounters desc -- Potential impact? Volume * variability
+),
+-- Join above two so able to compare total DRG averages vs per provider average
+cohortDRGLengthOfStaySummaryByProviderAndTotal AS
+(
+    select 
+        drg_mpi_code, drg_name, 
+        admn_prov_map_id,
+        byProv.avgLengthOfStayDay, byProv.stddevLengthOfStayDay, byProv.nPatients, byProv.nEncounters, byProv.variabilityImpact, byProv.totalDays,
+        byDRG.avgLengthOfStayDay, byDRG.stddevLengthOfStayDay, byDRG.nPatients, byDRG.nEncounters, byDRG.variabilityImpact, byDRG.totalDays
+    from 
+        cohortDRGLengthOfStaySummaryByProvider as byProv
+        join cohortDRGLengthOfStaySummary as byDRG using (drg_mpi_code, drg_name) 
+    order by 
+        byDRG.variabilityImpact desc,
+        byProv.totalDays desc
+
+),
+-- Break out data by length of stay values so can plot as a histogram
 cohortDRGLengthOfStayHistogram AS
 (
     select 
         --EXTRACT(YEAR FROM firstEventTime) as admitYear,
         drg_mpi_code, drg_name,
+        admn_prov_map_id,
         FLOOR(lengthOfStayHour/24) as lengthOfStayDayMin, FLOOR(lengthOfStayHour/24)+1 as lengthOfStayDayMax,
         count(distinct anon_id) as nPatients, count(distinct pat_enc_csn_id_coded) as nEncounters
     from cohortDiagnosisRelatedGroupLengthOfStay
+    where drg_mpi_code = '021' and drg_name like 'CRANIOTOMY%' -- Restrict to diagnosis to facilitate visualization
     group by 
         --admitYear, 
-        drg_mpi_code, drg_name, lengthOfStayDayMin, lengthOfStayDayMax
+        drg_mpi_code, drg_name, admn_prov_map_id, lengthOfStayDayMin, lengthOfStayDayMax
     order by 
         --admitYear desc, 
-        drg_mpi_code, drg_name, lengthOfStayDayMin
+        drg_mpi_code, drg_name, admn_prov_map_id, lengthOfStayDayMin
 ),
 
 spacer AS ( select * from `mining-clinical-decisions.shc_core.demographic`)
@@ -107,4 +168,4 @@ spacer AS ( select * from `mining-clinical-decisions.shc_core.demographic`)
 
 select *
 from cohortDRGLengthOfStayHistogram
-limit 1009
+--limit 1000
