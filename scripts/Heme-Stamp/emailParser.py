533ca933a50e44b2dde78c164f5cda659d6a7e6d
gracek012 <49053558+gracek012@users.noreply.github.com>
2022-07-10 22:26:48 -0700

    End-to-end updated

--- scripts/Heme-Stamp/emailParser.py
+++ scripts/Heme-Stamp/emailParser.py
@@ -16,16 +16,22 @@ for filepath in glob.glob('/Users/g0123/Downloads/emails/*.eml'):
     soup = BeautifulSoup(text, 'html.parser')
     table_body = soup.find('tbody')
     table_head = soup.find('thead')
-    print(table_head)
     data = []
     dataHead = []
     for row in table_body.findAll("tr"):
         cols = row.find_all("td")
-        cols = [ele.text.strip() for ele in cols]
-        data.append([ele for ele in cols if ele])
+        cols1 = [ele.text.strip() for ele in cols[:6]]
+        cols2 = [ele.text for ele in cols[6:]]
+        cols = cols1 + cols2
+        # cols = [ele.text.strip() for ele in cols]
+        # data.append([ele for ele in cols if ele])
+        data.append(cols)
 
+    print(table_head.findAll("tr"))
     for row in table_head.findAll("tr"):
         cols = row.find_all("th") #use 'td' to parse received emails. Else use 'th'
+        if not cols:
+            cols = row.find_all("td")
         cols = [ele.text.strip() for ele in cols]
         dataHead.append([ele for ele in cols if ele])
 
@@ -48,19 +54,44 @@ client = bigquery.Client("som-nero-phi-jonc101")
 table_id = "som-nero-phi-jonc101-secure.grace_db.test_table"
 
 response_set = set(('[x]', '[X]', 'x', 'X'))
+response_headers = set(('<10%', '10-30%', '30-50%', '50-70%', '70-90%', '>90%'))
 
 for email in all_emails:
     response = None
     for k, v in email.items():
-        if v in response_set:
+        #if k in response_headers and (v.strip() in response_set or v.strip() != ""):
+        if k in response_headers and v.strip() != "":
             response = k
-    print(email)
+    print("email: ", email)
 
-    update_query = """
-    UPDATE `som-nero-phi-jonc101-secure.grace_db.test_table`
-    SET Phys_estimate = {0}
-    WHERE MRN = {1} and CSN = {2}
-    """.format("'"+response+"'", email["MRN"], email["CSN"])
-    print(update_query)
+    email["Notes"] = email["Notes"].strip()
+
+    if response:
+        if email["Notes"]:
+            update_query = """
+            UPDATE `som-nero-phi-jonc101-secure.grace_db.test_table`
+            SET Phys_estimate = {0}, Notes = {1}, Response = True
+            WHERE MRN = {2}
+            """.format("'"+response+"'", "'''"+email["Notes"]+"'''", email["MRN"])
+            print(update_query)
+        else:
+            update_query = """
+            UPDATE `som-nero-phi-jonc101-secure.grace_db.test_table`
+            SET Phys_estimate = {0}, Response = True
+            WHERE MRN = {1}
+            """.format("'" + response + "'", email["MRN"])
+            print(update_query)
+
+    elif email["Notes"]:
+        update_query = """
+        UPDATE `som-nero-phi-jonc101-secure.grace_db.test_table`
+        SET Notes = {0}, Response = True
+        WHERE MRN = {1}
+        """.format("'''" + email["Notes"] + "'''", email["MRN"])
+        print(update_query)
+
 
     client.query(update_query)
+
+
+
--- scripts/Heme-Stamp/generateEmail.py
+++ scripts/Heme-Stamp/generateEmail.py
@@ -17,15 +17,52 @@ class email(object):
         self.password = creds["password"]
         self.server = 'smtp.stanford.edu'
         self.text_content = """
-            Hello,
+            Hello, thank you for participating in our study. To respond, please click the Reply button, press "..." to show the
+            email chain, and directly edit the table below. Please mark X in the corresponding column to indicate your
+            estimation of the likelihood that the Heme-STAMP test will result in at least one pathogenic variant for each patient.  
+            Please try to avoid  adding anything other than an X in these cells to ensure that we can automatically parse responses. 
+            Please feel free to utilize the Notes column to add any additional comments.
+            
+            This data was pulled from Epic and based on orders that have been submitted in the last week. If one of these orders
+            have since been canceled or you believe was mistakenly included, please write Canceled in the Notes section.
+            
+            We have added the latest WBC and Hgb count for your convenience. 
+            
+            Thank you!
             {table} 
         """
         self.html_content = """
-            <html><body><p>Hello</p>
+            <html>
+            <head>
+                <style> 
+                  table, th, td {{ border: 1px solid black; border-collapse: collapse; overflow: hidden; white-space: nowrap;}}
+                  th, td, tr {{ padding: 5px; overflow: hidden; white-space: nowrap;}}
+                </style>
+                </head>
+            <body><p>Hello, thank you for participating in our study. 
+            <br>
+            <br>
+            To respond, please click the Reply button, press "..." to show the
+            email chain, and directly edit the table below. Please mark X in the corresponding column to indicate your
+            estimation of the likelihood that the Heme-STAMP test will result in at least one pathogenic variant for each patient.
+            <br>
+            <br>  
+            Please try to avoid  adding anything other than an X in these cells to ensure that we can automatically parse responses. 
+            Please feel free to utilize the Notes column to add any additional comments.
+            <br>
+            <br>
+            This data was pulled from Epic and based on orders that have been submitted in the last week. If one of these orders
+            have since been canceled or you believe was mistakenly included, please mark Canceled in the Notes section.
+            <br>
+            <br>
+            We have added the latest WBC and Hgb count for your convenience. 
+            <br>
+            <br>
+            Thank you! </p>
             {table}
-            </body></html>
+            </body>
+            </html>
         """
-
 
 
     def send_email(self):
@@ -38,13 +75,14 @@ class email(object):
         message = MIMEMultipart(
             "alternative", None, [MIMEText(text), MIMEText(html,'html')])
 
-        message['Subject'] = "HS Estimates"
+        message['Subject'] = "Secure: Heme-STAMP Estimates"
         message['From'] = self.sender
         message['To'] = self.email
+        message['CC'] = self.sender
         server = smtplib.SMTP(self.server)
         server.ehlo()
         server.starttls()
         server.login(self.sender, self.password)
-        server.sendmail(self.sender, self.email, message.as_string())
+        server.sendmail(self.sender, ([self.email] + [self.sender]), message.as_string())
         server.quit()
         print("email sent")
