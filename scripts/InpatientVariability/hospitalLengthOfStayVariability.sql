03407a9cd501d8ab4558b9a63acc3e6c180a785c
Jonathan Chen <jonc101@gmail.com>
2022-11-05 14:03:25 -0700

    Update to shc_core_2021 database.
    Distinguish DRGs by drg_code_set_c
    Link to order set usage to example of Ortho elective admissions and whether they used Ortho joint replacement order set or not, and see difference in average length of stay.

--- scripts/InpatientVariability/hospitalLengthOfStayVariability.sql
+++ scripts/InpatientVariability/hospitalLengthOfStayVariability.sql
@@ -5,21 +5,25 @@ WITH
 cohortDiagnosisRelatedGroup AS
 (
     select *
-    from `mining-clinical-decisions.shc_core.drg_code`
-    /*where drg_mpi_code in 
-    (   '301' -- Hip Joint Replacement
-        --'470',    -- Hip and Knee Joint Replacement or Reattachment w/o MCC    14K encounters
+    from `som-nero-phi-jonc101.shc_core_2021.drg_code`
+    where drg_mpi_code =  '302' and drg_code_set_c = '51'    -- Knee Joint Replacement   8.6K encounters
+        -- drg_mpi_code =  '470' and drg_code_set_c = '6'    -- Major Hip and Knee Joint Replacement without MC, 15K Encounters
+    /*
+    (   --'301' -- Hip Joint Replacement
         --'301',    -- Hip Joint Replacement    9.7K encounters
-        --'302'     -- Knee Joint Replacement   8.6K encounters
+        --'470',    -- Hip and Knee Joint Replacement or Reattachment w/o MCC    14K encounters
+        '302'     -- Knee Joint Replacement   8.6K encounters
     )
+    and drg_code_set_c = '51' -- Or '6' or '3'? Unclear which one to interpret
     */
+    
 ),
 
 -- Find all Specialty Primary cohort Assignments
 cohortPrimaryEncounter AS
 (
     select distinct anon_id, pat_enc_csn_id_coded
-    from `mining-clinical-decisions.shc_core.treatment_team`
+    from `som-nero-phi-jonc101.shc_core_2021.treatment_team`
     where name = 'Primary Team'
     --and prov_name like 'ORTHO ARTHRITIS%'
     -- Up to 1,000 cases per year
@@ -29,7 +33,7 @@ cohortPrimaryEncounter AS
 emergencyAdmissionEncounter AS
 (
     select distinct anon_id, pat_enc_csn_id_coded
-    from `mining-clinical-decisions.shc_core.adt`
+    from `som-nero-phi-jonc101.shc_core_2021.adt`
     where pat_service_c in ('100','187') -- Emergency and Emergency Services
     and in_event_type_c = 1 -- Admission
 ),
@@ -44,7 +48,7 @@ cohortNonEmergencyEncounter AS
 cohortNonEmergencyEncounterByYear AS
 (
     select EXTRACT(YEAR FROM trtmnt_tm_begin_dt_jittered) as assignYear, count(distinct anon_id) nPatients, count(distinct pat_enc_csn_id_coded) as nEncounters
-    from cohortNonEmergencyEncounter join shc_core.treatment_team using (anon_id, pat_enc_csn_id_coded)
+    from cohortNonEmergencyEncounter join shc_core_2021.treatment_team using (anon_id, pat_enc_csn_id_coded)
     group by assignYear
     order by assignYear desc
     -- Close to 1,000 patients per year, confirming most of these admissions are elective / non-emergency
@@ -54,16 +58,16 @@ cohortNonEmergencyEncounterByYear AS
 cohortDiagnosisCounts AS
 (  
     select source, icd9, icd10, dx_name, count(distinct snee.anon_id) as nPatients, count(distinct snee.pat_enc_csn_id_coded) as nEncounters, count(*) as nDiagnoses
-    from `mining-clinical-decisions.shc_core.diagnosis_code` as dc 
+    from `som-nero-phi-jonc101.shc_core_2021.diagnosis` as dc 
         join cohortNonEmergencyEncounter as snee on (snee.anon_id = dc.anon_id and snee.pat_enc_csn_id_coded = dc.pat_enc_csn_id_jittered)
     group by source, icd9, icd10, dx_name
     order by nPatients desc
 ),
 diagnosisRelatedGroupCounts AS
 (
-    select drg_mpi_code, drg_name, count(distinct anon_id) as nPatients, count(distinct pat_enc_csn_id_coded) as nEncounters, count(*) as nDiagnoses
-    from `mining-clinical-decisions.shc_core.drg_code`
-    group by drg_mpi_code, drg_name
+    select drg_code_set_c, drg_mpi_code, drg_name, count(distinct anon_id) as nPatients, count(distinct pat_enc_csn_id_coded) as nEncounters, count(*) as nDiagnoses
+    from `som-nero-phi-jonc101.shc_core_2021.drg_code`
+    group by drg_code_set_c, drg_mpi_code, drg_name
     order by nEncounters desc
 ),
 
@@ -71,7 +75,7 @@ diagnosisRelatedGroupCounts AS
 cohortDiagnosisRelatedGroupLengthOfStay AS
 (
     select 
-        drg_mpi_code, drg_name, anon_id, pat_enc_csn_id_coded, 
+        drg_code_set_c, drg_mpi_code, drg_name, anon_id, pat_enc_csn_id_coded, 
         admn_prov_map_id, visit_prov_map_id,
         min(event_time_jittered) as firstEventTime,
         DATETIME_DIFF(max(event_time_jittered), min(event_time_jittered), HOUR) as lengthOfStayHour, 
@@ -79,10 +83,10 @@ cohortDiagnosisRelatedGroupLengthOfStay AS
         ROUND(DATETIME_DIFF(max(event_time_jittered), min(event_time_jittered), HOUR) / 24) as lengthOfStayDayFloor
     from cohortDiagnosisRelatedGroup
         join cohortNonEmergencyEncounter using (anon_id, pat_enc_csn_id_coded)
-        join shc_core.adt using (anon_id, pat_enc_csn_id_coded) -- Join ADT data for first and last event time to estimate length of stay
-        join shc_core.encounter using (anon_id, pat_enc_csn_id_coded)
+        join shc_core_2021.adt using (anon_id, pat_enc_csn_id_coded) -- Join ADT data for first and last event time to estimate length of stay
+        join shc_core_2021.encounter using (anon_id, pat_enc_csn_id_coded)
     group by 
-        drg_mpi_code, drg_name, anon_id, pat_enc_csn_id_coded,
+        drg_code_set_c, drg_mpi_code, drg_name, anon_id, pat_enc_csn_id_coded,
         admn_prov_map_id, visit_prov_map_id
 ),
 -- Calculate summary stats per diagnosis 
@@ -147,7 +151,7 @@ cohortDRGLengthOfStayHistogram AS
         FLOOR(lengthOfStayHour/24) as lengthOfStayDayMin, FLOOR(lengthOfStayHour/24)+1 as lengthOfStayDayMax,
         count(distinct anon_id) as nPatients, count(distinct pat_enc_csn_id_coded) as nEncounters
     from cohortDiagnosisRelatedGroupLengthOfStay
-    where drg_mpi_code = '021' and drg_name like 'CRANIOTOMY%' -- Restrict to diagnosis to facilitate visualization
+    --where drg_mpi_code = '021' and drg_name like 'CRANIOTOMY%' -- Restrict to diagnosis to facilitate visualization
     group by 
         --admitYear, 
         drg_mpi_code, drg_name, admn_prov_map_id, lengthOfStayDayMin, lengthOfStayDayMax
@@ -156,7 +160,66 @@ cohortDRGLengthOfStayHistogram AS
         drg_mpi_code, drg_name, admn_prov_map_id, lengthOfStayDayMin
 ),
 
-spacer AS ( select * from `mining-clinical-decisions.shc_core.demographic`)
+-- Find which order sets were used in each cohort admit/encounter
+cohortDiagnosisRelatedGroupOrderSet AS
+(
+    select distinct cdrg.*,  protocol_id, protocol_name
+    from shc_core_2021.proc_orderset as os
+        join cohortDiagnosisRelatedGroup as cdrg using (anon_id, pat_enc_csn_id_coded)
+),
+
+-- Most common order sets used in the cohort
+cohortDiagnosisRelatedGroupByOrderSet AS
+(
+    select protocol_id, protocol_name,
+        count(distinct anon_id) as nPatients, count(distinct pat_enc_csn_id_coded) as nEncounters
+    from cohortDiagnosisRelatedGroupOrderSet as cdrgos
+    group by protocol_id, protocol_name
+    order by nEncounters desc
+),
+
+-- Restrict to only order set of interest as the relevant "standard" pathway for the DRG cohort
+cohortDiagnosisRelatedGroupStandardOrderSet AS
+(
+    select distinct anon_id, pat_enc_csn_id_coded, protocol_id, protocol_name
+    from shc_core_2021.proc_orderset as os
+        join cohortDiagnosisRelatedGroup as cdrg using (anon_id, pat_enc_csn_id_coded)
+    where protocol_id = 891 -- IP ORT TOTAL JOINT REPLACEMENT
+),
+-- Left outer join against cohort DRG length of stay results so can differentiate who used and who did not use standard order set
+cohortDiagnosisRelatedGroupLengthOfStayStandardOrderSet AS
+(
+    select *, (protocol_id is not null) orderSetUsed
+    from cohortDiagnosisRelatedGroupLengthOfStay AS cdrglos
+        left join cohortDiagnosisRelatedGroupStandardOrderSet AS cdrgsos using (anon_id, pat_enc_csn_id_coded)
+),
+-- Break out summary stats by whether standard order set was used or not...
+cohortDRGLengthOfStaySummaryByStandardOrderSet AS
+(
+    select 
+        drg_code_set_c, drg_mpi_code, drg_name, 
+        orderSetUsed,
+        AVG(lengthOfStayDay) AS avgLengthOfStayDay, STDDEV(lengthOfStayDay) as stddevLengthOfStayDay,
+        COUNT(distinct anon_id) as nPatients, COUNT(distinct pat_enc_csn_id_coded) as nEncounters,
+        STDDEV(lengthOfStayDay) * COUNT(distinct pat_enc_csn_id_coded) AS variabilityImpact,
+        SUM(lengthOfStayDay) as totalDays
+    from cohortDiagnosisRelatedGroupLengthOfStayStandardOrderSet
+    group by 
+        drg_code_set_c, drg_mpi_code, drg_name,
+        orderSetUsed
+    order by 
+        drg_code_set_c, drg_mpi_code, drg_name,
+        stddevLengthOfStayDay * nEncounters desc -- Potential impact? Volume * variability
+),
+
+
+spacer AS ( select * from `som-nero-phi-jonc101.shc_core_2021.demographic`)
+
+select *
+from cohortDRGLengthOfStaySummaryByStandardOrderSet
+limit 1000
+
+
 
 
 
@@ -165,7 +228,3 @@ spacer AS ( select * from `mining-clinical-decisions.shc_core.demographic`)
 
 -- Correlate Discharge Order time Relative to Discharge Time vs. Length of Stay
 
-
-select *
-from cohortDRGLengthOfStayHistogram
---limit 1000
