98601a1a6735193cd9896c0d40b8bc0ae9096cd9
gracek012 <49053558+gracek012@users.noreply.github.com>
2023-11-20 11:39:49 -0800

    Update notes_query.sql
    Include all cultures ordered (not just resulted cultures) and draw from table that includes select proc_codes and descriptions

--- scripts/antibiotic-susceptibility/sql/queries/notes_query.sql
+++ scripts/antibiotic-susceptibility/sql/queries/notes_query.sql
@@ -1,13 +1,13 @@
 -- Query to pull cohort of notes to be used for phenotyping
 
-SELECT DISTINCT culture_anon_id as anon_id, note_date_jittered, note_type
+SELECT DISTINCT ord_anon_id as anon_id, note_date_jittered, note_type
 FROM (
-  SELECT culture.anon_id as culture_anon_id, culture.order_proc_id_coded as order_proc_id_coded, 
-  culture.description as description, culture.order_time_jittered as order_time_jittered
-  FROM `som-nero-phi-jonc101.shc_core_2022.culture_sensitivity` culture
-  JOIN `som-nero-phi-jonc101.shc_core_2022.order_proc` ord_proc
-  ON culture.order_proc_id_coded = ord_proc.order_proc_id_coded
-  WHERE cpt_code not in ("LABWDC", "LABWDCG", "LABWDCS", "LABDMCWDC") -- wound cultures
+  SELECT anon_id as ord_anon_id, order_proc_id_coded, abx_select.description as description, order_time_jittered
+  FROM `som-nero-phi-jonc101.shc_core_2022.order_proc` ord_proc
+  JOIN `som-nero-phi-jonc101.gk_abx.abx_select` abx_select
+  ON ord_proc.proc_code = abx_select.proc_code AND 
+  (ord_proc.proc_id = abx_select.proc_id AND ord_proc.cpt_code = abx_select.cpt_code)
+  OR ord_proc.description = abx_select.description
 ) AS cultures
 JOIN
 (
@@ -16,5 +16,5 @@ JOIN
   WHERE note_type not in ("Nursing Sign Out Note")
 )
 AS notes
-ON culture_anon_id = notes.anon_id
+ON ord_anon_id = notes.anon_id
 WHERE ABS(DATE_DIFF(cultures.order_time_jittered, notes.note_date_jittered, DAY)) <= 30
