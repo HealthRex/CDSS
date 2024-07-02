6a430f2fd30bb4f297600f8e4265eec7de9a03a5
Francois Grolleau <fc.grolleau@gmail.com>
2024-07-02 14:02:22 -0700

    specify joint distributuion via interactive contour

--- scripts/DevWorkshop/ROC-power/bivariate_normal/interactive_contours.py
+++ scripts/DevWorkshop/ROC-power/bivariate_normal/interactive_contours.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm, multivariate_normal, bernoulli
+from scipy.special import expit, logit
 
-to_01_fun = norm.cdf
-to_r_fun = norm.ppf
+to_01_fun = norm.cdf # expit
+to_r_fun = logit # norm.ppf
 
 def plot_contour(ax, X_mean, Y_mean, X_var, Y_var, corr, cases = True, epsi=1e-6, n_points=300): 
     x_ori = np.linspace(epsi, 1-epsi, n_points)
@@ -35,6 +36,7 @@ def plot_contour(ax, X_mean, Y_mean, X_var, Y_var, corr, cases = True, epsi=1e-6
         cbar.set_label('Density', rotation=270, labelpad=15)
 
     ax.plot(E_X, E_Y, marker='X', color='black', markersize=12)
+    ax.plot([0, 1], [0, 1], '-', color='black', linewidth=1)
     ax.plot([E_X, E_X], [0, E_Y], '--', color='black', linewidth=0.75)
     ax.plot([0, E_X], [E_Y, E_Y], '--', color='black', linewidth=0.75)
 
@@ -57,17 +59,17 @@ samples_cases = plot_contour(ax1, X_mean=0.75, Y_mean=0.85, X_var=.2, Y_var=0.3,
 
 # Create the second subplot on the first row and second column
 ax2 = plt.subplot2grid((3, 2), (0, 1))
-samples_controls = plot_contour(ax2, X_mean=0.4, Y_mean=0.5, X_var=.5, Y_var=0.5, corr=.5, cases=False)
+samples_controls = plot_contour(ax2, X_mean=0.2, Y_mean=0.3, X_var=0.1, Y_var=0.3, corr=.7, cases=False)
 
 # Create the third subplot on the second row, spanning both columns
-ax3 = plt.subplot2grid((3, 2), (1, 0), colspan=2)
+#ax3 = plt.subplot2grid((3, 2), (1, 0), colspan=2)
 # Example: Plotting a simple line plot on the third subplot
 x = np.linspace(0, 10, 100)
 y = np.sin(x)
 ax3.plot(x, y)
 #ax3.set_title('samples = ' + str(val_1 + val_2))
-ax3.set_xlabel('X axis')
-ax3.set_ylabel('Y axis')
+#ax3.set_xlabel('X axis')
+#ax3.set_ylabel('Y axis')
 
 # Adjust layout for better spacing
 plt.tight_layout()
