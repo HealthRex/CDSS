d7c93a43645695c66bdea6e148a724506a57c9ec
Francois Grolleau <grolleau@DNa829632.SUNet>
2024-07-09 21:59:52 -0700

    Pipeline to Conformalized Quantile Regression

--- scripts/cost_variability_2024/scripts/pipeline/all_drg_to_all_plots.py
+++ scripts/cost_variability_2024/scripts/pipeline/all_drg_to_all_plots.py
@@ -1,11 +1,10 @@
 import os
 os.chdir('/Users/grolleau/Desktop/github repos/CDSS/scripts/cost_variability_2024/scripts/pipeline/')
 from drg_to_plot import drg_to_plot
+from dat_to_conformal import drg_to_imp, drg_to_cqr
 from google.cloud import bigquery
 from google.cloud.bigquery import dbapi
 import pandas as pd
-
-drg_to_plot(2395)
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/grolleau/Desktop/github repos/Cost variability/json_credentials/grolleau_application_default_credentials.json'
 os.environ['GCLOUD_PROJECT'] = 'som-nero-phi-jonc101'
@@ -30,4 +29,9 @@ drg_list = pd.read_sql_query(drg_query, conn).iloc[:, 0].tolist()
 
 for i, drg in enumerate(drg_list):
     print(f"DRG No. {i+1} of {len(drg_list)}")
-    drg_to_plot(drg)
+    #drg_to_plot(drg)
+    try:
+        drg_to_cqr(drg)
+    except Exception as e:
+        print(f"An error occurred with DRG {drg}: {e}")
+        continue
