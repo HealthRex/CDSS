c166edfc860cf1aaed1382287324768ef5743faa
Francois Grolleau <fc.grolleau@gmail.com>
2024-07-22 09:46:08 -0700

    deploy v1 https://francois-grolleau.shinyapps.io/ss_auroc

--- scripts/DevWorkshop/ROC-power/shinyapp/basic-app/app.py
+++ scripts/DevWorkshop/ROC-power/shinyapp/basic-app/app.py
@@ -1,36 +1,34 @@
-# This app is translated from Mastering Shinywidgets
-# https://mastering-shiny.org/basic-reactivity.html#reactive-expressions-1
 from shiny import App, render, ui, reactive
 from numpy import random
 import asyncio
 from three_panel import *
 
-app_ui = ui.page_fixed(   
+app_ui = ui.page_fixed(ui.panel_title("Sample Size for Comparing Models' Area Under the ROC Curve", "Sample Size for Comparing AUROCs"),   
     ui.row(
         ui.column(
             4,
             "For cases (event occurs)",
-            ui.input_slider("X_mean1", label="Model A: Mean parameter", value=.2, min=1e-3, max=1-1e-3),
-            ui.input_slider("X_var1", label="Model A: Variance parameter", value=.2, min=1e-3, max=1-1e-3),
-            ui.input_slider("Y_mean1", label="Model B: Mean parameter", value=.2, min=1e-3, max=1-1e-3),
-            ui.input_slider("Y_var1", label="Model B: Variance parameter", value=.2, min=1e-3, max=1-1e-3),
-            ui.input_slider("corr1", label="Correlation between models A and B", value=.2, min=1e-3, max=1-1e-3),
+            ui.input_slider("X_mean1", label="Model A: Mean parameter", value=.6, min=1e-3, max=1-1e-3),
+            ui.input_slider("X_var1", label="Model A: Variance parameter", value=.5, min=1e-3, max=1-1e-3),
+            ui.input_slider("Y_mean1", label="Model B: Mean parameter", value=.6, min=1e-3, max=1-1e-3),
+            ui.input_slider("Y_var1", label="Model B: Variance parameter", value=.5, min=1e-3, max=1-1e-3),
+            ui.input_slider("corr1", label="Correlation between models A and B", value=.6, min=1e-3, max=1-1e-3),
         ),
         ui.column(
             4,
             "For controls (event does not occur)",
-            ui.input_slider("X_mean2", label="Model A: Mean parameter", value=.2, min=1e-3, max=1-1e-3),
-            ui.input_slider("X_var2", label="Model A: Variance parameter", value=.2, min=1e-3, max=1-1e-3),
-            ui.input_slider("Y_mean2", label="Model B: Mean parameter", value=.2, min=1e-3, max=1-1e-3),
-            ui.input_slider("Y_var2", label="Model B: Variance parameter", value=.2, min=1e-3, max=1-1e-3),
-            ui.input_slider("corr2", label="Correlation between models A and B", value=.2, min=1e-3, max=1-1e-3),
+            ui.input_slider("X_mean2", label="Model A: Mean parameter", value=.3, min=1e-3, max=1-1e-3),
+            ui.input_slider("X_var2", label="Model A: Variance parameter", value=.5, min=1e-3, max=1-1e-3),
+            ui.input_slider("Y_mean2", label="Model B: Mean parameter", value=.4, min=1e-3, max=1-1e-3),
+            ui.input_slider("Y_var2", label="Model B: Variance parameter", value=.5, min=1e-3, max=1-1e-3),
+            ui.input_slider("corr2", label="Correlation between models A and B", value=.7, min=1e-3, max=1-1e-3),
         ),
         ui.column(2,
             "Simulation",
-            ui.input_numeric("ss", label="Sample Size", value=500, min=100, max=100000),
-            ui.input_slider("prev", label="Prevalence", value=.05, min=1e-3, max=1-1e-3),
+            ui.input_numeric("ss", label="Sample Size", value=830, min=100, max=100000),
+            ui.input_slider("prev", label="Prevalence", value=.1, min=1e-3, max=1-1e-3),
             ui.input_slider("alpha_t", label="Alpha threshold", value=.05, min=1e-3, max=1-1e-3),
-            ui.input_select("n_sim", label=ui.markdown("**Choose no. of iterations to run the simulations**"), choices={0: "Zero (no simulation)", 500: 500, 1000: 1000, 2000: 2000}),
+            ui.input_select("n_sim", label=ui.markdown("**Choose no. of iterations to run the simulations**"), choices={0: "Zero (for parameter selection)", 100: "100 simulations (fastest)", 500: "500 simulations (intermediate)", 2000: "2000 simulations (slowest)"}),
         )
     ),
     ui.row(
