d918650ce35e264122d45e03c93c66444f06b037
Francois Grolleau <fc.grolleau@gmail.com>
2024-08-16 22:54:14 -0700

    edit ui for joint distribution page

--- scripts/DevWorkshop/ROC-power/shinyapp/basic-app/app.py
+++ scripts/DevWorkshop/ROC-power/shinyapp/basic-app/app.py
@@ -4,41 +4,44 @@ from numpy import random
 from three_panel import *
 
 app_ui = ui.page_fluid(ui.panel_title("Sample Size for Comparing Models' Area Under the ROC Curve:", "Sample Size for Comparing AUROCs"),  
-    ui.h4("Specifying parameters of two joint distributions"), 
+    ui.h4("Specifying parameters of two joint distributions"),
+    ui.br(),
+    ui.h4("- Inputs"), 
     ui.row(
         ui.column(
             4,
-            "For cases (event occurs):",
-            ui.input_slider("X_mean1", label="- Model A: Mean parameter", value=.7, step=.01, min=1e-2, max=1-1e-2),
-            ui.input_slider("Y_mean1", label="- Model B: Mean parameter", value=.6, step=.01, min=1e-2, max=1-1e-2),
-            ui.input_switch("change_var1", "Change variance parameters", False),
+            ui.strong("For cases (event occurs)"),
+            ui.input_slider("X_mean1", ui.p("Mean parameter for model A", ui.br(), ui.em("How well will model A do on cases?")), value=.7, step=.01, min=1e-2, max=1-1e-2),
+            ui.input_slider("Y_mean1", ui.p("Mean parameter for model B", ui.br(), ui.em("How well will model B do on cases?")), value=.6, step=.01, min=1e-2, max=1-1e-2),
+            ui.input_switch("change_var1", "Change variance parameters for cases", False, width='375px'),
             ui.output_ui("ui_X_var1"),
             ui.output_ui("ui_Y_var1"),
-            ui.input_switch("change_cor1", "Change correlation parameter", False),
+            ui.input_switch("change_cor1", "Change correlation parameter for cases", False, width='375px'),
             ui.output_ui("ui_corr1"),
+             ui.h4("- Results"), 
+            ui.output_plot("hist", width='900px', height='900px'),
         ),
         ui.column(
             4,
-            "For controls (event does not occur):",
-            ui.input_slider("X_mean2", label="- Model A: Mean parameter", value=.3, step=.01, min=1e-2, max=1-1e-2),
-            ui.input_slider("Y_mean2", label="- Model B: Mean parameter", value=.4, step=.01, min=1e-2, max=1-1e-2),
-            ui.input_switch("change_var2", "Change variance parameters", False),
+            ui.strong("For controls (event does not occur)"),
+            ui.input_slider("X_mean2", ui.p("Mean parameter for model A", ui.br(), ui.em("How well will model A do on controls?")), value=.3, step=.01, min=1e-2, max=1-1e-2),
+            ui.input_slider("Y_mean2", ui.p("Mean parameter for model B", ui.br(), ui.em("How well will model B do on controls?")), value=.4, step=.01, min=1e-2, max=1-1e-2),
+            ui.input_switch("change_var2", "Change variance parameters for controls", False, width='375px'),
             ui.output_ui("ui_X_var2"),
             ui.output_ui("ui_Y_var2"),
-            ui.input_switch("change_cor2", "Change correlation parameter", False),
+            ui.input_switch("change_cor2", "Change correlation parameter for controls", False, width='375px'),
             ui.output_ui("ui_corr2"),
         ),
-        ui.column(2,
-            "Simulation:",
-            ui.input_numeric("ss", label="- Sample Size", value=260, min=100, max=100000),
-            ui.input_slider("prev", label="- Prevalence", value=.1, step=.01, min=1e-2, max=1-1e-2),
-            ui.input_slider("alpha_t", label="- Alpha threshold", value=.05, step=.01, min=1e-2, max=1-1e-2),
-            ui.input_select("n_sim", label=ui.markdown("**Run the simulations**"), choices={0: "No iteration (for parameter selection)", 100: "100 iterations (fastest, least accurate)", 500: "500 iterations (intermediate)", 2000: "2000 iterations (slowest, most accurate)"}),
+        ui.column(4,
+            ui.strong("Simulation"),
+            ui.input_slider("prev", ui.p("Prevalence in the evaluation population", ui.br(), ui.em("What is the anticipated proportion of events in the test set?")), value=.1, step=.01, min=1e-2, max=1-1e-2),
+            ui.input_slider("alpha_t", ui.p("Alpha threshold", ui.br(), ui.em("Significance level, typically set to 0.05")), value=.05, step=.01, min=1e-2, max=1-1e-2),
+            ui.input_numeric("ss", ui.p("Sample size for power calculation", ui.br(), ui.em("Calculations are also performed at 0.5 and 1.5 that sample size")), value=260, min=100, max=100000),
+            ui.input_select("n_sim", ui.p(ui.strong("Run the simulations"), ui.br(), ui.em("Choose no. of iterations")), choices={0: "No iteration (for parameter selection)", 100: "100 iterations (fastest, least accurate)", 500: "500 iterations (intermediate)", 2000: "2000 iterations (slowest, most accurate)"}),
         )
     ),
-    ui.row(
-        ui.column(5, ui.output_plot("hist", width='1000px', height='1000px'),)
-    ),
+    ui.br(),
+    ui.output_ui("ui_text_meth"),
 )
 
 def server(input, output, session):
@@ -47,7 +50,7 @@ def server(input, output, session):
     @render.plot
     def hist():
            return three_panel(X_mean1=input.X_mean1(), Y_mean1=input.Y_mean1(), X_var1=input.X_var1() if input.change_var1() else .9, Y_var1=input.Y_var1() if input.change_var1() else .9, corr1=input.corr1() if input.change_cor1() else .6,
-                           X_mean2=input.X_mean2(), Y_mean2=input.Y_mean2(), X_var2=input.X_var2() if input.change_var2() else .9, Y_var2=input.Y_var2() if input.change_var2() else .9, corr2=input.corr2() if input.change_cor2() else .6,
+                           X_mean2=1-input.X_mean2(), Y_mean2=1-input.Y_mean2(), X_var2=input.X_var2() if input.change_var2() else .9, Y_var2=input.Y_var2() if input.change_var2() else .9, corr2=input.corr2() if input.change_cor2() else .6,
                            ss=input.ss(), prev=input.prev(), alpha_t=input.alpha_t(), n_sim=None if int(input.n_sim()) == 0 else int(input.n_sim())) 
         
     @render.ui 
@@ -55,41 +58,49 @@ def server(input, output, session):
     def ui_X_var1(): 
         if input.change_var1():
             value = input.X_var1() if "X_var1" in input else .9
-            return ui.input_slider("X_var1", "- Model A: Variance parameter", value=.9, step=.01, min=1e-2, max=1-1e-2)
+            return ui.input_slider("X_var1", ui.p("Variance parameter for model A", ui.br(), ui.em("How variable will model A be on cases?")), value=.9, step=.01, min=1e-2, max=1-1e-2)
         
     @render.ui 
     @reactive.event(input.change_var1)
     def ui_Y_var1(): 
         if input.change_var1():
             value = input.Y_var1() if "Y_var1" in input else .9
-            return ui.input_slider("Y_var1", "- Model B: Variance parameter", value=.9, step=.01, min=1e-2, max=1-1e-2)
+            return ui.input_slider("Y_var1", ui.p("Variance parameter for model B", ui.br(), ui.em("How variable will model B be on cases?")), value=.9, step=.01, min=1e-2, max=1-1e-2)
     
     @render.ui 
     @reactive.event(input.change_cor1)
     def ui_corr1(): 
         if input.change_cor1():
             value = input.corr1() if "corr1" in input else .6
-            return ui.input_slider("corr1", "- Correlation between models A and B", value=.6, step=.01, min=0, max=1-1e-2)
+            return ui.input_slider("corr1", ui.p("Correlation parameter for models A & B", ui.br(), ui.em("How close will predictions be on cases?")), value=.6, step=.01, min=0, max=1-1e-2)
         
     @render.ui 
     @reactive.event(input.change_var2)
     def ui_X_var2(): 
         if input.change_var2():
             value = input.X_var2() if "X_var2" in input else .9
-            return ui.input_slider("X_var2", "- Model A: Variance parameter", value=.9, step=.01, min=1e-2, max=1-1e-2)
+            return ui.input_slider("X_var2", ui.p("Variance parameter for model A", ui.br(), ui.em("How variable will model A be on controls?")), value=.9, step=.01, min=1e-2, max=1-1e-2)
         
     @render.ui 
     @reactive.event(input.change_var2)
     def ui_Y_var2(): 
         if input.change_var2():
             value = input.Y_var2() if "Y_var2" in input else .9
-            return ui.input_slider("Y_var2", "- Model B: Variance parameter", value=.9, step=.01, min=1e-2, max=1-1e-2)
+            return ui.input_slider("Y_var2", ui.p("Variance parameter for model B", ui.br(), ui.em("How variable will model B be on controls?")), value=.9, step=.01, min=1e-2, max=1-1e-2)
     
     @render.ui 
     @reactive.event(input.change_cor2)
     def ui_corr2(): 
         if input.change_cor2():
             value = input.corr1() if "corr2" in input else .6
-            return ui.input_slider("corr2", "- Correlation between models A and B", value=.6, step=.01, min=0, max=1-1e-2)
+            return ui.input_slider("corr2", ui.p("Correlation parameter for models A & B", ui.br(), ui.em("How close will predictions be on controls?")), value=.6, step=.01, min=0, max=1-1e-2)
+    
+    @render.ui
+    @reactive.event(input.n_sim)
+    def ui_text_meth(): 
+        if int(input.n_sim()) == 0:
+            return 
+        else:
+           return ui.h4("- Methods"), "In the plot above, each dot represents a DeLong p-value calculated on a dataset of the corresponding sample size. Each dataset is obtained by sampling from the provided probability distribution. If the prevalence is varied, the pilot test set is reweighed accordingly before resampling. Power is estimated as the fraction of p-values below the significance level."
 
 app = App(app_ui, server)
