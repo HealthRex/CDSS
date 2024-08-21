5d157473027d26b1aa047a3e20582a53a132a19c
Francois Grolleau <fc.grolleau@gmail.com>
2024-08-20 18:44:11 -0700

    update ui for three shiny apps

--- scripts/DevWorkshop/ROC-power/shinyapp/collins/app.py
+++ scripts/DevWorkshop/ROC-power/shinyapp/collins/app.py
@@ -1,3 +1,7 @@
+# rsconnect deploy shiny shinyapp/collins --name francois-grolleau --title prec_auroc1
+# rsconnect deploy shiny shinyapp/pilot --name francois-grolleau --title ss_auroc_pilot
+# rsconnect deploy shiny shinyapp/basic-app --name francois-grolleau --title ss_auroc
+
 from shiny import App, render, ui
 from scipy.stats import norm
 from stat_custom import collins_c
@@ -6,21 +10,21 @@ app_ui = ui.page_fluid(
     ui.panel_title("Sample Size Needed to Precisely Estimate a Model's Area Under the ROC Curve"),
     ui.br(),
     ui.h4("- Inputs"),
-    "Anticipated AUROC in the evaluation population",
-    ui.input_slider("auroc", label=ui.em('How well do we expect the model to perform on the test set?'), value=.77, step=.01, min=.5, max=1-1e-2, width="600px"),
-    "Prevalence in the evaluation population",
-    ui.input_slider("prev", label=ui.em('What is the anticipated proportion of events in the test set?'), value=.43, step=.01, min=1e-2, max=1-1e-2, width="600px"),
-    "Target width for the estimated AUROC 95% confidence interval",
-    ui.input_numeric("targ_ciw", label=ui.em('What level of precision do we aim to achieve for model evaluation?'), value=.1, step=.0001, min=1e-2, max=1-1e-2, width="600px"),
+    ui.input_slider("auroc", label=ui.p(ui.strong(ui.div({"style": ""},"Anticipated AUROC in the evaluation population")), ui.em("How well do we expect the model to perform on the test set?")), value=.77, step=.01, min=.5, max=1-1e-2, width="600px"),
+    ui.input_slider("prev", label=ui.p(ui.strong(ui.div({"style": ""},"Prevalence in the evaluation population")), ui.em("What is the anticipated proportion of events in the test set?")), value=.43, step=.01, min=1e-2, max=1-1e-2, width="600px"),
+    ui.input_numeric("targ_ciw", label=ui.p(ui.strong(ui.div({"style": ""},"Target width for the estimated AUROC 95% confidence interval")), ui.em("What level of precision do we aim to achieve for model evaluation?")), value=.1, step=.0001, min=1e-2, max=1-1e-2, width="600px"),
     ui.br(),
     ui.h4("- Result"),
     ui.div(
-    {"style": "font-weight: bold; text-indent: 40px;"},
+    {"style": "font-weight: bold; text-indent: 40px; font-size: 18px;"},
     ui.output_text("txt2"),
     ),
     ui.output_text("txt1"),
     ui.br(),
-    ui.h4("- References"),
+    ui.h4("- Methods"),
+    "Our code uses an iterative process over the equation given in the references below to identify the sample size needed to achieve the desired precision for the AUROC estimate.",
+    ui.div({"style": "margin-bottom: 20px;"}),
+    "References:",
     ui.br(),
     "- Riley et al. Evaluation of clinical prediction models (part 3): calculating the sample size required for an external validation study. BMJ (2024) [See Figure 3, Criterion 3, ",
     ui.a("link", href="https://www.bmj.com/content/384/bmj-2023-074821", target="_blank"), "].",
