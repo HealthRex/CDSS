25b884d4d7cfdf0f6d494ce4aff023473c4ad6de
Francois Grolleau <fc.grolleau@gmail.com>
2024-08-21 19:20:44 -0700

    update reweighting and pilot ui

--- scripts/DevWorkshop/ROC-power/shinyapp/pilot/app.py
+++ scripts/DevWorkshop/ROC-power/shinyapp/pilot/app.py
@@ -9,22 +9,37 @@ app_ui = ui.page_fluid(ui.panel_title("Sample Size for Comparing Models' Area Un
     ui.h4("- Inputs"),
     ui.input_file("file1", "Please upload a pilot test set as a CSV file", accept=[".csv"], multiple=False, button_label='Upload .CSV file', width='400px'),
     ui.output_text("text"),
-    ui.output_table("summary_csv"),
-    ui.input_switch("change_prev", "Vary the prevalence from the pilot test set", False, width='400px'),
-    ui.output_text("ui_text_prev"),
-    ui.output_ui("ui_prev"),
     ui.br(),
     ui.row(
     ui.input_slider("alpha_t", label=ui.p(ui.strong(ui.div({"style": ""},"Alpha threshold")), ui.em("Significance level, typically set to 0.05")), value=.05, step=.01, min=1e-2, max=1-1e-2, width='300px'),
     ui.input_numeric("ss", label=ui.p(ui.strong(ui.div({"style": ""}, "Sample size for power calculation")), ui.em("Calculations are also performed at 0.5 and 1.5 that sample size")), value=1060, min=100, max=100000, width='475px'),
-    ui.input_select("n_sim", label=ui.p(ui.strong(ui.div({"style": "font-weight: bold; color: red;"}, "Run the simulations")), ui.em("Choose no. of iterations")), choices={100: "100 iteration (fastest, least accurate)", 500: "500 iterations (intermediate)", 2000: "2000 iterations (slowest, most accurate)"}),
+    ui.input_select("n_sim", label=ui.p(ui.strong(ui.div({"style": "font-weight: bold; color: red;"}, "Run the simulations")), ui.em("Choose no. of iterations")), choices={100: "100 iterations (fastest, least accurate)", 500: "500 iterations (intermediate)", 2000: "2000 iterations (slowest, most accurate)"}, width='350px'),
 ),  
     ui.br(),
-    ui.h4("- Results"),
-    ui.row(ui.output_plot("int_plot", width='1000px', height='750px'),),
+    
+    ui.row(
+        ui.column(
+            8,
+            ui.h4("- Results"),
+            ui.row(ui.output_plot("int_plot", width='1000px', height='750px'),),
+        ),
+        ui.column(
+            4,
+            ui.div({"style": "margin-bottom: 75px;"}),
+            ui.strong(ui.div({"style": ""},"Pilot test set used:")),
+            ui.div({"style": "margin-bottom: 25px;"}),
+            ui.output_table("summary_csv"),
+            ui.input_switch("change_prev", "Vary the prevalence from the pilot test set", False, width='400px'),
+            ui.output_text("ui_text_prev"),
+            ui.output_ui("ui_prev"),
+        )
+    ),
+    ui.row(
     ui.h4("- Methods"),
-    "In the plot above, each dot represents a DeLong p-value calculated on a dataset of the corresponding sample size. Each dataset is obtained by resampling with replacement from the pilot test set. If the prevalence is varied, the pilot test set is reweighed accordingly before resampling. Power is estimated as the fraction of p-values below the significance level."
+    ui.div({"style": "margin-left: 5px;"}, "In the plot above, each dot represents a DeLong p-value calculated on a dataset of the corresponding sample size. Each dataset is obtained by resampling with replacement from the pilot test set. If the prevalence is varied, the pilot test set is reweighed accordingly before resampling. Power is estimated as the fraction of p-values below the significance level."),
+    )
 )
+
 
 
 def server(input: Inputs, output: Outputs, session: Session):
