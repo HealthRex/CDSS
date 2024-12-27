48d8355481487d17ab734835b5349d168d2f611e
gracek012 <49053558+gracek012@users.noreply.github.com>
2024-12-27 11:17:24 -0500

    Update cosmos.py

--- scripts/Heme-Stamp/cosmos.py
+++ scripts/Heme-Stamp/cosmos.py
@@ -4,60 +4,58 @@ import azure.cosmos.exceptions as exceptions
 from azure.cosmos.partition_key import PartitionKey
 import datetime
 import logging
+import os
 
 # import config
-settings = {
-    'host': 'https://machinelearningoutput.documents.azure.com:443/',
-    'master_key': 'LufjB7LUP4aRaDGIRs3NTTdIU440Gv1hlhVV7453HcL1UeJs3oXoj55lPsNmckzZMNEFXSu3oFTe2K3DsxLpZg==',
-    'database_id': 'machinelearningdb',
-    'container_id': 'machinelearningcontainer'
-}
-HOST = settings['host']
-MASTER_KEY = settings['master_key']
-DATABASE_ID = settings['database_id']
-CONTAINER_ID = settings['container_id']
+HOST = os.environ["COSMOS_HOST"]
+MASTER_KEY = os.environ["COSMOS_KEY"]
+DATABASE_ID = os.environ["DATABASE_ID"]
+CONTAINER_ID = os.environ["CONTAINER_ID"]
 
 
-def query_items(container):
+def query_items(container, partition_key):
     print('\nQuerying for an  Item by Partition Key\n')
 
-    # Including the partition key value of account_number in the WHERE filter results in a more efficient query
     items = list(container.query_items(
-        query="SELECT * FROM c WHERE c.partitionKey = 'HemeStamp'",
+        query=f"SELECT * FROM c WHERE c.partitionKey = '{partition_key}'",
     ))
     return items
-    # for item in items:
-    #    print(f"Item: {item}")
 
-
-def query_items_bydate(container, qdate):
+def query_items_bydate(container, qdate, partition_key):
     print('\nQuerying for an  Item by Partition Key\n')
 
-    # Including the partition key value of account_number in the WHERE filter results in a more efficient query
+    query = f"""
+    SELECT * FROM c WHERE c.partitionKey = '{partition_key}'
+    AND TimestampToDateTime(c._ts*1000) > '{qdate}'
+    """
     items = list(container.query_items(
-        query=f"SELECT * FROM c WHERE c.partitionKey = 'HemeStamp' AND TimestampToDateTime(c._ts*1000) > '{qdate}'",
+        query=query,
     ))
     return items
 
 
-def query_items_between_dates(container, date1, date2):
+def query_items_between_dates(container, date1, date2, partition_key):
     print('\nQuerying for an  Item by Partition Key\n')
 
-    # Including the partition key value of account_number in the WHERE filter results in a more efficient query
+    query = f"""
+    SELECT * FROM c WHERE c.partitionKey = '{partition_key}' AND 
+    TimestampToDateTime(c._ts*1000) >= '{date1}' AND 
+    TimestampToDateTime(c._ts*1000) <= '{date2}'"
+    """
     items = list(container.query_items(
-        query=f"SELECT * FROM c WHERE c.partitionKey = 'HemeStamp' AND TimestampToDateTime(c._ts*1000) >= '{date1}' AND TimestampToDateTime(c._ts*1000) <= '{date2}' ",
+        query=query
     ))
     return items
 
-    # for item in items:
-    #    print(f"Item: {item}")
 
-
-def get_epic_order(patient):
+def get_epic_order(patient, partition_key):
+    """
+    Formats the item to save (appends partition key)
+    """
     patient['DOB'] = ''
     order1 = {
         'id': datetime.datetime.now().strftime('%c'),
-        'partitionKey': 'HemeStamp',
+        'partitionKey': partition_key,
         'orderid': 'None',
         'order_date': datetime.datetime.now().strftime('%c'),
         'patient': patient
@@ -66,18 +64,25 @@ def get_epic_order(patient):
     return order1
 
 
-def create_item(container, patient):
-    # farts
-
-    epic_patient = get_epic_order(patient)
-    logging.info(epic_patient)
-
+def create_item(container, patient, partition_key):
+    """
+    Given a container, a patient dictionary, and a partion key, creates the
+    item
+    Args:
+        container: instantiated from cosmoswrite
+        patient: dictionary of data to save
+        partition_key: string to identify the model that this inference relates
+            to
+    """
+    epic_patient = get_epic_order(patient, partition_key)
     container.create_item(body=epic_patient)
 
 
 def getcosmosbetweendate(date1, date2):
     client = cosmos_client.CosmosClient(
-        HOST, {'masterKey': MASTER_KEY}, user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
+        HOST, {'masterKey': MASTER_KEY},
+        user_agent="CosmosDBPythonQuickstart",
+        user_agent_overwrite=True)
     # setup database for this sample
     try:
         db = client.create_database(id=DATABASE_ID)
@@ -89,7 +94,8 @@ def getcosmosbetweendate(date1, date2):
 
     # setup container for this sample
     try:
-        container = db.create_container(id=CONTAINER_ID, partition_key=PartitionKey(path='/partitionKey'))
+        container = db.create_container(id=CONTAINER_ID,
+                                        partition_key=PartitionKey(path='/partitionKey'))
         print('Container with id \'{0}\' created'.format(CONTAINER_ID))
 
     except exceptions.CosmosResourceExistsError:
@@ -100,8 +106,8 @@ def getcosmosbetweendate(date1, date2):
 
 
 def getcosmosbydate(qdate):
-    client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY}, user_agent="CosmosDBPythonQuickstart",
-                                        user_agent_overwrite=True)
+    client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY},
+                                        user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
     # setup database for this sample
     try:
         db = client.create_database(id=DATABASE_ID)
@@ -113,7 +119,8 @@ def getcosmosbydate(qdate):
 
     # setup container for this sample
     try:
-        container = db.create_container(id=CONTAINER_ID, partition_key=PartitionKey(path='/partitionKey'))
+        container = db.create_container(id=CONTAINER_ID,
+                                        partition_key=PartitionKey(path='/partitionKey'))
         print('Container with id \'{0}\' created'.format(CONTAINER_ID))
 
     except exceptions.CosmosResourceExistsError:
@@ -124,8 +131,8 @@ def getcosmosbydate(qdate):
 
 
 def getcosmos():
-    client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY}, user_agent="CosmosDBPythonQuickstart",
-                                        user_agent_overwrite=True)
+    client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY},
+                                        user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
     # setup database for this sample
     try:
         db = client.create_database(id=DATABASE_ID)
@@ -137,7 +144,8 @@ def getcosmos():
 
     # setup container for this sample
     try:
-        container = db.create_container(id=CONTAINER_ID, partition_key=PartitionKey(path='/partitionKey'))
+        container = db.create_container(id=CONTAINER_ID,
+                                        partition_key=PartitionKey(path='/partitionKey'))
         print('Container with id \'{0}\' created'.format(CONTAINER_ID))
 
     except exceptions.CosmosResourceExistsError:
@@ -147,15 +155,25 @@ def getcosmos():
     return query_items(container)
 
 
-def cosmoswrite(patient):
-    client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY}, user_agent="CosmosDBPythonQuickstart",
-                                        user_agent_overwrite=True)
+def cosmoswrite(patient, partition_key):
+    """
+    Base funtion to call that enables us to write an inference packet
+    to cosmos.
+    Args:
+        patient: a dictionary of items to write to cosmos
+        partition_key: a string indicating partition to write to. General
+            rule of thumb is that each new instantation of a model should
+            get its own partition key. ex: 20220705_label_hct.
+    """
+    client = cosmos_client.CosmosClient(
+        HOST, {'masterKey': MASTER_KEY},
+        user_agent="CosmosDBPythonQuickstart",
+        user_agent_overwrite=True)
 
     # setup database for this sample
     try:
         db = client.create_database(id=DATABASE_ID)
         print('Database with id \'{0}\' created'.format(DATABASE_ID))
-
 
     except exceptions.CosmosResourceExistsError:
         db = client.get_database_client(DATABASE_ID)
@@ -163,12 +181,12 @@ def cosmoswrite(patient):
 
     # setup container for this sample
     try:
-        container = db.create_container(id=CONTAINER_ID, partition_key=PartitionKey(path='/partitionKey'))
+        container = db.create_container(id=CONTAINER_ID,
+                                        partition_key=PartitionKey(path='/partitionKey'))
         print('Container with id \'{0}\' created'.format(CONTAINER_ID))
-
 
     except exceptions.CosmosResourceExistsError:
         container = db.get_container_client(CONTAINER_ID)
         print('Container with id \'{0}\' was found'.format(CONTAINER_ID))
 
-    create_item(container, patient)
+    create_item(container, patient, partition_key)
