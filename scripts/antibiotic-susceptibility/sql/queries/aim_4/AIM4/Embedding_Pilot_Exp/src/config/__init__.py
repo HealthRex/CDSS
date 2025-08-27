1e0c9d25200c95ae6da33842311479ce75634a09
Fateme (Fatima) <fateme.nateghi86@gmail.com>
2025-08-27 11:41:25 -0700

    Delete scripts/antibiotic-susceptibility/sql directory

--- scripts/antibiotic-susceptibility/sql/queries/aim_4/AIM4/Embedding_Pilot_Exp/src/config/__init__.py
+++
@@ -1,4 +0,0 @@
-from .config import LLM_O3_MINI_API_URL
-from .config import LLM_API_KEY
-
-__all__ = ["LLM_O3_MINI_API_URL", "LLM_API_KEY"]
--- scripts/antibiotic-susceptibility/sql/queries/aim_4/aim4_no_growth.sql
+++
@@ -1,6 +0,0 @@
--- TODO: Get review by Jon/Nick/project people to see if this is good to go/done
-CREATE OR REPLACE TABLE `antimicrobial_stewardship.aim4_no_growth` AS
-
-SELECT *
-FROM `antimicrobial_stewardship.aim4_base_cohort`
-WHERE was_positive = 0;
