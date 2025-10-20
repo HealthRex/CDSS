741de24f8023cc9ae2b8a76efabd9067121bb972
Fateme (Fatima) <fateme.nateghi86@gmail.com>
2025-10-20 11:46:13 -0700

    Refactor antibiotic class exposure SQL query

--- scripts/antibiotic-susceptibility/Aim 1a/time-to-event-augmented-queries/antibiotic_class_exposure.sql
+++ scripts/antibiotic-susceptibility/Aim 1a/time-to-event-augmented-queries/antibiotic_class_exposure.sql
@@ -1,26 +1,25 @@
-##############################################################################################################################################################
-#Goal: Indicating exposure to a class of antibiotics before specimen collection.
-##############################################################################################################################################################
-CREATE OR REPLACE TABLE `som-nero-phi-jonc101.antimicrobial_stewardship.microbiology_cultures_antibiotic_class_exposure_augmented` AS (
+###############################################################################################################
+# Goal: Indicate exposure to an antibiotic *class* before specimen collection.
+###############################################################################################################
+
+CREATE OR REPLACE TABLE `som-nero-phi-jonc101.antimicrobial_stewardship.microbiology_cultures_antibiotic_class_exposure` AS
 WITH class_exposure AS (
     SELECT
         mcp.anon_id,
         mcp.pat_enc_csn_id_coded,
         mcp.order_proc_id_coded,
         mcp.order_time_jittered_utc,
-        mcp.medication_category,
         mcp.medication_name,
+        mcp.medication_category,
         cl.antibiotic_class,
-        mcp.medication_time,
-        mcp.medication_time_to_cultureTime
+        mcp.medication_time_to_culturetime
     FROM
-        `som-nero-phi-jonc101.antimicrobial_stewardship.microbiology_cultures_prior_med_augmented` mcp
+        `som-nero-phi-jonc101.antimicrobial_stewardship.microbiology_cultures_medication_exposure` AS mcp
     LEFT JOIN
-        `som-nero-phi-jonc101.antimicrobial_stewardship.class_subtype_lookup` cl
+        `som-nero-phi-jonc101.antimicrobial_stewardship.class_subtype_lookup` AS cl
     ON
-        mcp.medication_name = cl.antibiotic
+        LOWER(mcp.medication_name) = LOWER(cl.antibiotic)
 )
 SELECT *
 FROM class_exposure
-    where medication_time_to_cultureTime>0
-)
+WHERE medication_time_to_culturetime > 0;
