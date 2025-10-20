42f978ddd9a15b722ea7e712b1d598383df218e3
Fateme (Fatima) <fateme.nateghi86@gmail.com>
2025-10-20 16:02:33 -0700

    Create table for pediatric antibiotic class exposure
    This SQL script creates a new table to indicate antibiotic class exposure in pediatric microbiology cultures before specimen collection.

---
+++ scripts/antibiotic-susceptibility/Aim 1a/pediatrics_queries/microbiology_cultures_antibiotic_class_exposure_peds.sql
@@ -0,0 +1,25 @@
+###############################################################################################################
+# Goal: Indicate exposure to an antibiotic *class* before specimen collection.
+###############################################################################################################
+
+CREATE OR REPLACE TABLE `som-nero-phi-jonc101.antimicrobial_stewardship.microbiology_cultures_antibiotic_class_exposure_peds` AS
+WITH class_exposure AS (
+    SELECT
+        mcp.anon_id,
+        mcp.pat_enc_csn_id_coded,
+        mcp.order_proc_id_coded,
+        mcp.order_time_jittered_utc,
+        mcp.medication_name,
+        mcp.medication_category,
+        cl.antibiotic_class,
+        mcp.medication_time_to_culturetime
+    FROM
+        `som-nero-phi-jonc101.antimicrobial_stewardship.microbiology_cultures_medication_exposure_peds` AS mcp
+    LEFT JOIN
+        `som-nero-phi-jonc101.antimicrobial_stewardship.class_subtype_lookup` AS cl
+    ON
+        LOWER(mcp.medication_name) = LOWER(cl.antibiotic)
+)
+SELECT *
+FROM class_exposure
+WHERE medication_time_to_culturetime >= 1; 
